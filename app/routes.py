1234bdb82003acb31143277203d2fc1d67788980
Rediet Medhane <redietkmedhane@gmail.com>
2023-05-03 16:58:34 -0700

    added endpoints for get_task_by_id, create_new_task, and create validate_task helper function

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, make_response, request
+from flask import abort, Blueprint, jsonify, make_response, request
 from app import db
 from app.models.task import Task
 
@@ -17,14 +17,23 @@ def create_new_task():
     request_body = request.get_json()
     new_task = Task(
         title = request_body["title"],
-        description = request_body["description"],
-        completed_at = request_body["completed_at"]
+        description = request_body["description"]
     )
     db.session.add(new_task)
     db.session.commit()
 
     result = new_task.to_dict()
-    return jsonify(result), 201
+    return {"task": new_task.to_dict()}, 201
 
+@task_bp.route("/<task_id>", methods = ["GET"])
+def get_task_by_id(task_id):
+    task = validate_task(task_id)
+    return {"task": task.to_dict()}, 200
 
-
+def validate_task(task_id):
+    try:
+        valid_task_id = int(task_id)
+    except:
+        return abort(make_response({"message": f"invalid id: {task_id}"}, 400))
+    
+    return Task.query.get_or_404(valid_task_id)
