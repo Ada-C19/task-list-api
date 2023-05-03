f723c35208d4ce6a9ca9ee2d48163eea62b05d55
dreinicke27 <danicareinicke@gmail.com>
2023-05-03 14:52:51 -0700

    Implemented POST function that can create a valid taks with null completed_at

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,26 @@
-from flask import Blueprint
+from flask import Blueprint, make_response, request, jsonify
+from app import db
+from app.models.task import Task
+
+tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+@tasks_bp.route("", methods=["POST"])
+def add_task():
+    request_body = request.get_json()
+    new_task = Task(
+        title = request_body["title"],
+        description = request_body["description"],
+        completed_at = request_body["completed_at"],
+    )
+    
+    response = {}
+    response["task"] = new_task
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return {"task":{
+        "title": new_task.title, 
+        "description": new_task.description,
+        "completed_at": new_task.completed_at
+    }}, 201
