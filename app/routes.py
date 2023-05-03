5d008bc70f1c828a9586ed610a71a669ecc1ef8f
jasmine <jtmsoto@gmail.com>
2023-05-03 16:24:36 -0700

    Refactor get_all_tasks to return a list of tasks with the correct key-value pairs

--- app/routes.py
+++ app/routes.py
@@ -7,4 +7,19 @@ task_bp = Blueprint("task", __name__, url_prefix="/tasks")
 
 @task_bp.route("", methods=["GET"])
 def get_all_tasks():
-    return jsonify([]), 200
+    response = []
+    tasks = Task.query.all()
+
+    for task in tasks:
+        task_dict = {
+            "id": task.task_id,
+            "title": task.title,
+            "description": task.description,
+            "is_complete": False
+        }
+        if task.completed_at is not None:
+            task_dict["is_complete"] = True
+
+        response.append(task_dict)
+
+    return jsonify(response), 200
