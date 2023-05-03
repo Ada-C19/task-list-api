093427c4690def6aa9afcc803c753ea298d0307d
Julie Loh <jlohusky@gmail.com>
2023-05-03 15:54:37 -0700

    updated Task model and added from_dict class method. Created POST method to routes.py

--- app/models/task.py
+++ app/models/task.py
@@ -2,7 +2,13 @@ from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     description = db.Column(db.Text)
+    is_complete = db.Column(db.Boolean, default=False)
     completed_at = db.Column(db.DateTime, nullable=True, default=None)
+
+    @classmethod
+    def from_dict(cls, task_data):
+        new_task = Task(title=task_data["title"], description=task_data["description"])
+        return new_task
--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,24 @@
+from app import db
+from app.models.task import Task
 from flask import Blueprint
+from flask import Blueprint, jsonify, abort, make_response, request
+
+
+tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+@tasks_bp.route("", methods=["POST"])
+def create_task():
+    request_body = request.get_json()
+    new_task = Task.from_dict(request_body)
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return {
+        "task": {
+            "id": new_task.id,
+            "title": new_task.title,
+            "description": new_task.description,
+            "is_complete": new_task.is_complete
+        }
+    }, 201
