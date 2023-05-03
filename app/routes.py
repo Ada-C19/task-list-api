cc4677c239c9b569b846b4a28e3979c6a975e7f1
Rediet Medhane <redietkmedhane@gmail.com>
2023-05-03 15:15:35 -0700

    defined blueprint and registered first endpoint

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,10 @@
 from flask import Blueprint
+from app import db
+from app.models.task import Task
+
+task_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+@task_bp.route("", methods = ["GET"])
+def get_all_tasks():
+    response_body = "All the tasks!"
+    return response_body, 200
