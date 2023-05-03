18debd5042f0505b8040992b3f0f4ad088eea699
jasmine <jtmsoto@gmail.com>
2023-05-03 16:01:43 -0700

    Add GET route and function get_all_tasks

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,10 @@
-from flask import Blueprint
+from flask import abort, Blueprint, jsonify, make_response, request
+from app import db
+from app.models.task import Task
+
+
+task_bp = Blueprint("task", __name__, url_prefix="/tasks")
+
+@task_bp.route("", methods=["GET"])
+def get_all_tasks():
+    return jsonify([]), 200
