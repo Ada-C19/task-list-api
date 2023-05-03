b393c75cb2af90d8d03c89f37e585b5e31650365
Rediet Medhane <redietkmedhane@gmail.com>
2023-05-03 14:48:08 -0700

    sets up Task model with attribbutes and to_dict method

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,17 @@
 from app import db
+from datetime import datetime
 
 
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    completed_at = db.Column(db.DateTime, nullable = True, default = None)
+
+    def to_dict(self):
+        return {
+            "task_id" : self.id,
+            "title" : self.title,
+            "description" : self.description,
+            "completed_at" : self.completed_at 
+        }
