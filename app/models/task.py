ea8078d7e3f1959ca29fe36ab80771ebf8c8fef0
April Zhang <aprilzh23@gmail.com>
2023-05-03 16:10:55 -0700

    Added is_complete column in clas Task

--- app/models/task.py
+++ app/models/task.py
@@ -3,7 +3,8 @@ from app import db
 
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    title = db.Column(db.String)
+    title = db.Column(db.String, unique=True)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable=True)
+    is_complete = db.Column(db.Boolean, default=False, nullable=False)
     
--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from app.models.task import Task
 
+# Define all routes with tasks_bp start with url_prefix (/tasks)
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+# @blueprint_name.route("/endpoint/path/here", methods=["GET"])
+# def endpoint_name():
+#     my_beautiful_response_body = "Hello, World!"
+#     return my_beautiful_response_body
+
+@tasks_bp.route("", methods=["POST"])
+def create_a_task():
+    
+    
+    request_body = request.get_json()
+    new_task = Task(title=request_body["title"], description=request_body["description"], completed_at=request_body["completed_at"])
+    return request_body
