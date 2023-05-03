d0a777c38a52752855307931cc7c067e8f297390
Melinda Mott <melinda.mott@gmail.com>
2023-05-03 15:25:27 -0700

    started cleaning up routes.py

--- app/routes.py
+++ app/routes.py
@@ -19,27 +19,29 @@ def validate_task(task_id):
 
     return task
 
-@tasks_bp.route("", methods=["POST"])
+@tasks_bp.route("/task", methods=["POST"])
 def create_task():
     request_body = request.get_json()
     new_task = Task(title=request_body["title"],
-                    description=request_body["description"])
+                    description=request_body["description"],
+                    completed_at=request_body["is_complete"])
 
     db.session.add(new_task)
     db.session.commit()
 
     return make_response(f"Task {new_task.title} successfully created", 201)
 
-@tasks_bp.route("", methods=["GET"])
+@tasks_bp.route("/task", methods=["GET"])
 def read_all_tasks():
     tasks_response = []
     tasks = Task.query.all()
-    for book in tasks:
+    for task in tasks:
         tasks_response.append(
             {
-                "id": book.id,
-                "title": book.title,
-                "description": book.description
+                "id": task.id,
+                "title": task.title,
+                "description": task.description,
+                "is_complete": task.completed_at
             }
         )
     return jsonify(tasks_response)
@@ -50,7 +52,8 @@ def read_one_task(task_id):
     return {
             "id": task.id,
             "title": task.title,
-            "description": task.description
+            "description": task.description,
+            "is_complete": task.completed_at
         }
 
 @tasks_bp.route("/<task_id>", methods=["PUT"])
@@ -61,6 +64,7 @@ def update_task(task_id):
 
     task.title = request_body["title"]
     task.description = request_body["description"]
+    task.completed_at = request_body["is_completed"]
 
     db.session.commit()
 
