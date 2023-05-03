458532316b3a2cd35b35960adefe0bd448340be7
Julie Loh <jlohusky@gmail.com>
2023-05-03 16:01:52 -0700

    added to_dict instance method to Task Class and created GET method to get all tasks in routes.py

--- app/models/task.py
+++ app/models/task.py
@@ -8,6 +8,15 @@ class Task(db.Model):
     is_complete = db.Column(db.Boolean, default=False)
     completed_at = db.Column(db.DateTime, nullable=True, default=None)
 
+    def to_dict(self):
+        task_as_dict = {}
+        task_as_dict["id"] = self.id
+        task_as_dict["title"] = self.title
+        task_as_dict["description"] = self.description
+        task_as_dict["is_complete"] = self.is_complete
+
+        return task_as_dict
+
     @classmethod
     def from_dict(cls, task_data):
         new_task = Task(title=task_data["title"], description=task_data["description"])
--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,14 @@ from flask import Blueprint, jsonify, abort, make_response, request
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 
+@tasks_bp.route("", methods=["GET"])
+def read_all_tasks():
+    tasks = Task.query.all()
+
+    tasks_response = [task.to_dict() for task in tasks]
+
+    return jsonify(tasks_response), 200
+
 @tasks_bp.route("", methods=["POST"])
 def create_task():
     request_body = request.get_json()
