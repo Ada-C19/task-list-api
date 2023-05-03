6f5b118d10fa7bf1ae3434b33226a0847c785901
jasmine <jtmsoto@gmail.com>
2023-05-03 16:35:44 -0700

    Add GET route and function get_one_task

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,7 @@ from app.models.task import Task
 
 task_bp = Blueprint("task", __name__, url_prefix="/tasks")
 
+
 @task_bp.route("", methods=["GET"])
 def get_all_tasks():
     response = []
@@ -23,3 +24,18 @@ def get_all_tasks():
         response.append(task_dict)
 
     return jsonify(response), 200
+
+
+@task_bp.route("/<task_id>", methods=["GET"])
+def get_one_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    task_dict = {
+        "id": task.task_id,
+        "title": task.title,
+        "description": task.description,
+        "is_complete": False
+    }
+    if task.completed_at is not None:
+        task_dict["is_complete"] = True
+
+    return {"task": task_dict}, 200
