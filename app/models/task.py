2842c44330dca0ecb9040565052912b21c08b990
Sel <selenetenorio27@hotmail.com>
2023-05-03 20:40:15 -0700

    Model Task created and blueprint registered

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,15 @@
+from sqlalchemy import DateTime
 from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    completed_at = db.Column(db.DateTime, nullable=True, default=None)
+
+    def to_dict(self):
+        return {"id":self.id,
+                "title": self.title,
+                "description": self.description,
+                "completed_at":self.completed_at}
--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,22 @@
-from flask import Blueprint
+from app import db
+from app.models.task import Task
+from flask import Blueprint, jsonify, abort, make_response, request
+
+tasks_bp = Blueprint("tasks_bp",__name__, url_prefix="/tasks")
+
+@tasks_bp.route("",methods=["POST"])
+def create_task():
+    request_body = request.get_json()
+
+    new_task = Task(title=request_body["title"],
+                    description=request_body["description"],
+                    completed_at=request_body["completed_at"])
+    
+    db.session.add(new_task)
+    db.session.commit()
+
+    return {"id":new_task.id,
+            "title": new_task.title,
+            "description": new_task.description,
+            "completed_at":new_task.completed_at, 
+            "msg": "successfully created"}, 201
