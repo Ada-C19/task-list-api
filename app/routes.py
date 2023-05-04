a719a05cf20f35726cceaadb8986f70434da4e67
louloera <louloera@gmail.com>
2023-05-03 21:50:39 -0700

    Validated that post json request contains title and details

--- app/routes.py
+++ app/routes.py
@@ -24,8 +24,12 @@ def handle_id_request(cls,id):
 def create_task():
 
     request_body = request.get_json()
-    new_task = Task.from_dict(request_body)
+
+    if "title" not in request_body or "description" not in request_body:
+        return make_response({"details": "Invalid data"},400)
     
+    new_task = Task.from_dict(request_body)
+
     db.session.add(new_task)
     db.session.commit()
 
