4a547d582c77bb63f0944e8c2dcd10a760f399dd
Jay King <jayriverking@gmail.com>
2023-05-04 14:55:13 -0400

    wave 01 method 01

--- app/models/task.py
+++ app/models/task.py
@@ -4,10 +4,13 @@ from datetime import datetime
 
 class Task(db.Model):
 # task_id: a primary key for each task
-    task_id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 # title: text to name the task
     title = db.Column(db.String)
 # description: text to describe the task
     description = db.Column(db.String)
 # completed_at: a datetime that has the date that a task is completed on. Can be nullable, and contain a null value. A task with a null value for completed_at has not been completed. When we create a new task, completed_at should be null AKA None in Python.
     completed_at = db.Column(db.DateTime, nullable=True)
+
+    def is_true(self):
+        return bool(self.completed_at)
--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,32 @@
-from flask import Blueprint
+from flask import Blueprint, make_response, request, jsonify, abort
+from app.models.task import Task
+from app import db
+
+
+tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+
+@tasks_bp.route("", methods=["POST"])
+def make_tast():
+    try:
+        request_body = request.get_json()
+
+        new_task = Task(
+            title = request_body["title"],
+            description =  request_body["description"],
+            completed_at = request_body["completed_at"]
+        )
+
+        db.session.add(new_task)
+        db.session.commit()
+
+        return make_response({
+"task": {
+    "id": new_task.task_id,
+    "title": new_task.title,
+    "description": new_task.description,
+    "is_complete": bool(new_task.completed_at)
+}
+}, 201)
+    except KeyError as error:
+        abort(make_response(f"{error.__str__()} is missing", 400))
