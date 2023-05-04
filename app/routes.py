aa872e34d12b63014ca52b17da374d9efab040dc
Elizabeth Zachary <elizabethzachary96@gmail.com>
2023-05-04 16:05:20 -0700

    added blueprint tasks_bp to __init__ and set up route for get_all_tasks()

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from app.models.task import Task
+from app import db
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 
 # get all endpoint
-@bp.route("", methods=["GET"])
+@tasks_bp.route("", methods=["GET"])
 def get_all_tasks():
-    pass
+    
+    tasks = Task.query.all()
+
+    task_list = [task.to_dict() for task in tasks]
+
+    return jsonify(task_list), 200
