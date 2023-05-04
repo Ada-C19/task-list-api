2a2d184545345b134d4d6e764dae981570d05542
Sophia Tran <sophiat8832@gmail.com>
2023-05-04 15:23:30 -0700

    updated Task model and changed return body of POST route to pass test_create_task

--- app/routes.py
+++ app/routes.py
@@ -4,3 +4,21 @@ from app import db
 from os import abort
 
 tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
+
+@tasks_bp.route("", methods=["POST"])
+def create_task():
+    request_body = request.get_json()
+    new_task = Task(title=request_body["title"],
+                    description=request_body["description"])
+    
+    db.session.add(new_task)
+    db.session.commit()
+
+    return {
+        "task": {
+            "id": new_task.id,
+            "title": new_task.title,
+            "description": new_task.description,
+            "is_complete": bool(new_task.completed_at)
+        }
+    }, 201
