1d8349b1b7e660db763e1b89af46194d92b759df
Monica <monicalagdaan@hotmail.com>
2023-05-04 14:25:51 -0700

    worked on wave 1

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,19 @@
-from flask import Blueprint
+from app import db
+from app.models.task import Task
+from flask import Blueprint, jsonify, make_response, request
+
+tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+@tasks_bp.route("", methods=["POST"])
+def create_one_task():
+    request_body = request.get_json()
+    new_task = Task(
+        title=request_body["title"],
+        description=request_body["description"],
+        completed_at=request_body["completed_at"])
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return jsonify(new_task), 201
+
