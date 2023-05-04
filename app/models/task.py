341e30d529807c99ff5c7e11e4d00a25ecd1ed57
Arianna Walden <nedlawa@gmail.com>
2023-05-04 14:41:26 -0700

    complete setup and first read route

--- app/models/task.py
+++ app/models/task.py
@@ -3,3 +3,21 @@ from app import db
 
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    completed_at = db.Column(db.DateTime, nullable=True)
+
+    def to_dict(task):
+        return dict(
+                id=task.task_id,
+                title=task.title,
+                description=task.description,
+                is_complete=task.completed_at
+    ) 
+
+    @classmethod
+    def from_dict(cls, task_data):
+        new_task = Task(title=task_data["title"],
+                        description=task_data["description"],
+                        completed_at=None,)
+        return new_task
--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,28 @@
-from flask import Blueprint
+from app import db
+from flask import Blueprint, request, make_response, jsonify
+from app.models.task import Task
+
+
+tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+@tasks_bp.route("", methods=["POST"])
+def create_tasks():
+    request_body = request.get_json()
+    new_task = Task.from_dict(request_body)
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return make_response(jsonify(f"'task':{new_task.to_dict()}"), 201)
+
+@tasks_bp.route("/<task_id>", methods=["GET"])
+def handle_task(task_id):
+    task = Task.validate_model(Task, task_id)
+    return Task.to_dict(task)
+
+@tasks_bp.route("", methods=["GET"])
+def handle_tasks():
+    tasks = Task.query.all()
+    tasks_response = [Task.to_dict(task) for task in tasks]
+
+    return make_response(jsonify(tasks_response),200)
