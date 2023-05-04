24613f529a730a24aa78b38c19e781d3dad5d433
Elizabeth Zachary <elizabethzachary96@gmail.com>
2023-05-04 15:55:43 -0700

    fixed model migration

--- app/models/task.py
+++ app/models/task.py
@@ -2,21 +2,22 @@ from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
-    title = db.Column()
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable=True)
 
     @classmethod
     def from_dict(cls, data_dict):
         return cls(
+            id=cls.id,
             title=data_dict["title"],
             description=data_dict["description"]
         )
 
     def to_dict(self):
         return dict(
-            id=self.task_id,
+            id=self.id,
             title=self.title,
             description=self.description
         )
