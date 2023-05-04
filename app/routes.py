641b8f40b2b3dba1326cf37ac4761640d29efcd3
Sydney Alberson <sydney.alberson@gmail.com>
2023-05-04 14:21:24 -0400

    Created route to read all tasks

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,20 @@
 from flask import Blueprint
+from app import db
+
+task_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+# Route to get tasks
+@task_bp.route("", methods=["GET"])
+
+def read_all_tasks():
+    tasks_response = []
+
+    tasks = Task.query.all()
+
+    for task in tasks:
+        tasks_reponse.append({
+            "title": task.title,
+            "description": task.description,
+        })
+
+    return tasks_response
