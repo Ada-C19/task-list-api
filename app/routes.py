f8d0c41dacd6cc0d15ff0696f89a603fdf379be6
Elizabeth Valle <e7valle@gmail.com>
2023-05-03 19:15:45 -0700

    Registered and created task_bp, created route for POST

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,29 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify, make_response
+from app import db
+from app.models.task import Task
+
+task_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+@task_bp.route("", methods=["POST"])
+def add_task():
+    request_body = request.get_json()
+    new_task = Task(
+        title = request_body["title"],
+        description = request_body["description"],
+        completed_at = request_body["completed_at"]
+    )
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    is_complete = True
+    if not new_task.completed_at:
+        is_complete = False
+    
+
+    return jsonify({"id": new_task.task_id,
+            "title": new_task.title,
+            "description": new_task.description,
+            "is_complete": is_complete
+            }), 201
+
