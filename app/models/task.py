4987db33887e8a074aca6ec723371fd01cbd6f0d
kathleen <kathleen.n.shin@gmail.com>
2023-05-04 11:06:04 -0700

    Added to_dict class method to task class

--- app/models/task.py
+++ app/models/task.py
@@ -7,3 +7,14 @@ class Task(db.Model):
     description = db.Column(db.Text)
     completed_at = db.Column(db.DateTime, nullable=True, default=None)
 
+    def to_dict(self):
+        task = {
+        "id": self.task_id,
+        "title": self.title,
+        "description": self.description,
+        "is_complete": False
+    }
+        if self.completed_at is not None:
+            task["is_complete"] = True
+
+        return task
