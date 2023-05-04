23b0bfcd1e143efcb60516dfbba1f0be618d2fa7
Rediet Medhane <redietkmedhane@gmail.com>
2023-05-04 12:11:57 -0700

    created 404endpoint for delete with invalid id and 400 endpoints for invalid data post requests

--- app/routes.py
+++ app/routes.py
@@ -16,26 +16,29 @@ def get_tasks():
 @task_bp.route("", methods = ["POST"])
 def create_new_task():
     request_body = request.get_json()
-    new_task = Task(
-        title = request_body["title"],
-        description = request_body["description"]
-    )
-    db.session.add(new_task)
-    db.session.commit()
+    try:
+        new_task = Task(
+            title = request_body["title"],
+            description = request_body["description"]
+        )
+        db.session.add(new_task)
+        db.session.commit()
 
-    result = new_task.to_dict()
-    return {"task": new_task.to_dict()}, 201
+        result = new_task.to_dict()
+        return {"task": new_task.to_dict()}, 201
+    except:
+        return abort(make_response({"details": "Invalid data"}, 400))
 
 
 @task_bp.route("/<task_id>", methods = ["GET"])
 def get_task_by_id(task_id):
-    task = validate_task(task_id)
+    task = validate_task_id(task_id)
     return {"task": task.to_dict()}, 200
 
 
 @task_bp.route("/<task_id>", methods = ["PUT"])
 def update_one_task(task_id):
-    updated_task = validate_task(task_id)
+    updated_task = validate_task_id(task_id)
 
     request_body = request.get_json()
     updated_task.title = request_body["title"],
@@ -46,14 +49,14 @@ def update_one_task(task_id):
 
 @task_bp.route("/<task_id>", methods = ["DELETE"])
 def delete_one_task(task_id):
-    task = validate_task(task_id)
+    task = validate_task_id(task_id)
     
     db.session.delete(task)
     db.session.commit()
 
     return {"details": f'Task {task_id} "{task.title}" successfully deleted'}, 200
 
-def validate_task(task_id):
+def validate_task_id(task_id):
     try:
         valid_task_id = int(task_id)
     except:
