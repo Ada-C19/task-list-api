9604a0a78877ec68d0a38114397862ea2a52f509
Ana Villalobos <ana.villalobos309@gmail.com>
2023-05-04 12:02:19 -0700

    Finished writing the tests for test_wave_01

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -50,7 +50,7 @@ def test_get_task(client, one_task):
         }
     }
 
-
+#####this one that we finished the test for
 @pytest.mark.skip(reason="No way to test this feature yet")
 def test_get_task_not_found(client):
     # Act
@@ -59,11 +59,8 @@ def test_get_task_not_found(client):
 
     # Assert
     assert response.status_code == 404
-
-    raise Exception("Complete test with assertion about response body")
-    # *****************************************************************
-    # **Complete test with assertion about response body***************
-    # *****************************************************************
+    #myassert
+    assert response_body == []
 
 
 @pytest.mark.skip(reason="No way to test this feature yet")
@@ -118,7 +115,7 @@ def test_update_task(client, one_task):
     assert task.description == "Updated Test Description"
     assert task.completed_at == None
 
-
+#####this one that we finished the test for
 @pytest.mark.skip(reason="No way to test this feature yet")
 def test_update_task_not_found(client):
     # Act
@@ -130,11 +127,8 @@ def test_update_task_not_found(client):
 
     # Assert
     assert response.status_code == 404
-
-    raise Exception("Complete test with assertion about response body")
-    # *****************************************************************
-    # **Complete test with assertion about response body***************
-    # *****************************************************************
+    #myassert
+    assert response_body == []
 
 
 @pytest.mark.skip(reason="No way to test this feature yet")
@@ -151,7 +145,7 @@ def test_delete_task(client, one_task):
     }
     assert Task.query.get(1) == None
 
-
+#####this one that we finished the test for
 @pytest.mark.skip(reason="No way to test this feature yet")
 def test_delete_task_not_found(client):
     # Act
@@ -160,12 +154,9 @@ def test_delete_task_not_found(client):
 
     # Assert
     assert response.status_code == 404
-
-    raise Exception("Complete test with assertion about response body")
-    # *****************************************************************
-    # **Complete test with assertion about response body***************
-    # *****************************************************************
-
+    #myassert
+    assert response == []
+    #this was already here:::: idk what the helk it does
     assert Task.query.all() == []
 
 
