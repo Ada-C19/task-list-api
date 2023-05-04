a728fc233ebb5b78a63d7172ae7eff42c09a10fb
Jay King <jayriverking@gmail.com>
2023-05-04 16:20:51 -0400

    commented out completed_at for create_task to pass the last test -- will work on it later if I can

--- app/routes.py
+++ app/routes.py
@@ -25,11 +25,10 @@ def validate_task(task_id):
 def create_task():
     try:
         request_body = request.get_json()
-
         new_task = Task(
             title = request_body["title"],
             description =  request_body["description"],
-            # completed_at = request_body["completed_at"] if request_body["completed_at"] else default
+            # completed_at = request_body["completed_at"]
         )
 
         db.session.add(new_task)
