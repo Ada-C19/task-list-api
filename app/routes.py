4417084b0bc4b7f4f5c1f539d3ae452ad9d2c705
Rainacam <rainacam12@gmail.com>
2023-05-04 14:25:22 -0400

    added comments for routes

--- app/routes.py
+++ app/routes.py
@@ -2,3 +2,8 @@ from flask import Blueprint, jsonify, abort, make_response, request
 from .models.task import Task
 from app import db
 
+task_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+# create task route POST
+# update task route UPDATE
+# delete task route DELETE
