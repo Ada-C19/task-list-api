21a1383a697516c9c07fd4a194945af03d4f062d
Lyuda Kim <lyudarkim@gmail.com>
2023-05-04 00:05:52 -0700

    Added validate_task() helper function to app/routes.py

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,17 @@ from app import db
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 
 
+def validate_task(task_id):
+    try:
+        task_id = int(task_id)
+    except:
+        abort(make_response({'msg': f"invalid id {task_id}"}, 400))
+    
+    task = Task.query.get(task_id)
+    
+    return task if task else abort(make_response({'msg': f"No task with id {task_id}"}, 404))
+
+
 @tasks_bp.route("", methods=['POST'])
 def create_task():
     request_body = request.get_json()
