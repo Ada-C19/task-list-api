fbcafaf67cbb12ff9f325903ea0df82d56a7da85
Lindsay <lindsaybolzwilson@gmail.com>
2023-05-04 14:17:27 -0400

    Created tasks post route and registered blueprint in __INIT__ file

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,26 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, abort, make_response, request
+from app.models.task import Task
+from app import db
+
+
+task_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+# route for posting a task to db
+@task_bp.route("", methods=['POST'])
+def post_one_task():
+    request_body = request.get_json()
+    new_task = Task(title=request_body['title'],
+                    description=request_body['description'],
+                    completed_at=None)
+    
+    db.session.add(new_task)
+    db.session.commit()
+
+    return jsonify({
+        "task": {
+            "id": new_task.task_id,
+            "title": new_task.title,
+            "description": new_task.description,
+            "completed_at": new_task.completed_at
+            }
+        }), 201
