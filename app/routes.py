39bd2ec52cfd0da66bd4956660768e4a7169e6cf
Lindsay <lindsaybolzwilson@gmail.com>
2023-05-04 14:25:15 -0400

    created get all tasks route

--- app/routes.py
+++ app/routes.py
@@ -21,6 +21,20 @@ def post_one_task():
             "id": new_task.task_id,
             "title": new_task.title,
             "description": new_task.description,
-            "completed_at": new_task.completed_at
+            "completed_at": bool(new_task.completed_at)
             }
         }), 201
+
+# Get all tasks
+@task_bp.route("", methods=["GET"])
+def get_all_tasks():
+    tasks = Task.query.all()
+    tasks_response = []
+    for task in tasks:
+        tasks_response.append({
+            "id": task.task_id,
+            "title": task.title,
+            "description": task.description,
+            "completed_at": bool(task.completed_at)
+        })
+    return jsonify(tasks_response), 200
