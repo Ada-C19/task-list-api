57ab8060a5de6ba1d9cf303426e00dc7382bfe69
April Zhang <aprilzh23@gmail.com>
2023-05-04 15:30:00 -0700

    creates validate_task() helper function in task routes

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, abort, make_response
 from app.models.task import Task
 from app import db
 
@@ -9,7 +9,14 @@ tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 # def endpoint_name():
 #     my_beautiful_response_body = "Hello, World!"
 #     return my_beautiful_response_body
-
+def validate_task(task_id):
+    try:
+        task_id = int(task_id)
+    except:
+        abort(make_response({"msg": f"Invalid id '{task_id}'"}, 400))
+    task = Task.query.get(task_id)
+    return task if task else abort(make_response({"msg": f"Na task with task id '{task_id}'"}, 404))
+        
 @tasks_bp.route("", methods=["POST"])
 def create_a_task():
     
@@ -40,8 +47,7 @@ def get_saved_tasks():
     
 @tasks_bp.route("/<task_id>", methods=["GET"])
 def get_one_task(task_id):
-    task_id = int(task_id)
-    task = Task.query.get(task_id)
+    task = validate_task(task_id)
     return {
         "task": task.to_dict()
     }, 200
@@ -49,7 +55,7 @@ def get_one_task(task_id):
 @tasks_bp.route("/<task_id>", methods=["PUT"])
 def update_one_task(task_id):
     request_body = request.get_json()
-    task = Task.query.get(task_id)
+    task = validate_task(task_id)
     if "title" in request_body:
         task.title = request_body["title"]
     if "description" in request_body:
@@ -60,7 +66,7 @@ def update_one_task(task_id):
 
 @tasks_bp.route("/<task_id>", methods=["DELETE"])
 def delete_one_task(task_id):
-    task_to_delete = Task.query.get(task_id)
+    task_to_delete = validate_task(task_id)
     
     db.session.delete(task_to_delete)
     db.session.commit()
