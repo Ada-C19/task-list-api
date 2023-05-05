d0f3355638592c7ffe46cba898d07a7484510d19
Yulia S <startyupp@gmail.com>
2023-05-05 02:06:35 -0700

    Defined planned functions signatures that are to be implemented.Ran migration.

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,35 @@
-from flask import Blueprint
+from flask import Blueprint, abort, jsonify, make_response, request
+from app import db
+from app.models.task import Task
+
+tasks_bp = Blueprint('tasks', __name__, url_prefix='/tasks')
+
+def validate_task_id(task_id):
+    pass
+
+def validate_task_entry(request):
+    pass
+
+@tasks_bp.route('', methods=['POST'])
+def create_task():
+    pass
+
+@tasks_bp.route('', methods=['GET'])
+def get_tasks():
+    pass
+
+@tasks_bp.route('/<task_id>', methods=['GET'])
+def get_task_by_id(task_id):
+    pass
+
+@tasks_bp.route('/<task_id>', methods=['PUT'])
+def replace_task(task_id):
+    pass
+
+@tasks_bp.route('/<task_id>', methods=['PATCH'])
+def update_task(task_id):
+    pass
+
+@tasks_bp.route('/<task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    pass
