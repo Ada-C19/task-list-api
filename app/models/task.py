c7946d1eb14f1f25c3fcc0f6203193a9ee6b71b4
halimAremu <halimat3000@gmail.com>
2023-05-05 17:27:38 -0500

    wave 1 progress

--- app/models/task.py
+++ app/models/task.py
@@ -3,3 +3,15 @@ from app import db
 
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    completed_at = db.Column(db.DateTime)
+    
+    def to_dict(self):
+        task_dict = {}
+        task_dict['id'] = self.task_id
+        task_dict['title'] = self.title
+        task_dict['description'] = self.description
+        task_dict['is_complete'] = False
+        
+        return task_dict
--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,23 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, make_response, request
+from app.models.task import Task
+from app import db
+
+task_bp = Blueprint("tasks", __name__,url_prefix="/tasks")
+
+
+@task_bp.route('', methods=['POST'])
+def create_task():
+    request_body = request.get_json()
+    new_task = Task(title = request_body["title"], description = request_body["description"], completed_at = request_body["completed_at"])
+    db.session.add(new_task)
+    db.session.commit()
+    return make_response(jsonify(f"Task {new_task.title} successfully created", 201))
+
+@task_bp.route('', methods=['GET'])
+def get_all_tasks():
+    task_response = []
+    tasks = Task.query.all()
+    for task in tasks:
+        task_response.append(task.to_dict())
+    return jsonify(task_response)
+    
