d6cc3f6b97a43e0ff9d462d1882e607f51ce05ec
Sage Choi <coolcuds@gmail.com>
2023-05-05 13:12:26 -0700

    Refactored to_dict() to have is_complete instead of completed_at

--- app/models/task.py
+++ app/models/task.py
@@ -2,14 +2,19 @@ from app import db
 
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, nullable=False)
-    description = db.Column(db.String, nullable=False)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
     completed_at = db.Column(db.DateTime)
 
     def to_dict(self):
-        return {
+        task =  {
             "id": self.task_id,
             "title": self.title,
             "description": self.description,
-            "completed_at": self.completed_at
+            "is_complete": False
         }
+
+        if self.completed_at:
+            task["is_complete"] = True
+
+        return task
