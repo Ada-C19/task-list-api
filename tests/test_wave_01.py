db5a2e12275d99f16cc3f691d0b7c8fb9d0666e9
Amanda <jonesamanda996@gmail.com>
2023-05-05 16:51:33 -0400

    404 error test response_body changed from data to task

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -60,7 +60,7 @@ def test_get_task_not_found(client):
     # Assert
     assert response.status_code == 404
     assert response_body == {
-        "details": "Data id 1 not found"
+        "details": "Task id 1 not found"
         }
     # raise Exception("Complete test with assertion about response body")
     # *****************************************************************
@@ -133,7 +133,7 @@ def test_update_task_not_found(client):
     # Assert
     assert response.status_code == 404
     assert response_body == {
-        "details": "Data id 1 not found"
+        "details": "Task id 1 not found"
         }
     # raise Exception("Complete test with assertion about response body")
     # *****************************************************************
@@ -165,7 +165,7 @@ def test_delete_task_not_found(client):
     # Assert
     assert response.status_code == 404
     assert response_body == {
-        "details": "Data id 1 not found"
+        "details": "Task id 1 not found"
         }
     # raise Exception("Complete test with assertion about response body")
     # *****************************************************************
