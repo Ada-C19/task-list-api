e0e1d518f64de383937700c53da64b0671d55d3f
Lyuda Kim <lyudarkim@gmail.com>
2023-05-04 21:32:43 -0700

    Added an assert statement to test_delete_task_not_found() in tests/test_wave_01.py

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -144,7 +144,7 @@ def test_update_task_not_found(client):
     # *****************************************************************
 
 
-@pytest.mark.skip(reason="No way to test this feature yet")
+# @pytest.mark.skip(reason="No way to test this feature yet")
 def test_delete_task(client, one_task):
     # Act
     response = client.delete("/tasks/1")
@@ -159,7 +159,7 @@ def test_delete_task(client, one_task):
     assert Task.query.get(1) == None
 
 
-@pytest.mark.skip(reason="No way to test this feature yet")
+# @pytest.mark.skip(reason="No way to test this feature yet")
 def test_delete_task_not_found(client):
     # Act
     response = client.delete("/tasks/1")
@@ -167,8 +167,10 @@ def test_delete_task_not_found(client):
 
     # Assert
     assert response.status_code == 404
+    assert response_body == {
+        "msg": f"No task with id 1"
+    }
 
-    raise Exception("Complete test with assertion about response body")
     # *****************************************************************
     # **Complete test with assertion about response body***************
     # *****************************************************************
