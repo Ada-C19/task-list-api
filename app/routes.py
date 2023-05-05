c6e32d36e75600478af91e2c1c869a8d2503f061
liz <lissete.trejo@gmail.com>
2023-05-05 08:04:24 -0700

    add validate_model() helper function and refactor get_tak, update_task, and delete_task to use validate_model

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,21 @@ from flask import Blueprint, request, make_response, jsonify, abort
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 
+def validate_model(cls, model_id):
+    try:
+        model_id = int(model_id)
+    except:
+        message = f"{cls.__name__} {model_id} invalid"
+        abort(make_response({"error": message}, 400))
+
+    model = cls.query.get(model_id)
+
+    if not model:
+        message = f"{cls.__name__} #{model_id} not found"
+        abort(make_response({"error": message}, 404))
+
+    return model
+
 @tasks_bp.route("", methods=["POST"])
 def create_task():
     request_body = request.get_json()
@@ -21,19 +36,12 @@ def get_all_tasks():
 
 @tasks_bp.route("/<task_id>", methods=["GET"])
 def get_task(task_id):
-    task = Task.query.get(task_id)
-    if task is None:
-        message = f"Task #{task_id} not found"
-        return make_response({"error": message}, 404)
+    task = validate_model(Task, task_id)
     return make_response({"task": task.to_dict()}, 200)
 
 @tasks_bp.route("/<task_id>", methods=["PUT"])
 def update_task(task_id):
-    task = Task.query.get(task_id)
-    if task is None:
-        message = f"Task #{task_id} not found"
-        return make_response({"error": message}, 404)
-    
+    task = validate_model(Task, task_id)
     task_data = request.get_json()
     task.title = task_data["title"]
     task.description = task_data["description"]
@@ -43,10 +51,7 @@ def update_task(task_id):
 
 @tasks_bp.route("/<task_id>", methods=["DELETE"])
 def delete_task(task_id):
-    task = Task.query.get(task_id)
-    if task is None:
-        message = f"Task #{task_id} not found"
-        return make_response({"error": message}, 404)
+    task = validate_model(Task, task_id)
     db.session.delete(task)
     db.session.commit()
     return make_response({"details": f"Task {task.task_id} \"{task.title}\" successfully deleted"}, 200)
