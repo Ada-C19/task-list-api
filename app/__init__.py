cd4a50e9af0200f0d4e7b59b9432fe2152ef1bff
Yulia S <startyupp@gmail.com>
2023-05-04 23:12:43 -0700

    Replaced enviromental variables for db uri, minor pystyle changes.

--- app/__init__.py
+++ app/__init__.py
@@ -12,16 +12,14 @@ load_dotenv()
 
 def create_app(test_config=None):
     app = Flask(__name__)
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-    if test_config is None:
-        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-            "SQLALCHEMY_DATABASE_URI")
+    
+    if not test_config:
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DEV_DB_URI")
     else:
-        app.config["TESTING"] = True
         app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-            "SQLALCHEMY_TEST_DATABASE_URI")
-
+            "TEST_DB_URI")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    
     # Import models here for Alembic setup
     from app.models.task import Task
     from app.models.goal import Goal
