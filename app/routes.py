1de08a848acecb6de2e80ab83545b04e76617f09
liz <lissete.trejo@gmail.com>
2023-05-05 07:08:49 -0700

    create and register tasp_bp, add create_task() POST method, pass test_create_task test

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,14 @@
-from flask import Blueprint
+from app import db
+from app.models.task import Task
+from flask import Blueprint, request, make_response, jsonify, abort
+
+tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+@tasks_bp.route("", methods=["POST"])
+def create_task():
+    request_body = request.get_json()
+    new_task = Task.from_dict(request_body)
+    db.session.add(new_task)
+    db.session.commit()
+
+    return make_response({"task": new_task.to_dict()}, 201)
