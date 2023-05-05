2ed5c2bd923b925c78b0762241aec072585268e0
Amanda <jonesamanda996@gmail.com>
2023-05-05 16:26:44 -0400

    Patch Path, Query functionality

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify, abort, make_response, request
-from os import abort
 from app import db
 from app.models.task import Task
+from datetime import datetime
 
 tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
 
@@ -9,19 +9,22 @@ def validate_task(task_id):
     try: 
         task_id = int(task_id)
     except:
-        abort(make_response({"message": f"task {task_id} invalid"}, 400))
+        abort(make_response({"details": "Invalid data"}, 400))
     
     task = Task.query.get(task_id)
 
     if not task: 
-        abort(make_response({"message": f"task {task_id} not found"}, 404))
-
+        abort(make_response({"details": f"Data id {task_id} not found"}, 404))
+        
     return task 
 
 @tasks_bp.route("", methods=['POST'])
 def create_task():
     request_body = request.get_json()
-    print("******MY PRINT******",request_body.get("description"))
+
+    if not request_body.get("title") or not request_body.get("description"):
+        return jsonify({"details": "Invalid data"}), 400
+
     new_task = Task(
         title=request_body["title"],
         description=request_body["description"]
@@ -42,7 +45,15 @@ def create_task():
 @tasks_bp.route("", methods=['GET'])
 def read_all_tasks():
     task_response = []
-    tasks = Task.query.all()
+    sort_query = request.args.get("sort")
+
+    if sort_query == 'asc':
+        tasks = Task.query.order_by(Task.title.asc()).all()
+    elif sort_query == 'desc':
+        tasks = Task.query.order_by(Task.title.desc()).all()
+    else:
+        tasks = Task.query.all()
+
     for task in tasks:
         task_response.append({
             "id": task.task_id,
@@ -52,5 +63,61 @@ def read_all_tasks():
         })
     return jsonify(task_response), 200
 
+@tasks_bp.route("/<task_id>", methods=['GET'])
+def read_task_by_id(task_id):
+    task = validate_task(task_id)
+    return {
+        "task": {
+            "id": task.task_id,
+            "title": task.title,
+            "description": task.description, 
+            "is_complete": True if task.completed_at else False
+        }
+    }, 200
 
+@tasks_bp.route("/<task_id>", methods=['PUT'])
+def update_task(task_id):
+    task = validate_task(task_id)
+    request_body = request.get_json()
 
+    task.title = request_body["title"]
+    task.description = request_body["description"]
+
+    db.session.commit()
+
+    return {
+        "task": {
+            "id": task.task_id,
+            "title": task.title,
+            "description": task.description,
+            "is_complete": True if task.completed_at else False
+        }
+    }, 200
+
+@tasks_bp.route("/<task_id>/", methods=['PATCH'])
+def mark_complete_task(task_id):
+    task = validate_task(task_id)
+
+    if task.completed_at:
+        return make_response({"details":f"Task with id {task_id} is already completed"})
+
+    task.completed_at = datetime.utcnow()
+
+    db.session.commit()
+    return {
+        "task": {
+            "id": task.task_id,
+            "title": task.title,
+            "description": task.description,
+            "is_complete": True 
+        }
+    }, 200
+
+@tasks_bp.route("/<task_id>", methods=['DELETE'])
+def delete_task(task_id):
+    task = validate_task(task_id)
+
+    db.session.delete(task)
+    db.session.commit()
+    
+    return make_response({'details': f'Task {task.task_id} "{task.title}" successfully deleted'}, 200)
