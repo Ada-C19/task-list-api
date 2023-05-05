7e55de3c3773f9b01600100fba4a9ae9b4072aea
Jiaping Chen <jiapingchen17@gmail.com>
2023-05-04 19:32:58 -0700

    added routes for get_task for POST

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,16 @@
 from app import db
 
-
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement = True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    completed_at = db.Column(db.DateTime, default = None, nullable = True)
+    
+    def to_dict(self):
+        return {
+                "id":self.task_id,
+                "title":self.title,
+                "description": self.description,
+                "completed_at": self.completed_at,
+                "is_complete": self.completed_at is not None
+                }
--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,35 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request, make_response
+from app import db
+from app.models.task import Task
+
+
+task_bp = Blueprint("task", __name__,url_prefix = "/tasks")
+
+# create
+@task_bp.route("", methods = ["POST"])
+def get_task():
+    response_body = request.get_json()
+    if "title" not in response_body or "description" not in response_body:
+        return jsonify({ "details": "Invalid data"}), 400
+    
+    new_task = Task(
+        title = response_body["title"],
+        description = response_body["description"],
+        completed_at = response_body.get("completed_at", None)
+    )
+    db.session.add(new_task)
+    db.session.commit() 
+
+    return jsonify({"task":{
+            "id": new_task.task_id,
+            "title": new_task.title,
+            "description": new_task.description,
+            "is_complete": new_task.completed_at is not None
+    }
+    }), 201
+
+
+
+
+
+    
