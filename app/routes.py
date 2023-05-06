7f4f8d76522201dbdf6d830939cc265b99efff4d
Ana Villalobos <ana.villalobos309@gmail.com>
2023-05-05 19:12:45 -0700

    Created the POST method route inside the routes.py file

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,25 @@
-from flask import Blueprint
+from flask import Blueprint, request, make_response, request
+from app import db
+from app.models.task import Task
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 
+@tasks_bp.route("", methods=["POST"])
+def add_task():
+    request_body = request.get_json()
+    new_task = Task(
+            title= request_body["title"],
+            description= request_body['description'],
+            completed_at= request_body['completed_at']
+        )
+    
+    db.session.add(new_task)
+    db.session.commit()
+
+    return {
+        "task": {
+            "title": new_task.title,
+            "description": new_task.description,
+            "is_complete": new_task.completed_at
+        }
+    }
