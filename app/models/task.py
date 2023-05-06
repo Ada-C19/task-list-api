e9af3a7749548d522880cdf1df57a7588344622a
Yulia S <startyupp@gmail.com>
2023-05-05 21:38:06 -0700

    Created 2 class methods: to_dict and from_dict.

--- app/models/task.py
+++ app/models/task.py
@@ -11,3 +11,20 @@ class Task(db.Model):
 
 def self_attributes(self):
     return set('title', 'description')
+
+def to_dict(self):
+    return {
+        'title': self.title,
+        'description': self.description,
+        'completed_at': self.completed_at,
+        'is_completed': self.is_completed
+    }
+    
+@classmethod
+def from_dict(cls, request_body):
+    task = cls(
+            title=request_body['title'],
+            description=request_body['description'],
+            completed_at=request_body['completed_at'],
+            is_complete=request_body['is_complete'])
+    return task
