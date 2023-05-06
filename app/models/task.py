6878b4f48beab35f9d0764d41959726919e4b72a
Yulia S <startyupp@gmail.com>
2023-05-06 01:45:37 -0700

    Added id to to_dict function. Refactored from_dict function.

--- app/models/task.py
+++ app/models/task.py
@@ -1,6 +1,5 @@
 from app import db
 
-
 class Task(db.Model):
     __tablename__ = 'tasks'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -11,29 +10,26 @@ class Task(db.Model):
 
     def to_dict(self):
         return {
+            'id': self.id,
             'title': self.title,
             'description': self.description,
             'completed_at': self.completed_at,
             'is_complete': self.is_complete
         }
+    
     @classmethod
     def get_attributes(self):
         return 'title', 'description'
     
     @classmethod
     def from_dict(cls, request_body):
-        task = cls(
-                title=request_body['title'],
-                description=request_body['description'])
-                # completed_at=request_body['completed_at'],
-                # is_complete=request_body['is_complete'])
+        if  not 'is_complete' in request_body:
+            task = cls(
+                    title=request_body['title'],
+                    description=request_body['description'])
+        else:
+            task = cls(
+                    title=request_body['title'],
+                    description=request_body['description'],
+                    is_complete=request_body['is_complete'])
         return task
-    # if not 'is_complete' in request_body:
-    #         task = cls(
-    #                 title=request_body['title'],
-    #                 description=request_body['description'])
-    #                 # completed_at=request_body['completed_at'],
-    #                 # is_complete=request_body['is_complete'])
-    #     else:
-    #         task = 
-    #         return task
