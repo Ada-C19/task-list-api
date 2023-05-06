63de10c2c6b69653630e1acdc1162e1450871200
Lyuda Kim <lyudarkim@gmail.com>
2023-05-06 15:36:37 -0700

    Set nullable=True in goal_id column in Task model

--- app/models/task.py
+++ app/models/task.py
@@ -6,7 +6,7 @@ class Task(db.Model):
     title = db.Column(db.String(80))
     description = db.Column(db.String(300))
     completed_at = db.Column(db.DateTime, nullable=True)
-    goal_id = db.Column(db.Integer, db.ForeignKey('goal.goal_id'))
+    goal_id = db.Column(db.Integer, db.ForeignKey('goal.goal_id'), nullable=True)
     goal = db.relationship("Goal", back_populates="tasks")
 
 
