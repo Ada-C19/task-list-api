d0ade9610a8d06d645115fa24fad28592af2ae9f
Winslow <owedwards@gmail.com>
2023-05-05 20:35:27 -0400

    Test update task pass

--- app/routes.py
+++ app/routes.py
@@ -91,7 +91,6 @@ def update_task(task_id):
 
     task.title = request_body["title"]
     task.description = request_body["description"]
-    task.completed_at = request_body["completed_at"]
 
     db.session.commit()
 
