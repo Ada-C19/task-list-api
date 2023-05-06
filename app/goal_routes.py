24aaf9a774add18ad99e8226864f3e404674fbe7
Lyuda Kim <lyudarkim@gmail.com>
2023-05-06 13:01:34 -0700

    Renamed validate_goal helper f in goal_routes.py

--- app/goal_routes.py
+++ app/goal_routes.py
@@ -7,7 +7,7 @@ goals_bp = Blueprint("goals", __name__, url_prefix="/goals")
 
 
 # Helper function
-def validate_goal(goal_id):
+def get_valid_goal_by_id(goal_id):
     try:
         goal_id = int(goal_id)
     except:
@@ -55,7 +55,7 @@ def handle_goals():
 
 @goals_bp.route("/<goal_id>", methods=['GET'])
 def handle_goal(goal_id):
-    goal = validate_goal(goal_id)
+    goal = get_valid_goal_by_id(goal_id)
     return {"goal": goal.to_dict()}, 200
 
 
@@ -63,7 +63,7 @@ def handle_goal(goal_id):
 def update_one_goal(goal_id):
     request_body = request.get_json()
 
-    goal_to_update = validate_goal(goal_id)
+    goal_to_update = get_valid_goal_by_id(goal_id)
     goal_to_update.title = request_body["title"]
 
     db.session.commit()
@@ -73,7 +73,7 @@ def update_one_goal(goal_id):
 
 @goals_bp.route("/<goal_id>", methods=['DELETE'])
 def delete_one_goal(goal_id):
-    goal_to_delete = validate_goal(goal_id)
+    goal_to_delete = get_valid_goal_by_id(goal_id)
 
     db.session.delete(goal_to_delete)
     db.session.commit()
