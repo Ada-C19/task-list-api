ccc020a8ceda4f63e712d45faa4222db4420f723
Winslow <owedwards@gmail.com>
2023-05-05 20:57:57 -0400

    test_delete_task_not_found pass

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -138,7 +138,6 @@ def test_delete_task(client, one_task):
     assert Task.query.get(1) == None
 
 
-@pytest.mark.skip(reason="No way to test this feature yet")
 def test_delete_task_not_found(client):
     # Act
     response = client.delete("/tasks/1")
@@ -146,11 +145,7 @@ def test_delete_task_not_found(client):
 
     # Assert
     assert response.status_code == 404
-
-    raise Exception("Complete test with assertion about response body")
-    # *****************************************************************
-    # **Complete test with assertion about response body***************
-    # *****************************************************************
+    assert response_body == {"message": f"Sorry, task 1 does not exist"}
 
     assert Task.query.all() == []
 
