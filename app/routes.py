46689b7851d5b5939c65ef544f5a2adc758375e0
Sophia Tran <sophiat8832@gmail.com>
2023-05-06 16:00:14 -0700

    completed test_get_task_not_found and deleted import statement in routes

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify, abort, make_response, request
 from app.models.task import Task
 from app import db
-from os import abort
+# from os import abort
 
 
 tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
