44c0177695480c405a7afc63b020812be33bb174
liz <lissete.trejo@gmail.com>
2023-05-05 19:56:37 -0700

    update Goal and Task models to create one to many relationship between Goal and Tasks. confirm all wave 1-5 tests still pass

--- app/models/task.py
+++ app/models/task.py
@@ -5,13 +5,15 @@ class Task(db.Model):
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable=True)
+    goal = db.relationship("Goal", back_populates="tasks")  
+    goal_id = db.Column(db.Integer, db.ForeignKey('goal.id'), nullable=True)
 
     def to_dict(self):
         return {
             "id": self.id,
             "title": self.title,
             "description": self.description,
-            "is_complete": bool(self.completed_at)
+            "is_complete": bool(self.completed_at),
         }
     
     @classmethod
