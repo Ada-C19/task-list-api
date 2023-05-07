5472f34208916a1ab7f81af633b74b9c299f0574
Ana Silvera <anasilverx@gmail.com>
2023-05-06 19:24:35 -0700

    added attributes to Task Model. initialized flask.

--- app/models/task.py
+++ app/models/task.py
@@ -2,4 +2,7 @@ from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(80))
+    description = db.Column(db.String(255))
+    completed_at = db.Column(db.DateTime, nullable=True)
