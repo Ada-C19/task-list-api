43fa4090a5e496759c5b7589ba03bfeb22cadca9
Muniba <mbashirkimes@gmail.com>
2023-05-07 16:28:40 -0400

    Adds create_task route and read all tasks route

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,26 @@
 from app import db
-
+# from sqlalchemy import Column, Integer, DateTime
+# from sqlalchemy.sql import func
 
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    completed_at = db.Column(db.DateTime, nullable=True)
+    
+
+    def to_dict(self):
+        goal_as_dict = {}
+        goal_as_dict["task_id"] = self.task_id
+        goal_as_dict["title"] = self.title
+        goal_as_dict["description"] = self.description
+        goal_as_dict["completed_at"] = self.completed_at
+
+        return goal_as_dict
+
+    @classmethod
+    def from_dict(cls, goal_data):
+        new_goal = Goal(title=goal_data["title"],
+                        description=goal_data["description"]
+                        )
+        return new_goal
--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,44 @@
-from flask import Blueprint
+from app import db
+from flask import Blueprint, jsonify, abort, make_response, request
+from app.models.task import Task
+from app.models.goal import Goal
+
+tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
+goals_bp = Blueprint("goals_bp", __name__, url_prefix="/goals")
+
+def validate_model(cls, model_id):
+    try:
+        model_id = int(model_id)
+    except:
+        abort(make_response({"message":f"{cls.__name__} {model_id} invalid"}, 400))
+
+    model = cls.query.get(model_id)
+
+    if not model:
+        abort(make_response({"message":f"{cls.__name__} {model_id} not found"}, 404))
+
+    return model
+
+@tasks_bp.route("", methods=["POST"])
+def create_task():
+    request_body = request.get_json()
+    new_task = Task.from_dict(request_body)
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return make_response(jsonify(f"Book {new_task.title} successfully created"), 201)
+
+@tasks_bp.route("", methods=["GET"])
+def read_all_tasks():
+    
+    title_query = request.args.get("title")
+    if title_query:
+        tasks = Task.query.filter_by(title=title_query)
+    else:
+        tasks = Task.query.all()
+
+    tasks_response = []
+    for task in tasks:
+        tasks_response.append(task.to_dict())
+    return jsonify(tasks_response)
