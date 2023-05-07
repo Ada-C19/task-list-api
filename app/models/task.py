94fcb215ca5e2a8dc9e37be2ef852037df6bffdb
Nadia Mirzai <nadia.m.mirzai@gmail.com>
2023-05-07 14:08:59 -0400

    added methods from_dict and to_dict to the task model. added a route to create a task in the database

--- app/models/task.py
+++ app/models/task.py
@@ -6,3 +6,15 @@ class Task(db.Model):
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable=True)
+
+    @classmethod
+    def from_dict(cls, dict_data):
+        return cls(title = dict_data["title"],
+                   description = dict_data["description"] #completed_at = dict_data["completed_at"]
+                   )
+    
+    def to_dict(task):
+        return dict(id=task.id,
+                    title=task.title,
+                    description=task.description,
+                    completed_at=task.completed_at)
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,21 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, make_response
+from .models.task import Task
+from app import db
+
+task_bp = Blueprint("task", __name__, url_prefix="/tasks")
 
 
+# Create task:
+@task_bp.route("", methods=["POST"])
+def create_task():
+    request_body = request.get_json()
+    task = Task.from_dict(request_body)
+    db.session.add(task)
+    db.session.commit()
+    response = Task.query.all()
+    response_body ={"task":{"id":response[0].task_id,
+                            "title":request_body["title"],
+                            "description":request_body["description"],
+                            "is_complete": False}} #request_body["completed_at"]
+    return make_response(response_body, 201)
 
