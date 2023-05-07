b9433ac3b5974454d611d7346397bebbed3d7541
Yulia S <startyupp@gmail.com>
2023-05-06 18:01:36 -0700

    Added functions body for PUT, PATCH and DELETE methods.

--- app/routes.py
+++ app/routes.py
@@ -16,7 +16,7 @@ def validate_id(model, id):
 def validate_entry(model, request_body):
     for atr in model.get_attributes():
         if atr not in request_body:
-            abort(make_response(f'Invalid Request. {model.__name__} {atr} missing', 400))
+            abort(make_response({'Invalid Request': f'Missing {model.__name__} {atr}'}, 400))
     return request_body
 
 def create_response(entity):
@@ -59,12 +59,41 @@ def get_task_by_id(task_id):
 
 @tasks_bp.route('/<task_id>', methods=['PUT'])
 def replace_task(task_id):
-    pass
+    task = validate_id(Task, task_id)
+    
+    request_body = request.get_json()
+    valid_request = validate_entry(Task, request_body)
+    
+    task.title = valid_request['title']
+    task.description = valid_request['description']
+    if 'completed_at' in valid_request:
+        task.completed_at = valid_request['completed_at']
+        task.is_complete = True
+    
+    db.session.commit()
+    
+    return {'task': create_response(task)}, 200
 
 @tasks_bp.route('/<task_id>', methods=['PATCH'])
 def update_task(task_id):
-    pass
+    task = validate_id(Task, task_id)
+    
+    request_body = request.get_json()
+
+    task.title = request_body.get('title', task.title)
+    task.description = request_body.get('description', task.description)
+    if 'completed_at' in request_body:
+        task.completed_at = request_body['completed_at']
+        task.is_complete = True
+    
+    db.session.commit()
+    
+    return {'task': create_response(task)}, 200
 
 @tasks_bp.route('/<task_id>', methods=['DELETE'])
 def delete_task(task_id):
-    pass
+    task = validate_id(Task, task_id)
+    task_title = task.title
+    db.session.delete(task)
+    db.session.commit()
+    return {'details': f"Task {task_id} '{task_title}' successfully deleted"}, 200
