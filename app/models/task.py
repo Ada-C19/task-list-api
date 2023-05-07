2180655c8614dee613f765a86e3a3490ce14edc9
Ana Silvera <anasilverx@gmail.com>
2023-05-07 16:21:07 -0700

    implemented route that creates a new task. added class method from_dict to model

--- app/models/task.py
+++ app/models/task.py
@@ -6,3 +6,13 @@ class Task(db.Model):
     title = db.Column(db.String(80))
     description = db.Column(db.String(255))
     completed_at = db.Column(db.DateTime, nullable=True)
+
+    @classmethod
+    def from_dict(cls, task_details):
+        new_task = cls(
+            title=task_details["title"],
+            description=task_details["description"],
+            completed_at=task_details["completed_at"]
+        )
+
+        return new_task
--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,23 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from app.models.task import Task
+from app import db
+
+tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
+
+@tasks_bp.route("", methods=["POST"])
+def create_task():
+    request_body = request.get_json()
+    print(request_body)
+    new_task = Task.from_dict(request_body)
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return {
+        "task":{
+            "id":new_task.task_id,
+            "title":new_task.title,
+            "description":new_task.description,
+            "is_complete": False
+        }
+    }, 201
