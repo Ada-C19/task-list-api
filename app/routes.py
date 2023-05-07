be614ce9346eb05bd6c8dfea9cf9ed1ee3b627b4
Janice <janicenguyenn@gmail.com>
2023-05-06 23:01:12 -0700

    created task Blueprint, POST route and create_task function

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,23 @@
-from flask import Blueprint
+from app import db
+from app.models.task import Task
+from flask import Blueprint, jsonify, request
+
+tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+@tasks_bp.route("", methods=['POST'])
+def create_task():
+    request_body = request.get_json()
+    
+    new_task = Task(title=request_body["title"], description=request_body["description"], completed_at=request_body["completed_at"])
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return {
+        "task": {
+        "id": new_task.task_id,
+        "title": new_task.title,
+        "description": new_task.description,
+        "is_complete": False
+        }
+    }, 201
