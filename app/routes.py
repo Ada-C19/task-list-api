6a850b55b18568b6d693db555470692cd7649fbd
Susi <susi.franco32@gmail.com>
2023-05-06 22:41:02 -0700

    updated routes to pass wave 1 tests

--- app/routes.py
+++ app/routes.py
@@ -15,7 +15,7 @@ def validate_task(cls, task_id):
     
     task = Task.query.get(task_id)
     if task is None:
-        abort(make_response({"msg": f"task id: {task_id} not found."}, 404))
+        abort(make_response({"msg": "Task not found."}, 404))
 
     return task 
 
@@ -24,21 +24,21 @@ def validate_task(cls, task_id):
 @tasks_bp.route("", methods=["GET"])
 def get_all_tasks():
     
-    title_query = request.args.get("name")
+    title_query = request.args.get("title")
 
     if title_query:
-        planets = Task.query.filter_by(name=title_query)
+        tasks = Task.query.filter_by(title=title_query)
     #grab all info from the instance task table
     else:
-        planets = Task.query.all()
+        tasks = Task.query.all()
         
-    task_dict = [task.to_dict() for task in planets]
+    task_dict = [task.to_dict() for task in tasks]
     return jsonify(task_dict), 200
 
 @tasks_bp.route("/<task_id>", methods=["GET"])
 def single_task(task_id):
     task = validate_task(Task,task_id)
-    return jsonify(task.to_dict()), 200
+    return jsonify({"task": task.to_dict()}), 200
 
 @tasks_bp.route("/<task_id>", methods=["PUT"])
 def update_task(task_id):
@@ -60,21 +60,26 @@ def delete_single_task(task_id):
     db.session.delete(task)    
     db.session.commit()
 
-    return f"Task at id:{task_id} was sucessfully deleted", 200
+    success_message = f"Task {task.task_id} \"{task.title}\" successfully deleted"
+    return jsonify({"details": success_message}), 200
+
 
 @tasks_bp.route("", methods=['POST'])
 def create_task():
     request_body = request.get_json()
-    new_task = Task(title=request_body["title"],
-                    description=request_body["description"], 
-                    completed_at=request_body["completed_at"])
-    
+    title = request_body.get("title")
+    description = request_body.get("description")
+
+    if not description or not title:
+        return {"details": "Invalid data"}, 400
+
+    new_task = Task(title=title, description=description)
     db.session.add(new_task)
     db.session.commit()
 
-    return ({"task": {
+    return {"task": {
         "id": new_task.task_id,
         "title": new_task.title,
         "description": new_task.description,
-        "is_complete": (new_task.completed_at != None)}
-        }, 201)
+        "is_complete": new_task.completed_at is not None}
+    }, 201
