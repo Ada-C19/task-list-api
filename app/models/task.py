aee379b984da17226a4ca93eea9c882cbd74b012
Sydney Alberson <sydney.alberson@gmail.com>
2023-05-07 13:17:47 -0400

    Created get task by id function and helper function to validate id

--- app/models/task.py
+++ app/models/task.py
@@ -2,7 +2,7 @@ from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(32))
     description = db.Column(db.String(120))
     completed_at = db.Column(db.DateTime, nullable=True)
--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,60 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify, make_response, abort
 from app import db
+from app.models.task import Task
 
-task_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+
+# # Route to create a task
+# @tasks_bp.route("", methods=["POST"])
+
+# def create_task():
+#     request_body = request.get_json()
+    
+#     new_task = Task(
+#         title=request_body["title"],
+#         description=request_body["description"]
+#     )
 
 # Route to get tasks
-@task_bp.route("", methods=["GET"])
-
-def read_all_tasks():
-    tasks_response = []
+@tasks_bp.route("", methods=["GET"])
+def get_all_tasks():
 
     tasks = Task.query.all()
 
+    tasks_response = []
     for task in tasks:
-        tasks_reponse.append({
+        tasks_response.append({
+            "id": task.id,
             "title": task.title,
             "description": task.description,
+            "is_complete": False
         })
 
-    return tasks_response
+    return jsonify(tasks_response), 200
+
+
+# Validate ID function
+def validate_id(task_id):
+    task = Task.query.get(task_id)
+
+    if not task:
+        return abort(make_response({"message": f"Task with ID {task_id} not found."}, 404))
+
+    return task
+
+@tasks_bp.route("/<task_id>", methods=["GET"])
+def get_task_by_id(task_id):
+
+    task = validate_id(task_id)
+
+    return {"task": {
+            "id": task.id,
+            "title": task.title,
+            "description": task.description,
+            "is_complete": False
+        }}, 200
+
+@tasks_bp.route("", methods=["POST"])
+def create_task():
+    pass
