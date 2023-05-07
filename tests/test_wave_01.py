8bae664cca0b0de630c45ddfa163fc99bef92b23
Jessica <Yiskah.Scientia@gmail.com>
2023-05-06 19:47:57 -0600

    Wave 1 finish tests

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -60,11 +60,11 @@ def test_get_task_not_found(client):
     # Assert
     assert response.status_code == 404
 
-    raise Exception("Complete test with assertion about response body")
+    # raise Exception("Complete test with assertion about response body")
     # *****************************************************************
     # **Complete test with assertion about response body***************
     # *****************************************************************
-
+    assert response_body == {'message': 'Task 1 was not found.'}
 
 # @pytest.mark.skip(reason="No way to test this feature yet")
 def test_create_task(client):
@@ -131,11 +131,11 @@ def test_update_task_not_found(client):
     # Assert
     assert response.status_code == 404
 
-    raise Exception("Complete test with assertion about response body")
+    # raise Exception("Complete test with assertion about response body")
     # *****************************************************************
     # **Complete test with assertion about response body***************
     # *****************************************************************
-
+    assert response_body == {'message': 'Task 1 was not found.'}
 
 # @pytest.mark.skip(reason="No way to test this feature yet")
 def test_delete_task(client, one_task):
@@ -160,14 +160,13 @@ def test_delete_task_not_found(client):
 
     # Assert
     assert response.status_code == 404
+    assert Task.query.all() == []
 
-    raise Exception("Complete test with assertion about response body")
+    # raise Exception("Complete test with assertion about response body")
     # *****************************************************************
     # **Complete test with assertion about response body***************
     # *****************************************************************
-
-    assert Task.query.all() == []
-
+    assert response_body == {'message': 'Task 1 was not found.'}
 
 # @pytest.mark.skip(reason="No way to test this feature yet")
 def test_create_task_must_contain_title(client):
@@ -201,3 +200,49 @@ def test_create_task_must_contain_description(client):
         "details": "Invalid data"
     }
     assert Task.query.all() == []
+
+
+
+
+
+
+
+def test_get_invalid_task_returns_400(client):
+    response = client.get("/tasks/mystery")
+
+    response_body = response.get_json()
+
+    assert response.status_code == 400
+    assert response_body == {'message': 'Task mystery is invalid.'}
+
+def test_update_task_not_found(client):
+    # Act
+    response = client.put("/tasks/mystery", json={
+        "title": "Updated Task Title",
+        "description": "Updated Test Description",
+    })
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 400
+    assert response_body == {'message': 'Task mystery is invalid.'}
+
+def test_delete_task_not_found(client):
+    # Act
+    response = client.delete("/tasks/mystery")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 400
+    assert Task.query.all() == []
+    assert response_body == {'message': 'Task mystery is invalid.'}
+
+def test_get_all_tasks_returns_array_of_tasks_and_200(client, three_tasks):
+    response = client.get("/tasks")
+    response_body = response.get_json()
+
+    assert response.status_code == 200
+    assert len(response_body) == 3
+    assert response_body == [{"title": "Water the garden 🌷", "description": "", "completed_at": None},
+                            {"title": "Answer forgotten email 📧", "description": "", "completed_at": None},
+                            {"title": "Pay my outstanding tickets 😭", "description": "", "completed_at": None},]
