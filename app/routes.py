348780e47a029a95c286b34d3618c367cda1e9f0
Katherine Guarnizo <guarnizo.katherine.5@gmail.com>
2023-05-07 12:23:52 -0400

    Complete set up and task Model

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint,jsonify, request
+from app import db
+
+tasks_bp = Blueprint("books", __name__, url_prefix="/tasks")
+
+def create_task():
+    request_body = request.get_json()
+
+    new_task = Task()
