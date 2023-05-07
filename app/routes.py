3d7de9b36dd93f7e6a2fc50f05927d690926a1f1
Sage Choi <coolcuds@gmail.com>
2023-05-06 18:43:34 -0700

    Added PUT route for updating a task specifed by ID

--- app/routes.py
+++ app/routes.py
@@ -4,39 +4,8 @@ from flask import Blueprint, jsonify, make_response, request, abort
 
 tasks_bp = Blueprint("task", __name__, url_prefix="/tasks")
 
-
-@tasks_bp.route("", methods=["POST"])
-def create_task():
-    request_body = request.get_json()
-
-    if "title" not in request_body or "description" not in request_body:
-        return {"details": f"Invalid data"}, 400
-
-    new_task = Task(
-        title=request_body["title"],
-        description=request_body["description"]
-    )
-
-    db.session.add(new_task)
-    db.session.commit()
-
-    return {"task": new_task.to_dict()}, 201
-
-
-@tasks_bp.route("", methods=["GET"])
-def get_all_tasks():
-    task_query = request.args.get("title")
-
-    # Store all tasks, or a task specified by title
-    all_tasks = Task.query.all() if not task_query else Task.query.filter_by(title=task_query)
-
-    # Add each task in all_tasks to response as a dictionary
-    response = [task.to_dict() for task in all_tasks]
-
-    return jsonify(response), 200
-
-
 def validate_task(task_id):
+    """Validate that task exists in database before using route."""
     try:
         task_id = int(task_id)
     except:
@@ -49,8 +18,57 @@ def validate_task(task_id):
     return task
 
 
+@tasks_bp.route("", methods=["POST"])
+def create_task():
+    """Create and add task to database."""
+    request_body = request.get_json()
+
+    if "title" not in request_body or "description" not in request_body:
+        return {"details": f"Invalid data"}, 400
+
+    new_task = Task(
+        title = request_body["title"],
+        description = request_body["description"]
+    )
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return {"task": new_task.to_dict()}, 201
+
+
+@tasks_bp.route("", methods=["GET"])
+def get_all_tasks():
+    """Get all tasks or a task by title."""
+    task_query = request.args.get("title")
+
+    # Retrieve all tasks, or a task specified by title
+    all_tasks = Task.query.all() if not task_query else Task.query.filter_by(title=task_query)
+
+    # Add each task in all_tasks to response as a dictionary
+    response = [task.to_dict() for task in all_tasks]
+
+    return jsonify(response), 200
+
+
 @tasks_bp.route("/<task_id>", methods=["GET"])
 def get_one_task(task_id):
+    """Get one task by id."""
+    return {"task": validate_task(task_id).to_dict()}, 200
+
+
+@tasks_bp.route("/<task_id>", methods=["PUT"])
+def update_task(task_id):
+    """Updates task specifed by id."""
     task = validate_task(task_id)
 
+    request_body = request.get_json()
+    task.title = request_body["title"]
+    task.description = request_body["description"]
+
+    db.session.commit()
+
     return {"task": task.to_dict()}, 200
+
+
+# TODO: DELETE route
