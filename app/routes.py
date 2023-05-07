ac253ae7eae39cedc9dfee19df254f178b9940e7
Ana Villalobos <ana.villalobos309@gmail.com>
2023-05-06 22:24:23 -0700

    Updated the POST request to /tasks to handle missing data

--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,11 @@ tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 @tasks_bp.route("", methods=["POST"])
 def add_task():
     request_body = request.get_json()
+    if len(request_body) != 3:
+        return make_response({"details": "Invalid data"}, 400)
+    #
+    # if not request_body["title"]:
+    #     return make_response({"details": "Invalid data"}, 400)
     new_task = Task(
             title= request_body["title"],
             description= request_body['description'],
