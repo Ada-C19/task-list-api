95e8547037c7dacbc8d2b882df2506a733fc3050
Susi <susi.franco32@gmail.com>
2023-05-08 16:03:51 -0700

    added slack integration, import slack_sdk

--- app/routes.py
+++ app/routes.py
@@ -2,13 +2,16 @@ from flask import Blueprint, jsonify, abort, make_response, request
 from app.models.task import Task
 from app import db
 from datetime import datetime
+import os
+import requests
+from slack_sdk import WebClient
 
 
 
 
 tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
 
-def validate_task(cls, task_id):
+def validate_object(cls, task_id):
     #handle invalid task id, return 400
     try:
         task_id = int(task_id)
@@ -41,12 +44,12 @@ def get_all_tasks():
 
 @tasks_bp.route("/<task_id>", methods=["GET"])
 def single_task(task_id):
-    task = validate_task(Task,task_id)
+    task = validate_object(Task,task_id)
     return jsonify({"task": task.to_dict()}), 200
 
 @tasks_bp.route("/<task_id>", methods=["PUT"])
 def update_task(task_id):
-    task = validate_task(Task, task_id)
+    task = validate_object(Task, task_id)
 
     request_body = request.get_json()
 
@@ -59,7 +62,7 @@ def update_task(task_id):
 
 @tasks_bp.route("/<task_id>", methods=["DELETE"])
 def delete_single_task(task_id):
-    task = validate_task(Task, task_id)
+    task = validate_object(Task, task_id)
 
     db.session.delete(task)    
     db.session.commit()
@@ -90,17 +93,24 @@ def create_task():
 
 @tasks_bp.route("/<task_id>/mark_complete", methods=["PATCH"])
 def mark_task_complete(task_id):
-    task = validate_task(Task, task_id)
+    task = validate_object(Task, task_id)
     if task.completed_at is not None:
         # The task is already complete
         return {"task": task.to_dict()}, 200
     task.completed_at = datetime.utcnow()
     db.session.commit()
+
+    #make a request to slack, post to slack channel task-notifications  
+    slack_token= os.environ["SLACK_TOKEN"]
+    client = WebClient(token=slack_token)
+    result = client.chat_postMessage(channel="task-notifications", 
+        text=f"Someone just completed the task {task.title}")
+
     return {"task": task.to_dict()}, 200
 
 @tasks_bp.route("/<task_id>/mark_incomplete", methods=["PATCH"])
 def mark_task_incomplete(task_id):
-    task = validate_task(Task, task_id)
+    task = validate_object(Task, task_id)
     if task.completed_at is None:
         # The task is already incomplete
         return {"task": task.to_dict()}, 200
