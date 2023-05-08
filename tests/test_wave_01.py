426efd3638b5dc1dbdc1ffcce7b088ba06ff1fc1
mlhuynh <greenlinh@gmail.com>
2023-05-08 05:55:22 -0700

    Completed test_update_task_not_found assertion in wave_01 module.

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -131,8 +131,9 @@ def test_update_task_not_found(client):
 
     # Assert
     assert response.status_code == 404
+    assert response_body == {f"message": "Task 1 not found"}
 
-    raise Exception("Complete test with assertion about response body")
+    # raise Exception("Complete test with assertion about response body")
     # *****************************************************************
     # **Complete test with assertion about response body***************
     # *****************************************************************
