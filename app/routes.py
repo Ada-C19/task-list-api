94712f36ffe9d821319de964aeba6a4f39c2c25a
Janice <janicenguyenn@gmail.com>
2023-05-07 20:38:00 -0700

    implemented DELETE route to delete one task and created delete_task function

--- app/routes.py
+++ app/routes.py
@@ -85,3 +85,16 @@ def update_task(task_id):
             "is_complete": False
         }
     }, 200
+
+
+@tasks_bp.route("/<task_id>", methods=["DELETE"])
+def delete_task(task_id):
+    task = validate_task(task_id)
+
+    db.session.delete(task)
+    db.session.commit()
+
+    return{
+        "details": f"Task {task_id} \"{task.title}\" successfully deleted"
+    }, 200
+
