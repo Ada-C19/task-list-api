c7ddd1008c9f46c699825ec9ae2398aa93d9ef07
Airis Wang <airis.wang2014@fgvmail.br>
2023-05-08 12:21:24 -0400

    fixed Task model

--- app/models/task.py
+++ app/models/task.py
@@ -1,11 +1,11 @@
 from app import db
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, nullable=True)
-    description = db.Column(db.String, nullable=True)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
     completed_at = db.Column(db.DateTime, nullable=True)
-    goal_id = db.Column(db.Integer, db.ForeignKey("goal.goal_id"))
+    goal_id = db.Column(db.Integer, db.ForeignKey("goal.goal_id"), nullable=True)
     goal = db.relationship("Goal", back_populates="tasks")
 
     def to_dict(self):
