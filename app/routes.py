c200858af7901cf0f5674281fb25527f335b3063
Sage Choi <coolcuds@gmail.com>
2023-05-08 14:23:34 -0700

    Added message for bot to send on Slack when task marked complete in PATCH route

--- app/routes.py
+++ app/routes.py
@@ -1,19 +1,22 @@
 from app import db
 from app.models.task import Task
 from flask import Blueprint, jsonify, make_response, request, abort
-import copy
 import datetime
+# from slack_sdk import WebClient
+# from slack_sdk.errors import SlackApiError
+import requests
+import os
 
 tasks_bp = Blueprint("task", __name__, url_prefix="/tasks")
 
-def validate_task(task_id):
+def validate_model(model, task_id):
     """Validate that task exists in database before using route."""
     try:
         task_id = int(task_id)
     except:
         abort(make_response({"message": f"Task '{task_id}' invalid"}, 400))
 
-    task = Task.query.get(task_id)
+    task = model.query.get(task_id)
     if not task:
         abort(make_response({"message": f"Task '{task_id}' not found"}, 404))
 
@@ -38,6 +41,8 @@ def create_task():
 
     return {"task": new_task.to_dict()}, 201
 
+@tasks_bp.route("")
+
 
 @tasks_bp.route("", methods=["GET"])
 def get_all_tasks():
@@ -60,13 +65,13 @@ def get_all_tasks():
 @tasks_bp.route("/<task_id>", methods=["GET"])
 def get_one_task(task_id):
     """Get one task by id."""
-    return {"task": validate_task(task_id).to_dict()}, 200
+    return {"task": validate_model(Task, task_id).to_dict()}, 200
 
 
 @tasks_bp.route("/<task_id>", methods=["PUT"])
 def update_task(task_id):
     """Updates task specifed by id."""
-    task = validate_task(task_id)
+    task = validate_model(Task, task_id)
 
     request_body = request.get_json()
     task.title = request_body["title"]
@@ -76,22 +81,32 @@ def update_task(task_id):
 
     return {"task": task.to_dict()}, 200
 
+
 @tasks_bp.route("/<task_id>/mark_complete", methods=["PATCH"])
 def mark_task_as_complete(task_id):
-    """Mark task specifed by id as complete."""
-    task = validate_task(task_id)
+    """Mark task specifed by id as completed and send bot message on Slack."""
+    task = validate_model(Task, task_id)
 
     task.completed_at = datetime.datetime.now()
     task.is_complete = True
 
     db.session.commit()
 
+    bot_data = {
+        "token": os.environ.get("SLACKBOT_API_KEY"),
+        "channel": "task-notifications",
+        "text": f"'{task.title}' has just been completed! 🥳"
+    }
+
+    response = requests.post(url="https://slack.com/api/chat.postMessage", data=bot_data)
+
     return {"task": task.to_dict()}, 200
+
 
 @tasks_bp.route("/<task_id>/mark_incomplete", methods=["PATCH"])
 def mark_task_as_incomplete(task_id):
     """Mark task specifed by id as incomplete."""
-    task = validate_task(task_id)
+    task = validate_model(Task, task_id)
 
     task.completed_at = None
     task.is_complete = False
@@ -104,7 +119,7 @@ def mark_task_as_incomplete(task_id):
 @tasks_bp.route("/<task_id>", methods=["DELETE"])
 def delete_task(task_id):
     """Delete task specifed by id."""
-    task = validate_task(task_id)
+    task = validate_model(Task, task_id)
 
     db.session.delete(task)
     db.session.commit()
