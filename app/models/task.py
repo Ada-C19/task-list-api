4596b7dad9f529f28490e746002f497b2b56a63a
rmora101 <59456883+rmora101@users.noreply.github.com>
2023-05-08 13:14:13 -0700

    added a to dict function to the task

--- app/models/task.py
+++ app/models/task.py
@@ -6,3 +6,12 @@ class Task(db.Model):
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, default= None) #something about has to be null
+
+    def to_dict(self):
+        return {
+            "task_id": self.task_id,
+            "title" : self.title,
+            "description" : self.description,
+            "completed_at" : self.completed_at
+        }
+
