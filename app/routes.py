271530d625714346e3901d6118c08b7dd7bac52c
rmora101 <59456883+rmora101@users.noreply.github.com>
2023-05-08 10:46:29 -0700

    started the post route

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, db, jsonify, request
+
+
+task_bp = Blueprint("tasks"), __name__,url_prefix="/tasks"
+
+#post a task
+@task_bp.route("", methods=["POST"])
+def add_task():
+    request_body = request.get_json()
+    # new_task = 
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return {"id": new_task.id}, 201
