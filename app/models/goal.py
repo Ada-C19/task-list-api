6a4dcfec8781918bebafe1005ef4834d9d84ffad
Raissa Barbosa <raissaglaucie@icloud.com>
2023-05-08 19:13:23 -0400

    Goal model and Migration are done

--- app/models/goal.py
+++ app/models/goal.py
@@ -3,3 +3,17 @@ from app import db
 
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+
+
+    def to_dict(self):
+        return {
+        "id" : self.goal.id,
+        "title" : self.title
+        }
+    
+    @classmethod
+    def from_dict(cls, goal_data):
+        return cls(
+            title = goal_data["title"]
+        )
