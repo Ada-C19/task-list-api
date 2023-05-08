c52930300a476de717d1de812eb22b4dd1d94c89
rmora101 <59456883+rmora101@users.noreply.github.com>
2023-05-08 15:37:40 -0700

    added functions to the models

--- app/models/task.py
+++ app/models/task.py
@@ -2,16 +2,24 @@ from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, default= None) #something about has to be null
 
     def to_dict(self):
         return {
-            "task_id": self.task_id,
+            "id": self.task_id,
             "title" : self.title,
             "description" : self.description,
-            "completed_at" : self.completed_at
+            "is_complete" : self.completed_at is not None
         }
+    
 
+    @classmethod
+    def from_dict(cls, task_data):
+        return cls(
+            title = task_data["title"],
+            description = task_data["description"],
+            completed_at = task_data["completed_at"],
+        )
