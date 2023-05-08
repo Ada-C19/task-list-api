0cd35c68ceae7c258b6ab764da1f53272079d31a
Rediet Medhane <redietkmedhane@gmail.com>
2023-05-08 15:35:46 -0700

    created CRUD routes for goals, passes wave 5

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import abort, Blueprint, jsonify, make_response, request
 from app import db
 from app.models.task import Task
+from app.models.goal import Goal
 from datetime import datetime, date
 import logging
 import os
@@ -8,6 +9,8 @@ from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 
 task_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+goal_bp = Blueprint("goals", __name__, url_prefix="/goals")
+
 client = WebClient(token=os.environ.get("SLACK_BOT_TOKEN"))
 logger = logging.getLogger(__name__)
 
@@ -47,13 +50,13 @@ def create_new_task():
 
 @task_bp.route("/<task_id>", methods = ["GET"])
 def get_task_by_id(task_id):
-    task = validate_task_id(task_id)
+    task = validate_item(Task, task_id)
     return {"task": task.to_dict()}, 200
 
 
 @task_bp.route("/<task_id>", methods = ["PUT"])
 def update_one_task(task_id):
-    updated_task = validate_task_id(task_id)
+    updated_task = validate_item(Task, task_id)
 
     request_body = request.get_json()
     updated_task.title = request_body["title"],
@@ -64,30 +67,30 @@ def update_one_task(task_id):
 
 @task_bp.route("/<task_id>", methods = ["DELETE"])
 def delete_one_task(task_id):
-    task = validate_task_id(task_id)
+    task = validate_item(Task, task_id)
     
     db.session.delete(task)
     db.session.commit()
 
     return {"details": f'Task {task_id} "{task.title}" successfully deleted'}, 200
 
-def validate_task_id(task_id):
+def validate_item(Model, id):
     try:
-        valid_task_id = int(task_id)
+        valid_id = int(id)
     except:
-        return abort(make_response({"message": f"invalid id: {task_id}"}, 400))
+        return abort(make_response({"message": f"invalid id: {id}"}, 400))
     
-    return Task.query.get_or_404(valid_task_id)
+    return Model.query.get_or_404(valid_id)
 #passes wave one
 
 @task_bp.route("/<task_id>/mark_complete", methods = ["PATCH"])
 def mark_task_complete(task_id):
-    updated_task = validate_task_id(task_id)
+    updated_task = validate_item(Task, task_id)
 
     updated_task.completed_at = datetime.now()
     db.session.commit()
+    
     channel_id = "C056SJEC9D0"
-
     try:
         # Call the chat.postMessage method using the WebClient
         result = client.chat_postMessage(
@@ -102,8 +105,55 @@ def mark_task_complete(task_id):
 
 @task_bp.route("/<task_id>/mark_incomplete", methods = ["PATCH"])
 def mark_task_incomplete(task_id):
-    updated_task = validate_task_id(task_id)
+    updated_task = validate_item(Task, task_id)
 
     updated_task.completed_at = None
     db.session.commit()
     return {"task": updated_task.to_dict()}, 200
+
+@goal_bp.route("", methods = ["POST"])
+def create_goal():
+    request_body = request.get_json()
+    try:
+        new_goal = Goal(
+            title = request_body["title"]
+        )
+        db.session.add(new_goal)
+        db.session.commit()
+
+        result = new_goal.to_dict()
+        return {"goal": result}, 201
+    except:
+        return abort(make_response({"details": "Invalid data"}, 400))
+    
+@goal_bp.route("", methods = ["GET"])
+def get_goals():
+    response = []
+    all_goals = Goal.query.all()
+    for goal in all_goals:
+        response.append(goal.to_dict())
+    return jsonify(response), 200
+
+@goal_bp.route("/<goal_id>", methods = ["GET"])
+def get_task_by_id(goal_id):
+    goal = validate_item(Goal, goal_id)
+    return {"goal": goal.to_dict()}, 200
+
+@goal_bp.route("/<goal_id>", methods = ["PUT"])
+def update_one_goal(goal_id):
+    updated_goal = validate_item(Goal, goal_id)
+
+    request_body = request.get_json()
+    updated_goal.title = request_body["title"]
+
+    db.session.commit()
+    return {"goal": updated_goal.to_dict()}, 200
+
+@goal_bp.route("/<goal_id>", methods = ["DELETE"])
+def delete_one_goal(goal_id):
+    goal = validate_item(Goal, goal_id)
+    
+    db.session.delete(goal)
+    db.session.commit()
+
+    return {"details": f'Goal {goal_id} "{goal.title}" successfully deleted'}, 200
