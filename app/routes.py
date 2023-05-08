32f1fb1f05b37b1744ae87f8de5765354d0f4950
mlhuynh <greenlinh@gmail.com>
2023-05-07 22:18:03 -0700

    Wrote POST endpoint and create_task function in app routes.py module.

--- app/routes.py
+++ app/routes.py
@@ -2,4 +2,19 @@ from app import db
 from app.models.task import Task
 from flask import Blueprint, jsonify, make_response, request, abort
 
-tasks_bp = Blueprint('tasks_bp', __name__, url_prefix='/tasks')
+
+tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
+
+@tasks_bp.route("", methods=["POST"])
+def create_task():
+    request_body = request.get_json()
+    new_task = Task(
+        title = request_body["title"],
+        description = request_body["description"],
+        is_complete = request_body["is_complete"]
+    )
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return make_response(f"Task {new_task.title} has been successfully created. Hurray!", 201)
