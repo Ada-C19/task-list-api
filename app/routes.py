1011c9636e58c80695bc0c116fad8b73d39d3849
Janice <janicenguyenn@gmail.com>
2023-05-07 19:46:06 -0700

    created validate_task helper function and refactored get_by_id to use it

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,28 @@
 from app import db
 from app.models.task import Task
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, make_response, abort
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+def validate_task(task_id):
+    try:
+        task_id = int(task_id)
+    except:
+        abort(make_response({"message": f"task {task_id} invalid"}, 400))
+    
+    task = Task.query.get(task_id)
+    if not task:
+        abort(make_response({"message":f"task {task_id} not found"}, 404))
+    return task
+
+
 
 # POST request to create new task
 @tasks_bp.route("", methods=['POST'])
 def create_task():
     request_body = request.get_json()
     
-    new_task = Task(title=request_body["title"], description=request_body["description"], completed_at=request_body["completed_at"])
+    new_task = Task(title=request_body["title"], description=request_body["description"])
 
     db.session.add(new_task)
     db.session.commit()
@@ -42,7 +55,7 @@ def get_all_tasks():
 @tasks_bp.route("/<task_id>", methods=["GET"])
 
 def get_by_id(task_id):
-    task = Task.query.get(task_id)
+    task = validate_task(task_id)
 
     return {
         "task": {
