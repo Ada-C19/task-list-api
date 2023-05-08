7e8f2466962cf0307f9a5d7a95ae8f233b4161f5
Aiyana Ball <ballaiyana@gmail.com>
2023-05-07 23:26:29 -0700

    updated Task model

--- app/models/task.py
+++ app/models/task.py
@@ -5,22 +5,30 @@ class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String)
     description = db.Column(db.String)
-    completed_at = db.Column(db.Integer, nullable = True)
+    completed_at = db.Column(db.DateTime, nullable = True)
 
     def to_dict(self):
+        if self.completed_at != None:
+            is_complete = True
+        else:
+            is_complete = False
         return {
             "task_id": self.task_id,
             "title": self.title,
             "description": self.description,
-            "completed_at": self.completed_at
+            "is_complete": is_complete
         }
 
     def to_dict_task(self):
+        if self.completed_at != None:
+            is_complete = True
+        else:
+            is_complete = False
         return {
             "task": {
             "task_id": self.task_id,
             "title": self.title,
             "description": self.description,
-            "completed_at": self.completed_at
+            "is_complete": is_complete
             }
         }
