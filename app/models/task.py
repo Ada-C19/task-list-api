51269ba13b96f2ab75e5840e24c31f823754416a
Ana Silvera <anasilverx@gmail.com>
2023-05-07 17:00:27 -0700

    implemented route to get all tasks. added fix to create a task

--- app/models/task.py
+++ app/models/task.py
@@ -12,7 +12,16 @@ class Task(db.Model):
         new_task = cls(
             title=task_details["title"],
             description=task_details["description"],
-            completed_at=task_details["completed_at"]
+            completed_at=task_details.get("completed_at", None)
         )
 
         return new_task
+
+    # def to_dict(self):
+    #     return {
+    #         "id": self.task_id,
+    #         "title": self.title,
+    #         "description": self.description,
+    #         "completed_at": self.completed_at,
+    #         "is_complete": self.is_complete
+    #     }
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 from app.models.task import Task
 from app import db
 
@@ -21,3 +21,19 @@ def create_task():
             "is_complete": False
         }
     }, 201
+
+@tasks_bp.route("", methods=["GET"])
+def read_all_tasks():
+    #Retrieve a list of all the task objects in database
+    all_tasks = Task.query.all()
+    
+    tasks_response = []
+    for task in all_tasks:
+        tasks_response.append({
+            'id':task.task_id,
+            'title': task.title,
+            'description':task.description,
+            'is_complete': False 
+        })
+
+    return jsonify(tasks_response), 200
