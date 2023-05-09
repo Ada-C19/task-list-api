390ee3d4a41e1bf4d95dcee8b755a82fc197a23e
Sel <selenetenorio27@hotmail.com>
2023-05-08 20:00:13 -0700

    Connect to Render db

--- app/__init__.py
+++ app/__init__.py
@@ -17,8 +17,9 @@ def create_app(test_config=None):
     app.config["SLACK_API_POST"] = os.environ.get("SLACK_API_POST")
 
     if test_config is None:
-        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-            "SQLALCHEMY_DATABASE_URI")
+        # app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+        #     "SQLALCHEMY_DATABASE_URI")
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("RENDER_DATABASE_URI")
     else:
         app.config["TESTING"] = True
         app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
