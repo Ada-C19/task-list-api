aa8f3cb987f701510d0e0ffa90dda7a7fd41a061
Sydney Alberson <sydney.alberson@gmail.com>
2023-05-09 18:09:05 -0400

    Completed Wave 05

--- app/models/goal.py
+++ app/models/goal.py
@@ -2,5 +2,5 @@ from app import db
 
 class Goal(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String)
+    title = db.Column(db.String, nullable=False)
     task = db.relationship("Task", back_populates="goal")
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify, make_response, abort
 from app import db
 from app.models.task import Task
+from app.models.goal import Goal
 from sqlalchemy import desc, asc
 from datetime import datetime
 
@@ -28,18 +29,18 @@ def get_all_tasks():
 
 
 # Validate ID function
-def validate_id(task_id):
-    task = Task.query.get(task_id)
+def validate_model(cls, model_id):
+    model_item = cls.query.get(model_id)
 
-    if not task:
-        return abort(make_response({"message": f"Task with ID {task_id} not found."}, 404))
+    if not model_item:
+        return abort(make_response({"message": f"{cls.__name__} with ID {model_id} not found."}, 404))
 
-    return task
+    return model_item
 
 @tasks_bp.route("/<task_id>", methods=["GET"])
 def get_task_by_id(task_id):
 
-    task = validate_id(task_id)
+    task = validate_model(Task, task_id)
 
     return {"task": {
             "id": task.id,
@@ -74,7 +75,7 @@ def create_task():
 
 @tasks_bp.route("/<task_id>", methods=["PUT"])
 def update_task(task_id):
-    task = validate_id(task_id)
+    task = validate_model(Task, task_id)
     request_body = request.get_json()
     
     task.title = request_body["title"],
@@ -91,7 +92,7 @@ def update_task(task_id):
 
 @tasks_bp.route("/<task_id>", methods=["DELETE"])
 def delete_task(task_id):
-    task = validate_id(task_id)
+    task = validate_model(Task, task_id)
 
     db.session.delete(task)
     db.session.commit()
@@ -102,7 +103,7 @@ def delete_task(task_id):
 
 @tasks_bp.route("/<task_id>/mark_incomplete", methods=["PATCH"])
 def mark_incomplete(task_id):
-    task = validate_id(task_id)
+    task = validate_model(Task, task_id)
 
     task.completed_at = None
 
@@ -117,12 +118,11 @@ def mark_incomplete(task_id):
 
 @tasks_bp.route("/<task_id>/mark_complete", methods=["PATCH"])
 def mark_complete(task_id):
-    task = validate_id(task_id)
+    task = validate_model(Task, task_id)
 
     task.completed_at = datetime.utcnow()
 
     db.session.commit()
-    
     
     return {"task": {
             "id": task.id,
@@ -132,5 +132,73 @@ def mark_complete(task_id):
         }}, 200
 
 
+@goals_bp.route("", methods=["POST"])
+def create_goal():
+    request_body = request.get_json()
+    try:
+        new_goal = Goal(
+            title = request_body["title"]
+        )
+    except:
+        return {
+            "details": "Invalid data"
+        }, 400
 
+    db.session.add(new_goal)
+    db.session.commit()
 
+    return jsonify({"goal": {
+            "id": new_goal.id,
+            "title": new_goal.title
+        }
+    }), 201
+
+@goals_bp.route("", methods=["GET"])
+def get_all_goals():
+    goals = Goal.query.all()
+    
+    goals_response = []
+    for goal in goals:
+        goals_response.append({
+            "id": goal.id,
+            "title": goal.title
+            })
+    
+    return jsonify(goals_response), 200
+
+@goals_bp.route("/<goal_id>", methods=["GET"])
+def get_goal_by_id(goal_id):
+    goal = validate_model(Goal, goal_id)
+    
+    return {
+        "goal": {
+            "id": goal.id,
+            "title": goal.title
+        }
+    }, 200
+
+@goals_bp.route("/<goal_id>", methods=["PUT"])
+def update_goal(goal_id):
+    goal = validate_model(Goal, goal_id)
+
+    request_body = request.get_json()
+    
+    goal.title = request_body["title"],
+    
+    db.session.commit()
+
+    return jsonify({"goal": {
+            "id": goal.id,
+            "title": goal.title
+            }}), 200
+
+@goals_bp.route("/<goal_id>", methods=["DELETE"])
+def delete_goal(goal_id):
+    goal = validate_model(Goal, goal_id)
+
+    db.session.delete(goal)
+    db.session.commit()
+    
+    return jsonify({
+        "details": f"Goal {goal.id} \"{goal.title}\" successfully deleted"
+        }), 200
