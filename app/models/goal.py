6bf4ae26a26ac7b1a34ada9a2ad425f64dcb023a
Sophia Tran <sophiat8832@gmail.com>
2023-05-09 13:47:41 -0700

    implemented to_dict method

--- app/models/goal.py
+++ app/models/goal.py
@@ -4,3 +4,11 @@ from app import db
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
+
+
+    def to_dict(self):
+        goal_as_dict = {}
+        goal_as_dict["id"] = self.goal_id
+        goal_as_dict["title"] = self.title
+
+        return goal_as_dict
