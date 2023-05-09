31b29314e0e47f2828c089b99a1b73becd01dbee
Gweneth Johnson <johnsong0119@gmail.com>
2023-05-09 16:25:53 -0700

    forgot to commit a million things wave one and two are done and the tests

--- app/models/task.py
+++ app/models/task.py
@@ -3,3 +3,30 @@ from app import db
 
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text, nullable= False)
+    description = db.Column(db.Text, nullable= False)
+    completed_at = db.Column(db.DateTime, nullable= True)
+
+    def to_dict(self):
+        return_dict = {}
+        return_dict["id"] = self.task_id
+        return_dict["title"] = self.title
+        return_dict["description"] = self.description
+        return_dict["is_complete"] = self.completed_at 
+        if not self.completed_at:
+            return_dict["is_complete"] = False
+        return return_dict
+
+
+        # return({"id" : self.task_id,
+        #         "title" : self.title,
+        #         "description" : self.description,
+        #         "is_complete" : self.completed_at})
+    
+    @classmethod
+    def from_dict(cls, data_dict):
+        return cls(
+            title = data_dict["title"],
+            description = data_dict["description"],
+            completed_at = data_dict.get("completed_at")
+        )
---
+++ app/routes/task_routes.py
@@ -0,0 +1,73 @@
+from app import db
+from flask import Blueprint, jsonify, abort, make_response, request
+from app.models.task import Task
+from app.helpers.helpers import validate_model
+from sqlalchemy import desc, asc
+
+task_bp = Blueprint("task_bp", __name__, url_prefix="/tasks")
+
+@task_bp.route("", methods=["POST"])
+def create_task():
+    request_body = request.get_json()
+    try:
+        new_task = Task.from_dict(request_body)
+
+        db.session.add(new_task)
+        db.session.commit()
+        # if not new_task["is_complete"]:
+        #     new_task["is_complete"] = False
+
+        return {"task" : new_task.to_dict()}, 201
+    
+    except KeyError:
+        abort(make_response({"details": "Invalid data"}, 400))
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return make_response(f"Task {new_task.title} successfully created", 201)
+
+@task_bp.route("", methods=["GET"])
+def get_all_tasks():
+    title_query = request.args.get("sort")
+    if title_query:
+        if title_query == "asc":
+            tasks = Task.query.order_by(Task.title.asc()).all()
+        elif title_query == "desc":
+            tasks = Task.query.order_by(Task.title.desc()).all()
+    else:
+        tasks = Task.query.all()
+    if not tasks:
+        return make_response(jsonify([]), 200)
+
+    results = [task.to_dict() for task in tasks]
+    return jsonify(results)
+
+@task_bp.route("/<task_id>", methods=["GET"])
+def get_task_by_id(task_id):
+    task = validate_model(Task, task_id)
+    results = {"task" : task.to_dict()}
+
+    return make_response(jsonify(results), 200)
+
+@task_bp.route("/<task_id>", methods=["PUT"])
+def update_task(task_id):
+    task_data = request.get_json()
+    task_to_update = validate_model(Task, task_id)
+
+    task_to_update.title = task_data["title"]
+    task_to_update.description = task_data["description"]
+    
+    
+
+    db.session.commit()
+    return {"task": task_to_update.to_dict()}
+
+@task_bp.route("/<task_id>", methods=["DELETE"])
+def delete_task(task_id):
+    task = validate_model(Task, task_id)
+
+    db.session.delete(task)
+    db.session.commit()
+
+    return make_response({"details": f"Task {task_id} \"{task.title}\" successfully deleted"})
