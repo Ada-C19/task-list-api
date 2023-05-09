7c9a6775036dc499daa22fef38f987c71c913199
Genesis Quinn <genesis@quinns.io>
2023-05-09 18:48:00 -0400

    get all tasks[GET], get one task[GET], create a task[POST]

--- app/routes.py
+++ app/routes.py
@@ -15,3 +15,52 @@ def create_task():
     response = {"task": new_task.to_dict()}
 
     return make_response(jsonify(response), 201)
+
+@tasks_bp.route("", methods=["GET"])
+def get_all_tasks():
+    title_param = request.args.get("title")
+    
+    if title_param:
+        tasks = Task.query.filter_by(title=title_param)
+    else:
+        tasks = Task.query.all()
+
+    task_list=[task.to_dict()for task in tasks]
+
+    return jsonify(task_list), 200
+
+
+@tasks_bp.route("/<task_id>", methods=["GET"])
+def get_one_planet(task_id):
+
+    task = validate_model(Task, task_id)
+    response = {"task": task.to_dict()}
+
+    return make_response(jsonify(response), 200)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# HELPER FUNCTION
+def validate_model(cls, id):
+    try:
+        id = int(id)
+    except:
+        abort(make_response({"message": f"{id} was invalid"}, 400))
+
+    model = cls.query.get(id)
+
+    if not model:
+        abort(make_response(
+            {"message": f"{cls.__name__} with id {id} was not found"}, 404))
+    
+    return model
