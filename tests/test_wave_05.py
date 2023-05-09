6e7c8f2a42df42f827e64feb0999cded05c1f66b
Sophia Tran <sophiat8832@gmail.com>
2023-05-09 14:23:46 -0700

    implemented rest_delete_goal_not_found

--- tests/test_wave_05.py
+++ tests/test_wave_05.py
@@ -154,17 +154,21 @@ def test_delete_goal(client, one_goal):
     # *****************************************************************
     assert response_body == {"message":"goal 1 not found"}
 
-@pytest.mark.skip(reason="test to be completed by student")
+# @pytest.mark.skip(reason="test to be completed by student")
 def test_delete_goal_not_found(client):
-    raise Exception("Complete test")
+    # raise Exception("Complete test")
 
     # Act
     # ---- Complete Act Here ----
+    response = client.delete("/goals/1")
+    response_body = response.get_json()
 
     # Assert
     # ---- Complete Assertions Here ----
     # assertion 1 goes here
+    assert response.status_code == 404
     # assertion 2 goes here
+    assert response_body == {"message":"goal 1 not found"}
     # ---- Complete Assertions Here ----
 
 
