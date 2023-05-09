d89a6cf62be6e9fc6b28e36e5b8f58483c517e81
selene castillo <secast@live.com>
2023-05-08 17:36:33 -0700

    updated tests and goals model

--- app/models/goal.py
+++ app/models/goal.py
@@ -3,3 +3,15 @@ from app import db
 
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+
+    def to_dict(self):
+        return {
+            "id": self.goal_id,
+            "title": self.title
+        }
+
+    @classmethod
+    def from_dict(cls, goal_data):
+        new_goal = cls(title=goal_data["title"])
+        return new_goal
