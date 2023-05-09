8161e0c771925fab1623d5e7ee76bcacb11b9f46
Sarah Kim <kimsuyon2013@gmail.com>
2023-05-08 22:43:00 -0700

    passed tests for marking task as incomplete

--- tests/test_wave_03.py
+++ tests/test_wave_03.py
@@ -39,8 +39,6 @@ def test_mark_complete_on_incomplete_task(client, one_task):
     }
     assert Task.query.get(1).completed_at
 
-
-@pytest.mark.skip(reason="No way to test this feature yet")
 def test_mark_incomplete_on_complete_task(client, completed_task):
     # Act
     response = client.patch("/tasks/1/mark_incomplete")
@@ -94,8 +92,6 @@ def test_mark_complete_on_completed_task(client, completed_task):
     }
     assert Task.query.get(1).completed_at
 
-
-@pytest.mark.skip(reason="No way to test this feature yet")
 def test_mark_incomplete_on_incomplete_task(client, one_task):
     # Act
     response = client.patch("/tasks/1/mark_incomplete")
