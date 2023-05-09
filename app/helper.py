264e1c0653a8c522f5fd4cc631d7593483375988
fabiola gonzalez <gonzalezfabs@gmail.com>
2023-05-08 20:29:22 -0700

    Completed get_all/get_one GET/POST functions

---
+++ app/helper.py
@@ -0,0 +1,15 @@
+from flask import abort,make_response
+from app.models.task import Task
+
+def validate_task(id):
+    try:
+        id = int(id)
+    except:
+        abort(make_response({"message": f"Task {id} is invalid"},400))
+    
+    task = Task.query.get(id)
+
+    if not task:
+        abort(make_response({"message": f"Task {id} not found."},404))
+    
+    return task
