76c39582efca278e8819d7fdb9bb793c9da09511
Charlie <charliethemushi@gmail.com>
2023-05-09 13:27:23 -0700

    added DELETE endpoint, added try/except for POST invalid inputs

--- app/routes.py
+++ app/routes.py
@@ -15,19 +15,23 @@ def validate_task(task_id):
         abort(make_response({"message":f"task {task_id} not found"}, 404))
 
     return task
+    
 
 task_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 
 @task_bp.route("", methods=["POST"])
 def create_tasks():
+    
     request_body = request.get_json()
-    new_task = Task.from_dict(request_body)
-
-
+    try:
+        new_task = Task.from_dict(request_body)
+    except:
+        abort(make_response({"details": "Invalid data"},400))
+        
     db.session.add(new_task)
     db.session.commit()
     
-    return make_response(f"Task {new_task.title} successfully created", 201)
+    return make_response(jsonify(f"Task {new_task.title} successfully created", 201))
 
 
 @task_bp.route("", methods=["GET"])
@@ -51,12 +55,15 @@ def read_one_task(task_id):
 
 @task_bp.route("/<task_id>", methods=["DELETE"])
 def delete_task(task_id):
-    
+
+    request_body = request.get_json()
     task = validate_task(task_id)
+    task.description = request_body["description"]
     db.session.delete(task)
     db.session.commit()
 
-    return make_response(f"task #{task_id} successfully deleted",200)
+    return make_response(jsonify({"details": f"Task {task_id}: \"{task.description}\" successfully deleted"}), 200)
+
 
 @task_bp.route("/<task_id>", methods=["PUT"])
 def update_task(task_id):
