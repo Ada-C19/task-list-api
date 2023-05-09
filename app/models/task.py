48afeaa4ed3e730d0954048a4c85c48021e24d4f
Sage Choi <coolcuds@gmail.com>
2023-05-09 01:23:25 -0700

    Added relationship between tasks and goals, and to_dict_with_goal()

--- app/models/task.py
+++ app/models/task.py
@@ -5,10 +5,21 @@ class Task(db.Model):
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime)
+    goal_id = db.Column(db.Integer, db.ForeignKey('goal.goal_id'))
+    goal = db.relationship("Goal", back_populates="tasks")
 
     def to_dict(self):
         return {
             "id": self.task_id,
+            "title": self.title,
+            "description": self.description,
+            "is_complete": self.completed_at is not None
+        }
+
+    def to_dict_with_goal(self):
+        return {
+            "id": self.task_id,
+            "goal_id": self.goal_id,
             "title": self.title,
             "description": self.description,
             "is_complete": self.completed_at is not None
