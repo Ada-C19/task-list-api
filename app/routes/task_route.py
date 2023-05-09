23220388fad8b9cfc7c41c2f2e11b5438e3e8070
An Ba <ngwin.ab@gmail.com>
2023-05-09 16:47:00 -0400

    created routes for task model

---
+++ app/routes/task_route.py
@@ -0,0 +1,138 @@
+from flask import Blueprint, jsonify, abort, make_response, request
+from app.models.task import Task
+from app import db
+from datetime import date
+
+tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+# helper function
+
+def validate_model(cls, model_id):
+    try:
+        model_id = int(model_id)
+    except:
+        abort(make_response({"message": f"{cls.__name__} {model_id} is not valid."}), 400)
+
+    model = cls.query.get(model_id)
+
+    if not model:
+        abort(make_response({"message": f"{cls.__name__} {model_id} is not found."}, 404))
+
+    return model
+
+# route to get all tasks
+
+@tasks_bp.route("", methods=["GET"])
+def read_all_tasks():
+    sort_query = request.args.get("sort")
+    if sort_query == "asc":
+        tasks = Task.query.order_by(Task.title.asc())
+    elif sort_query == "desc":
+        tasks = Task.query.order_by(Task.title.desc())
+    else:
+        tasks = Task.query.all()
+
+    task_response = []
+    for task in tasks:
+        task_response.append(task.to_dict())
+    return jsonify(task_response), 200
+
+# route to create a new task
+
+@tasks_bp.route("", methods=["POST"])
+def create_task():
+    request_body = request.get_json()
+
+    if request_body.get("title") and request_body.get("description"):
+        new_task = Task.from_dict(request_body)
+
+        db.session.add(new_task)
+        db.session.commit()
+
+        return {"task": new_task.to_dict()}, 201
+
+    return jsonify({"details": "Invalid data"}), 400
+
+# route to get a task by id
+
+@tasks_bp.route("/<task_id>", methods=["GET"])
+def read_one_task(task_id):
+    task = validate_model(Task, task_id)
+    return {
+        "task": {
+            "id": task.id,
+            "title": f"{task.title}",
+            "description": f"{task.description}",
+            "is_complete": False
+        }
+    }, 200
+
+# route to update a task by id
+
+@tasks_bp.route("/<task_id>", methods=["PUT"])
+def update_task(task_id):
+    task = validate_model(Task, task_id)
+    request_body = request.get_json()
+    task.title = request_body["title"]
+    task.description = request_body["description"]
+
+    db.session.commit()
+
+    return {
+        "task": {
+            "id": task.id,
+            "title": f"{task.title}",
+            "description": f"{task.description}",
+            "is_complete": False
+        }
+    }, 200
+
+# route to delete a task by id
+
+@tasks_bp.route("/<task_id>", methods=["DELETE"])
+def delete_task(task_id):
+    task = validate_model(Task, task_id)
+
+    db.session.delete(task)
+    db.session.commit()
+
+    return jsonify({"details": f'Task {task_id} "{task.title}" successfully deleted'}), 200
+
+
+# route to mark Complete on an Incompleted Task
+@tasks_bp.route("/<task_id>/mark_complete", methods=["PATCH"])  #path param
+def mark_complete_on_incomplete_task(task_id):
+    task = validate_model(Task, task_id)
+
+    task.completed_at = date.today()
+
+    db.session.commit()
+
+    return {
+        "task": {
+            "id": task.id,
+            "title": f"{task.title}",
+            "description": f"{task.description}",
+            "is_complete": True
+        }
+    }, 200
+
+# route to mark Incomplete on a Completed Task
+@tasks_bp.route("/<task_id>/mark_incomplete", methods=["PATCH"])
+def mark_incomplete_on_complete_task(task_id):
+    task = validate_model(Task, task_id)
+
+    task.completed_at = None
+
+    db.session.commit()
+
+    return {
+        "task": {
+            "id": task.id,
+            "title": f"{task.title}",
+            "description": f"{task.description}",
+            "is_complete": False
+        }
+    }, 200
+
+
