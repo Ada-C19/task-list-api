7695002a6b9151b0b83d18ab878a632e4c080483
Sophia Tran <sophiat8832@gmail.com>
2023-05-09 14:18:31 -0700

    implemented delete_goal DELETE request for goals route

--- app/routes.py
+++ app/routes.py
@@ -174,3 +174,15 @@ def update_goal(goal_id):
     
     db.session.commit()
     return {"goal": goal.to_dict()}, 200
+
+
+@goals_bp.route("/<goal_id>", methods=["DELETE"])
+def delete_goal(goal_id):
+    goal = validate_goal_by_id(goal_id)
+
+    db.session.delete(goal)
+    db.session.commit()
+
+    return {
+        "details": f"Task {goal_id} \"{goal.title}\" successfully deleted"
+    }, 200
