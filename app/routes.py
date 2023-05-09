774eb729bd05db751a8a2be7f47229e842d29097
Alejandra <adom2128@gmail.com>
2023-05-08 19:58:39 -0600

    includes wave 2 with all tests passing

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from app import db
 from flask import Blueprint, request, make_response, jsonify
 from app.models.task import Task
 from .routes_helpers import validate_model
+from sqlalchemy import asc, desc
 
 task_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 
@@ -19,9 +20,18 @@ def create_task():
 
 @task_bp.route("", methods=["GET"])
 def get_all_tasks():
-    tasks = Task.query.all()
+    # tasks = Task.query.all()
 
-    tasks_response = [task.to_dict() for task in tasks]
+    task_query = Task.query
+
+    sort_query = request.args.get("sort")
+
+    if sort_query == "asc":
+        task_query = task_query.order_by(asc(Task.title))
+    elif sort_query == "desc":
+        task_query = task_query.order_by(desc(Task.title))
+    
+    tasks_response = [task.to_dict() for task in task_query]
     return jsonify(tasks_response)
 
 
