6d812d3ad439ff67de254219c1d2ac44f7f314f0
Lyuda Kim <lyudarkim@gmail.com>
2023-05-08 20:33:20 -0700

    Actually deleted from_dict() from the Task model

--- app/models/task.py
+++ app/models/task.py
@@ -23,11 +23,5 @@ class Task(db.Model):
         return task_dict
 
 
-    # @classmethod
-    # def from_dict(cls, task_data):
-    #     new_task = Task(title=task_data["title"], 
-    #                     description=task_data["description"])   
-
-    #     return new_task  
             
         
