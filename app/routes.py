0b06c3e477b5c5e08b3789a745dd917f967efa29
Genesis Quinn <genesis@quinns.io>
2023-05-09 16:40:59 -0400

    created and registered tasks_bp

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,5 @@
-from flask import Blueprint
+from app import db
+from models.task import Task
+from flask import Blueprint, jsonify, make_response, request, abort
+
+tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
