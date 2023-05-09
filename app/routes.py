9463bcb66040c3eb587b344e80bf66abda70f0f5
Hannah <hannah@chandleys.com>
2023-05-09 12:44:34 -0700

    Created and registered blueprint for tasks

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, make_response, request
+from app.models.task import Task
+from app import db
+
+# All routes for tasks start with "/tasks" URL prefix
+tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
