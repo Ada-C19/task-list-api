3de43773b66971e17e104ce3a783b5cb4ba3dc96
Amber Shay <mashaad21@gmail.com>
2023-05-09 15:21:29 -0600

    successfully create new task

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@ from app.models.task import Task
 from app.models.goal import Goal
 from flask import Blueprint, jsonify, make_response, request, abort
 
-task_list_bp = Blueprint("task_list_bp", __name__, url_prefix="/task")
+task_list_bp = Blueprint("task_list_bp", __name__, url_prefix="/tasks")
 
 def validate_model_task(cls, model_id):
     try:
@@ -19,28 +19,32 @@ def validate_model_task(cls, model_id):
     return task
 
 
-def validate_model_goal(cls, model_id):
-    try:
-        model_id = int(model_id)
-    except:
-        abort(make_response({"message": f"{cls.__name__} {model_id} invalid"}, 400))
+# def validate_model_goal(cls, model_id):
+#     try:
+#         model_id = int(model_id)
+#     except:
+#         abort(make_response({"message": f"{cls.__name__} {model_id} invalid"}, 400))
 
-    goal = cls.query.get(model_id)
+#     goal = cls.query.get(model_id)
 
-    if not goal:
-        abort(make_response({"message": f"{cls.__name__} {model_id} not found"}, 404))
+#     if not goal:
+#         abort(make_response({"message": f"{cls.__name__} {model_id} not found"}, 404))
 
-    return goal
+#     return goal
 
 @task_list_bp.route("", methods=["POST"])
 def create_task():
-    request_body = request.json()
+    request_body = request.get_json()
     new_task = Task.task_from_dict(request_body)
 
     db.session.add(new_task)
     db.session.commit()
 
-@task_list_bp.route("", methods=["GET"])
-def say_hi_new_task():
-    my_response = "Please create a new task"
-    return my_response
+    return make_response(jsonify(f"Task {new_task.title} successfully created"), 201)
+
+
+
+# @task_list_bp.route("", methods=["GET"])
+# def say_hi_new_task():
+#     my_response = "Please create a new task"
+#     return my_response
