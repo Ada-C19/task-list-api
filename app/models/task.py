d3b6a25352a78d4cc47012caa1db724e3b817df5
Luwam Ghebremicael <luwammikael@gmail.com>
2023-05-08 17:59:10 -0700

    added nullability

--- app/models/task.py
+++ app/models/task.py
@@ -2,4 +2,19 @@ from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement = True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    completed_at = db.Column(db.DateTime, default = None, nullable = True)
+
+
+def to_json(self):
+    is_complete = True if self.completed_at else False;
+
+    return{
+        "id": self.task_id,
+        "title": self.title,
+        "description": self.description,
+        "is_complete": is_complete
+    }
+
