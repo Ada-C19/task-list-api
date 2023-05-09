e2c7b6e44fe9f46a4433176f6256275519f27122
Hannah <hannah@chandleys.com>
2023-05-09 13:11:11 -0700

    Created GET route for tasks

--- app/routes/tasks_routes.py
+++ app/routes/tasks_routes.py
@@ -5,3 +5,10 @@ from app import db
 # All routes for tasks start with "/tasks" URL prefix
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 
+@tasks_bp.route("", methods=["GET"])
+def handle_get_animals_request():
+    tasks = Task.query.all()
+    task_response = []
+    for task in tasks:
+        task_response.append(task.to_dict())
+    return jsonify(task_response), 200
