11d74865e55125c696c4845ade6aee04b42b55e7
Amber Shay <mashaad21@gmail.com>
2023-05-09 12:52:59 -0600

    successful simple get with postman

--- app/models/task.py
+++ app/models/task.py
@@ -2,7 +2,29 @@ from app import db
 
 
 class Task(db.Model):
+    """Task definition"""
     task_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
-    completed_at = db.Column(db.DateTime)
+    completed_at = db.Column(db.DateTime, default=None) # set default value to <null>
+    is_complete = db.Column(db.String, unique=False)    # designate as not completed
+
+    def task_to_dict(self):
+        """Create task as a dictionary"""
+        task_as_dict = {}
+
+        task_as_dict["task_id"] = self.task_id
+        task_as_dict["title"] = self.title
+        task_as_dict["description"] = self.description
+        task_as_dict["completed_at"] = self.completed_at
+        task_as_dict["is_complete"] = self.is_complete
+
+        return task_as_dict
+    
+    @classmethod
+    def task_from_dict(cls, task_data):
+        new_task = Task(title=task_data["title"],
+                        description=task_data["description"],
+                        completed_at=task_data["completed_at"],
+                        is_complete=task_data["is_complete"])
+        return new_task
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, jsonify, make_response, request, abort
+from app import db
 from app.models.task import Task
 from app.models.goal import Goal
+from flask import Blueprint, jsonify, make_response, request, abort
 
-task_list_bp = Blueprint("task_list", __name__)
+task_list_bp = Blueprint("task_list_bp", __name__, url_prefix="/task")
 
 def validate_model_task(cls, model_id):
     try:
@@ -11,7 +12,6 @@ def validate_model_task(cls, model_id):
         abort(make_response({"message": f"{cls.__name__} {model_id} invalid"}, 400))
 
     task = cls.query.get(model_id)
-    goal = cls.query.get(model_id)
 
     if not task:
         abort(make_response({"message": f"{cls.__name__} {model_id} not found"}, 404))
@@ -31,3 +31,16 @@ def validate_model_goal(cls, model_id):
         abort(make_response({"message": f"{cls.__name__} {model_id} not found"}, 404))
 
     return goal
+
+@task_list_bp.route("", methods=["POST"])
+def create_task():
+    request_body = request.json()
+    new_task = Task.task_from_dict(request_body)
+
+    db.session.add(new_task)
+    db.session.commit()
+
+@task_list_bp.route("", methods=["GET"])
+def say_hi_new_task():
+    my_response = "Please create a new task"
+    return my_response
