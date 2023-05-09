f9108383a5e5a748257980a048699eea262c036f
Ariel Williams <arielwilliams121013@gmail.com>
2023-05-09 01:31:56 -0700

    Created class instance function to_dict() in task.py. Updated code in routes.py. Now passing 9 tests, and failing 2 in wave 1.

--- app/models/task.py
+++ app/models/task.py
@@ -2,8 +2,16 @@ from app import db
 
 
 class Task(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable=True)
 
+
+    def to_dict(self):
+        return {
+            "id": self.task_id,
+            "title": self.title,
+            "description": self.description,
+            "is_complete": self.completed_at != None
+        }
--- app/routes.py
+++ app/routes.py
@@ -10,12 +10,12 @@ def validate_task(task_id):
     try:
         task_id = int(task_id)
     except:
-        abort(make_response({"message":f"task {task_id} invalid"}, 400))
+        abort(make_response({"message":f"Task {task_id} invalid"}, 400))
 
     task = Task.query.get(task_id)
 
     if not task:
-        abort(make_response({"message":f"task {task_id} not found"}, 404))
+        abort(make_response({"message":f"Task {task_id} not found"}, 404))
 
     return task
 
@@ -27,17 +27,11 @@ def validate_task(task_id):
 def create_one_task():
     request_body = request.get_json()
 
-    if "title" not in request_body.get_json():
-        abort(make_response({"message": "title not found"}), 400)
-    if "description" not in request_body.get_json():
-        abort(make_response({"message": "description not found"}), 400)
-    if "completed_at" not in request_body.get_json():
-        abort(make_response({"message": "completed_at not found"}), 400)
-
-    # Idk if I need completed_at bc it's nullable in task.py 
-    new_task = Task(title=request_body["title"], 
-                    description=request_body["description"],
-                    completed_at=request_body["completed_at"])
+    if "title" not in request_body or "description" not in request_body:
+        abort(make_response({"details": "Invalid data"}, 400))
+    else:
+        new_task = Task(title=request_body["title"], 
+                    description=request_body["description"])
 
     db.session.add(new_task)
     db.session.commit()
@@ -51,15 +45,8 @@ def read_all_tasks():
     tasks_response = []
     tasks = Task.query.all()
     for task in tasks:
-        tasks_response.append(
-            {
-                "id": task.id,
-                "title": task.title,
-                "description": task.description,
-                "completed_at": task.completed_at,
-                "is_completed": False if task.completed_at == None else True
-            }
-        )
+        tasks_response.append(task.to_dict())
+
     return jsonify(tasks_response), 200 
 
 
@@ -67,13 +54,8 @@ def read_all_tasks():
 @tasks_bp.route("/<task_id>", methods=["GET"])
 def read_one_task(task_id):
     task = validate_task(task_id)
-    return jsonify({
-            "id": task.id,
-            "title": task.title,
-            "description": task.description,
-            "completed_at": task.completed_at,
-            "is_completed": False if task.completed_at == None else True
-        }), 200
+    return {"task": task.to_dict()}, 200
+    # returning as a dict here plus calling to_dict because it's a nested dictionary. If I updated to_dict to handle nested dictionaries it would no longer work for read_all_tasks
 
 
 # update one task
@@ -83,7 +65,7 @@ def update_one_task(task_id):
 
     request_body = request.get_json()
 
-    task.id = request_body["id"]
+    task.id = request_body["task_id"]
     task.title = request_body["title"]
     task.description = request_body["description"]
     task.completed_at = request_body["completed_at"]
@@ -91,7 +73,7 @@ def update_one_task(task_id):
 
     db.session.commit()
 
-    return jsonify(make_response(f"Task #{task.id} successfully updated")), 200 
+    return jsonify(make_response(f"Task #{task.task_id} successfully updated")), 200 
 
 
 # delete one task
@@ -102,6 +84,10 @@ def delete_task(task_id):
     db.session.delete(task)
     db.session.commit()
 
-    return jsonify(make_response(f"Task #{task.id} successfully deleted")), 200
+    return make_response(jsonify({
+        "details": f'Task {task.task_id} "{task.title}" successfully deleted'
+    }), 200)
+
+    # return jsonify(make_response(f"Task #{task.id} successfully deleted")), 200
 
 
