c61c1a7c69512e6676d14d56a60866dc83bda49f
Hannah <hannah@chandleys.com>
2023-05-09 14:17:24 -0700

    Stash restore commit

--- app/models/task.py
+++ app/models/task.py
@@ -2,15 +2,24 @@ from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, default=None)
 
     def to_dict(self):
         return {
-            "task_id": self.task_id,
+            "task_id": self.id,
             "title": self.title,
             "description": self.description,
             "completed_at": self.completed_at
         }
+
+    @classmethod
+    def from_dict(cls, task_details):
+        new_task = cls(
+            title=task_details["title"],
+            description=task_details["description"],
+            completed_at=task_details["completed_at"]
+        )
+        return new_task
--- app/routes/tasks_routes.py
+++ app/routes/tasks_routes.py
@@ -1,9 +1,22 @@
-from flask import Blueprint, jsonify, make_response, request
+from flask import Blueprint, jsonify, abort, make_response, request
 from app.models.task import Task
+# from app.routes.helper_functions import get_valid_item_by_id
 from app import db
 
 # All routes for tasks start with "/tasks" URL prefix
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+
+def get_valid_item_by_id(model, id):
+    try:
+        id = int(id)
+    except:
+        abort(make_response({'msg': f"Invalid id '{id}'"}, 400))
+
+    item = model.query.get(id)
+
+    return item if item else abort(make_response({'msg': f"No {model.__name__} with id {id}"}, 404))
+
 
 @tasks_bp.route("", methods=["GET"])
 def handle_get_animals_request():
@@ -12,3 +25,44 @@ def handle_get_animals_request():
     for task in tasks:
         task_response.append(task.to_dict())
     return jsonify(task_response), 200
+
+@tasks_bp.route("", methods=['POST'])
+def create_task():
+    request_body = request.get_json()
+
+    new_task = Task.from_dict(request_body)
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    # Check whether task "is_complete"
+    is_complete = False
+    if new_task.to_dict()["completed_at"]:
+        is_complete = True
+
+    return {
+        "task": {
+            "id": new_task.id,
+            "title": new_task.title,
+            "description": new_task.description,
+            "is_complete": is_complete
+        }
+    }, 201
+
+@tasks_bp.route("/<task_id>", methods=["GET"])
+def handle_task(task_id):
+    task = get_valid_item_by_id(Task, task_id)
+    task = task.to_dict()
+
+    is_complete = False
+    if task["completed_at"]:
+        is_complete = True
+
+    return [
+        {
+            "id": task.id,
+            "title": task.title,
+            "description": task.description,
+            "is_complete": is_complete
+        }
+    ], 200
