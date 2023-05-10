51d327d7fb3dafa33d1b1b4b33f30daef62c6650
Aiyana Ball <ballaiyana@gmail.com>
2023-05-10 13:28:11 -0700

    added to_dict functions

--- app/models/goal.py
+++ app/models/goal.py
@@ -3,3 +3,18 @@ from app import db
 
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+
+    def to_dict(self):
+        return {
+            "id": self.goal_id,
+            "title": self.title
+        }
+
+    def to_dict_goal(self):
+        return {
+            "goal": {
+            "id": self.goal_id,
+            "title": self.title
+            }
+        }
