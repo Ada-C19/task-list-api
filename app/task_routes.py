cec2d56db86019e70a876ec0b3baae4bde29cd4e
Abby Castillo <abby.machines@gmail.com>
2023-05-09 21:04:06 -0700

    implement delete_task endpoint

--- app/task_routes.py
+++ app/task_routes.py
@@ -66,3 +66,12 @@ def update_task(task_id):
     db.session.commit()
     
     return task.to_dict()
+
+@tasks_bp.route("/<task_id>", methods=["DELETE"])
+def delete_task(task_id):
+    task = validate_task(task_id)
+    
+    db.session.delete(task)
+    db.session.commit()
+    
+    return make_response({"details": f'Task {task.task_id} "{task.title}" successfully deleted'})
