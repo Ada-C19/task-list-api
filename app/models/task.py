b1848b0be436acabae3cf1e17f938d91fa21cdda
Niambi <niambiada19@gmail.com>
2023-05-10 14:01:04 -0700

    Created enpoints in routes.py updated model in task.py and passed tests in test_wave_01.py

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,25 @@
 from app import db
 
-
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    completed_at = db.Column(db.DateTime, nullable=True) #null=None 
+    
+    def to_dict(self):
+        if Task.completed_at is True:
+            task_as_dict["is_completed"] == True
+        else:  
+            task_as_dict = {}
+            task_as_dict["id"] = self.task_id
+            task_as_dict["title"] = self.title
+            task_as_dict["description"] = self.description
+            task_as_dict["is_complete"] = False
+
+            return task_as_dict
+
+    @staticmethod
+    def from_dict(task):
+            new_task = Task(title=task["title"],
+                            description=task["description"])
+            return new_task
--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,89 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, abort, make_response, request
+from app.models.task import Task
+from app import db
+
+task_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
+
+def validate_task(task_id):
+    try:
+        task_id = int(task_id)
+    except:
+        abort(make_response({"message":f"Task {task_id} invalid"}, 400))
+    
+    task = Task.query.get(task_id)
+    
+    if not task:
+        abort(make_response({"message":f"Task {task_id} not found"}, 404))
+    return task
+
+#CREATE TASK
+
+@task_bp.route("", methods=["POST"])
+def create_task():
+    request_body = request.get_json()
+
+    try:
+        new_task = Task.from_dict(request_body)
+        new_task.description == "Test Description"
+        new_task.completed_at == None
+    except:    
+        abort(make_response({"details": "Invalid data"}, 400))
+
+    db.session.add(new_task)
+    db.session.commit()
+    
+    return {"task":new_task.to_dict()}, 201
+
+@task_bp.route("", methods=["GET"])
+def get__all_tasks():
+    title_query = request.args.get("title")
+    if title_query == "asc":
+        tasks = Task.query.order_by(Task.title.asc)
+    
+    else:
+        tasks = Task.query.all()
+
+    task_response = []
+    for task in tasks:
+        task_response.append(task.to_dict())
+    return jsonify(task_response)
+
+
+@task_bp.route("/<task_id>", methods=["GET"])
+def get_one_task(task_id):
+    task = validate_task(task_id)
+    return {"task": task.to_dict()}
+    
+
+#UPDATE TASK
+@task_bp.route("/<task_id>", methods=["PUT"])
+def update_task(task_id):
+    task = validate_task(task_id)
+
+    request_body = request.get_json()
+    task.title = request_body["title"]
+    task.description = request_body["description"]
+
+    #db.session.add(task)
+    db.session.commit()
+    
+    return make_response({
+            "task": {
+            "id": 1,
+            "title": "Updated Task Title",
+            "description": "Updated Test Description",
+            "is_complete": False
+        }
+        })
+
+
+#DELETE TASK
+@task_bp.route("/<task_id>", methods=["DELETE"])
+def delete_task(task_id):
+    task = validate_task(task_id)
+
+    db.session.delete(task)
+    db.session.commit()
+
+    return make_response({"details": "Task 1 \"Go on my daily walk 🏞\" successfully deleted"
+})
