a62e537aa619e0493b90462fcd28ab74bc8c6c9a
Carline <carlinesenat@gmail.com>
2023-05-10 10:06:53 -0700

    refactoring for wave 1 + tests

--- app/models/task.py
+++ app/models/task.py
@@ -5,5 +5,13 @@ class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
-    completed_at = db.Column(db.DateTime, nullable=False)
+    completed_at = db.Column(db.DateTime, nullable=True)
+
+    def to_dict(self):
+        return dict(
+            id = self.task_id,
+            title = self.title,
+            description = self.description,
+            is_complete = self.completed_at is not None
+        )
 
--- app/routes.py
+++ app/routes.py
@@ -4,64 +4,72 @@ from app.models.task import Task
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 
-# class Task:
-#     def __init__(self, id, title, description, completed_at):
-#         self.id = id
-#         self.title = title
-#         self.description = description
-#         self.completed_at = completed_at
-
-# tasks = [
-#     Task(1, "Take the dog out", "Dog needs to go pee + poo thrice a day.", "Completed"),
-#     Task(2, "Supplemental Studies", "We need to stay ahead of the game! Get studying.", "Completed"),
-#     Task(3, "Sweep the floors", "Not just sweep the kitchen + common area, but also vaccuum the rooms.", "Not Complete")
-# ] 
-
 #VALIDATE TASKS
-# def validate_task(task_id):
-#     try:
-#         task_id = int(task_id)
-#     except:
-#         abort(make_response({"message":f"book {task_id} invalid"}, 400))
+def validate_task(task_id):
+    try:
+        task_id = int(task_id)
+    except:
+        abort(make_response({"message":f"book {task_id} invalid"}, 400))
     
-#     for task in tasks:
-#         if task.id == task_id:
-#             return task
-#     abort(make_response({"message":"There's no {task_id} sorry."}))
+    task = Task.query.get(task_id)
+
+    if not task:
+        abort(make_response({"message":"There's no {task_id} sorry."}))
+    return task
+
 
 #POST endpoint CREATES A NEW TASK
 @tasks_bp.route("", methods=["POST"])
 def create_task():
-    request_body = request.get_json()
-    new_task = Task(title=request_body["title"],
-                    description=request_body["description"])
+        request_body = request.get_json()
+        new_task = Task(title=request_body["title"],
+                        description=request_body["description"],
+                        is_complete=request_body["Completed?"])
 
-    
-    db.session.add(new_task)
-    db.session.commit()
+        
+        db.session.add(new_task)
+        db.session.commit()
 
-    return make_response(f"Made a new task: {new_task.title}", 201)
+        return make_response(f"Made a new task: {new_task.title}", 201)
 
 #GET THE TASKS
-# @tasks_bp.route("", methods=["GET"])
-# def get_tasks():
-#     task_force = []
-#     for task in tasks:
-#         task_force.append({
-#             "id": task.id,
-#             "title": task.title,
-#             "description": task.description,
-#             "completed at":task.completed_at
-#         })
-#     return jsonify(task_force)
+@tasks_bp.route("", methods=["GET"])
+def get_tasks():
+    tasks = Task.query.all()
+    task_force = [task.to_dict for task in tasks]
+        # task_force.append({
+        #     "id": task.id,
+        #     "title": task.title,
+        #     "description": task.description,
+        #     "completed at":task.completed_at
+        # })
+    return make_response(jsonify(task_force), 200)
 
-# @tasks_bp.route("/<task_id>", methods=["GET"])
-# def handle_tasks(task_id):
-#     task = validate_task(task_id)
+@tasks_bp.route("/<task_id>", methods=["GET"])
+def handle_tasks(task_id):
+    task = validate_task(task_id)
     
-#     return {
-#         "id": task.id,
-#         "title": task.title,
-#         "description": task.description,
-#         "completed at":task.completed_at
-#         }
+    return task.to_dict(), 200
+
+#Update a task
+@tasks_bp.route("/<task_id>", methods=["PUT"])
+def update_task(task_id):
+    task = validate_task(task_id)
+
+    request_body = request.get_json()
+    task.title = request_body["title"]
+    task.description = request_body["description"]
+
+    db.session.commit()
+
+    return make_response(f"The task {task.id} was updated!!")
+
+#DELETES A TASK
+@tasks_bp.route("/<task_id>", methods=["DELETE"])
+def delete_task(task_id):
+    task = validate_task(task_id)
+
+    db.session.delete(task)
+    db.session.commit()
+
+    return make_response(f"The task {task.title} was deleted!")
