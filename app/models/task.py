7043ebad2fd66cd0dffc842f0e944ada262364b0
rmora101 <59456883+rmora101@users.noreply.github.com>
2023-05-10 14:35:24 -0700

    added to the function

--- app/models/task.py
+++ app/models/task.py
@@ -1,4 +1,5 @@
 from app import db
+from app.models import goal
 
 
 class Task(db.Model):
@@ -6,15 +7,17 @@ class Task(db.Model):
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, default= None) #something about has to be null
-    goal_id = db.Column(db.Integer, db.ForeignKey('goal.goal_id'))
+    goal_id = db.Column(db.Integer, db.ForeignKey('goal.goal_id'), nullable = True)
     goal = db.relationship("Goal", back_populates="tasks")
 
     def to_dict(self):
         return {
             "id": self.task_id,
+            "goal_id" : self.goal_id is not None,
             "title" : self.title,
             "description" : self.description,
-            "is_complete" : self.completed_at is not None
+            "is_complete" : self.completed_at is not None # might break code
+    
         }
     
 
