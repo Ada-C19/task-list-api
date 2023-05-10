15792a9ed9d379469148cf002d551abc05f5e413
Mikayla Howden <MDHowden2020@gmail.com>
2023-05-09 17:56:03 -0700

    Modified tests to include additional assert statements for Wave 1

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -51,7 +51,7 @@ def test_get_task(client, one_task):
     }
 
 
-@pytest.mark.skip(reason="No way to test this feature yet")
+# @pytest.mark.skip(reason="No way to test this feature yet")
 def test_get_task_not_found(client):
     # Act
     response = client.get("/tasks/1")
@@ -59,8 +59,7 @@ def test_get_task_not_found(client):
 
     # Assert
     assert response.status_code == 404
-
-    raise Exception("Complete test with assertion about response body")
+    assert response_body == {"msg":"Task 1 not found"}
     # *****************************************************************
     # **Complete test with assertion about response body***************
     # *****************************************************************
@@ -119,7 +118,7 @@ def test_update_task(client, one_task):
     assert task.completed_at == None
 
 
-@pytest.mark.skip(reason="No way to test this feature yet")
+# @pytest.mark.skip(reason="No way to test this feature yet")
 def test_update_task_not_found(client):
     # Act
     response = client.put("/tasks/1", json={
@@ -130,8 +129,7 @@ def test_update_task_not_found(client):
 
     # Assert
     assert response.status_code == 404
-
-    raise Exception("Complete test with assertion about response body")
+    assert response_body == {"msg":"Task 1 not found"}
     # *****************************************************************
     # **Complete test with assertion about response body***************
     # *****************************************************************
@@ -152,7 +150,7 @@ def test_delete_task(client, one_task):
     assert Task.query.get(1) == None
 
 
-@pytest.mark.skip(reason="No way to test this feature yet")
+# @pytest.mark.skip(reason="No way to test this feature yet")
 def test_delete_task_not_found(client):
     # Act
     response = client.delete("/tasks/1")
@@ -160,8 +158,7 @@ def test_delete_task_not_found(client):
 
     # Assert
     assert response.status_code == 404
-
-    raise Exception("Complete test with assertion about response body")
+    assert response_body == {"msg":"Task 1 not found"}
     # *****************************************************************
     # **Complete test with assertion about response body***************
     # *****************************************************************
