4946b0f5d839ff7f0c9c0349415a4a0e603f4195
Hannah <hannah@chandleys.com>
2023-05-09 22:11:09 -0700

    Added assert statements to remaining tests in wave_3

--- tests/test_wave_03.py
+++ tests/test_wave_03.py
@@ -115,7 +115,6 @@ def test_mark_incomplete_on_incomplete_task(client, one_task):
     assert Task.query.get(1).completed_at == None
 
 
-@pytest.mark.skip(reason="No way to test this feature yet")
 def test_mark_complete_missing_task(client):
     # Act
     response = client.patch("/tasks/1/mark_complete")
@@ -123,14 +122,9 @@ def test_mark_complete_missing_task(client):
 
     # Assert
     assert response.status_code == 404
+    # *** Added asset statement below *** #
+    assert response_body == {'msg': "No Task with id 1"}
 
-    raise Exception("Complete test with assertion about response body")
-    # *****************************************************************
-    # **Complete test with assertion about response body***************
-    # *****************************************************************
-
-
-@pytest.mark.skip(reason="No way to test this feature yet")
 def test_mark_incomplete_missing_task(client):
     # Act
     response = client.patch("/tasks/1/mark_incomplete")
@@ -138,8 +132,5 @@ def test_mark_incomplete_missing_task(client):
 
     # Assert
     assert response.status_code == 404
-
-    raise Exception("Complete test with assertion about response body")
-    # *****************************************************************
-    # **Complete test with assertion about response body***************
-    # *****************************************************************
+    # *** Added asset statement below *** #
+    assert response_body == {'msg': "No Task with id 1"}
