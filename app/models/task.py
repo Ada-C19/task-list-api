cff0cd62bcba1df0b70ba42c76530c1b1029581b
TombAnneX <109308405+AKTuminello@users.noreply.github.com>
2023-05-10 06:50:45 -0600

    All Wave 1 Tests Passing

--- app/models/task.py
+++ app/models/task.py
@@ -2,9 +2,9 @@ from app import db
 
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(255), nullable=False)
-    description = db.Column(db.String(), nullable=False)
-    completed_at = db.Column(db.DateTime(), nullable=True)
+    title = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String(50), nullable=False)
+    completed_at = db.Column(db.DateTime, nullable=True, default=None)
 
     def to_dict(self):
         return {
--- app/routes_helpers.py
+++ app/routes_helpers.py
@@ -1,4 +1,4 @@
-from flask import abort, make_response
+from flask import abort, make_response, jsonify
 from app.models.task import Task
 
 
@@ -6,11 +6,17 @@ def validate_model(cls, model_id):
     try:
         model_id = int(model_id)
     except:
-        abort(make_response({"message": f"{cls.__name__} {model_id} invalid"}, 400))
+        abort(make_response({"details": f"{cls.__name__} {model_id} invalid"}, 400))
 
     model = cls.query.get(model_id)
 
     if not model:
-        abort(make_response({"message": f"{cls.__name__} {model_id} not found"}, 404))
+        abort(make_response({"details": f"{cls.__name__} not found"}, 404))
 
     return model
+
+
+def respond_with_json(data, status_code=200):
+    response = jsonify(data)
+    response.status_code = status_code
+    return response
