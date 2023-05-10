cbe6b6525da404ec908ce9a9f387e7d3dd30bd6f
Sage Choi <coolcuds@gmail.com>
2023-05-10 15:04:00 -0700

    Modified details string

--- app/routes/goal_routes.py
+++ app/routes/goal_routes.py
@@ -59,7 +59,7 @@ def delete_goal(goal_id):
     db.session.delete(goal)
     db.session.commit()
 
-    return {"details": f"Goal {goal.goal_id} \"{goal.title}\" successfully deleted"}, 200
+    return {"details": f'Goal {goal.goal_id} "{goal.title}" successfully deleted'}, 200
 
 
 @goals_bp.route("/<goal_id>/tasks", methods=["GET"])
