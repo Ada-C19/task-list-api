625d48197985f7f0b38eba29c6d8eec87a4d9d02
Charlie <charliethemushi@gmail.com>
2023-05-10 15:41:19 -0700

    setup tokens for Render

--- app/__init__.py
+++ app/__init__.py
@@ -16,13 +16,13 @@ def create_app(test_config=None):
 
     if test_config is None:
         app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-            "SQLALCHEMY_DATABASE_URI")
+            "RENDER_DB")
     else:
         app.config["TESTING"] = True
         app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
             "SQLALCHEMY_TEST_DATABASE_URI")
 
-    api_token = os.environ.get("API_TOKEN")
+    #api_token = os.environ.get("API_TOKEN")
     # Import models here for Alembic setup
     from app.models.task import Task
     from app.models.goal import Goal
