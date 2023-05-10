e316d5200dad0659e488c4d0514b8bed5e543847
Mikayla Howden <MDHowden2020@gmail.com>
2023-05-09 17:47:20 -0700

    Added DELETE request to goals route

--- app/routes.py
+++ app/routes.py
@@ -184,3 +184,12 @@ def update_goal(goal_id):
 
     db.session.commit()
     return {"goal": goal.to_dict()}, 200
+
+@goal_bp.route("/<goal_id>", methods=["DELETE"])
+def delete_goal(goal_id):
+    goal = validate_item(Goal, goal_id)
+    
+    db.session.delete(goal)
+    db.session.commit()
+
+    return {"details": f'Goal {goal_id} "{goal.title}" successfully deleted'}, 200
