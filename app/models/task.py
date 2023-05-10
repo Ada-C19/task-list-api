71e5df5620e4328fbad102778efa1271614401b4
Kira Sidhu <kira.kaur.sidhu@gmail.com>
2023-05-10 13:43:19 -0700

    Create Task model in app/task.py, created CRUD routes in app/routes.py and implement and pass tests in tests/test_wave_01

--- app/models/task.py
+++ app/models/task.py
@@ -2,4 +2,27 @@ from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement = True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    completed_at = db.Column(db.DateTime)
+
+    def to_dict(self):
+        if self.completed_at is None:
+            completed = False
+        else:
+            completed = True
+        return {
+            "id": self.task_id,
+            "title": self.title,
+            "description": self.description,
+            "is_complete": completed
+        }
+
+    @classmethod
+    def from_dict(cls, task_data):
+        return cls(
+            title = task_data["title"],
+            description = task_data["description"],
+            completed_at = task_data["completed_at"] if "completed_at" in task_data else None
+        )
--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,71 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request, abort, make_response
+from app import db
+from app.models.task import Task
+
+task_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+def validate_item(model, item_id):
+    try:
+        item_id = int(item_id)
+    except ValueError:
+        return abort(make_response({"details": "Invalid data"}, 400))
+    
+    return model.query.get_or_404(item_id)
+
+
+@task_bp.route("", methods=["POST"])
+def create_task():
+    request_body = request.get_json()
+    if "title" not in request_body or "description" not in request_body:
+        abort(make_response({"details": "Invalid data"}, 400))
+    new_task = Task.from_dict(request_body)
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return {"task": new_task.to_dict()}, 201
+
+@task_bp.route("", methods=["GET"])
+def get_tasks():
+    response = []
+    name_query = request.args.get("name")
+
+    if name_query is None:
+        all_tasks = Task.query.all()
+    else:
+        all_tasks = Task.query.filter_by(name=name_query)
+
+    for task in all_tasks:
+        response.append(task.to_dict())
+
+    return jsonify(response), 200
+
+@task_bp.route("/<task_id>", methods=["GET"])
+def get_one_task(task_id):
+    task = validate_item(Task, task_id)
+
+    return {"task": task.to_dict()}, 200
+
+@task_bp.route("/<task_id>", methods=["PUT"])
+def update_task(task_id):
+    task = validate_item(Task, task_id)
+
+    request_data = request.get_json()
+
+    task.title = request_data["title"]
+    task.description = request_data["description"]
+    if "completed_at" in request_data:
+        task.completed_at = request_data["completed_at"]
+
+    db.session.commit()
+
+    return {"task": task.to_dict()}, 200
+
+@task_bp.route("/<task_id>", methods=["DELETE"])
+def delete_task(task_id):
+    task = validate_item(Task, task_id)
+
+    db.session.delete(task)
+    db.session.commit()
+
+    return {"details": f'Task {task_id} "{task.title}" successfully deleted'}, 200
