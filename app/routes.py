40a9883c47541b66f7b9782efa94968ec289f4a9
fabiola gonzalez <gonzalezfabs@gmail.com>
2023-05-10 12:24:28 -0700

    Revert "All tests in Wave 1 pass, need to verify with Postman"
    This reverts commit 1fe0d3d20e639a69ee77ef2394353cfbb037ed81.

--- app/routes.py
+++ app/routes.py
@@ -6,7 +6,7 @@ from app.helper import validate_task
 #CREATE BP/ENDPOINT
 tasks_bp = Blueprint("tasks",__name__, url_prefix="/tasks")
 
-#GET ALL tasks [GET]/tasks :(CREATE)
+# GET all Tasks - /tasks - (CREATE)
 @tasks_bp.route("",methods=["GET"])
 def get_all_tasks():
     title_query = request.args.get("title")
@@ -29,22 +29,18 @@ def get_all_tasks():
     return jsonify(tasks_response),200
 
 
-#GET one task [GET]/tasks/<id> :(CREATE)
+# GET one Tasks - /tasks/<id> - (CREATE)
 @tasks_bp.route("/<id>",methods=["GET"])
 def get_one_task(id):
     task = validate_task(id)
 
     return jsonify({'task':task.to_dict()}),200
 
-#CREATE one task/must contain title+description [POST]/tasks/<id> :(CREATE)
+#CREATE one task -POST /tasks/<id> - (CREATE)
 @tasks_bp.route("",methods=["POST"])
 def create_task():
     request_body = request.get_json()
-    if request.method == "POST":
-        if "name" not in request_body or "description" not in request_body:
-            message = {"details": "Invalid data"}
-            return make_response(message,400)
-           
+        
     new_task = Task.create_dict(request_body)
 
     db.session.add(new_task)
@@ -52,10 +48,10 @@ def create_task():
 
     return make_response({"task":new_task.to_dict()}), 201
 
-#UPDATE one task/RETURN msg not found [PUT]/tasks/<id> :(UPDATE)
+#UPDATE a task/ RETURN msg not found -PUT / tasks/<id> - (UPDATE)
 @tasks_bp.route("/<id>",methods=["PUT"])
 def update_task(id):
-    task = Task.query.get(id) #SQLA used to retrieve row from db tbl to update/delete
+    task = Task.query.get(id)
     if task is None:
         return jsonify({'error': 'Task not found'}),404
     
@@ -66,7 +62,7 @@ def update_task(id):
     return jsonify({'task':task.to_dict()}),200
 
 
-#DELETE one task [DELETE]/tasks/<id> :(DELETE)
+#DELETE one task -DELETE /tasks/<id> - (DELETE)
 @tasks_bp.route("/<id>",methods=["DELETE"])
 def delete_task(id):
     task_to_delete = validate_task(id)
@@ -74,17 +70,9 @@ def delete_task(id):
     db.session.delete(task_to_delete)
     db.session.commit()
 
-    message = {'details': f'Task {task_to_delete.id} "{task_to_delete.title}" successfully deleted'}
+    message = {'details': f'Task 1 "{task_to_delete.title}" successfully deleted'}
 
     return make_response(message,200)
-
-
-
-
-
-
-
-
 
 #CREATE task must contain title/description - POST /tasks/<id> - (CREATE)
 # @tasks_bp.route("",methods=["POST"])
