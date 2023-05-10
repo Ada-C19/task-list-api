c9015cca07ae714ded60791c44501db5ab26c727
alyssa <tn.alyssa@gmail.com>
2023-05-09 21:07:41 -0500

    implemented PUT and DELETE routes, wrote tests for wave 5

--- app/goal_routes.py
+++ app/goal_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, abort, make_response, jsonify
 from app.models.goal import Goal
 from app import db
+from .task_routes import validate_model
 
 goals_bp = Blueprint("goals_bp", __name__, url_prefix="/goals")
 
@@ -29,7 +30,34 @@ def list_all_goals():
     return jsonify(goals_response)
 
 
+@goals_bp.route("/<goal_id>", methods=["GET"])
+def list_specific_goal(goal_id):
+    goal = validate_model(Goal, goal_id)
+    return make_response({"goal": goal.to_dict()})
 
+
+# Update
+@goals_bp.route("<goal_id>", methods=["PUT"])
+def update_goal(goal_id):
+    goal = validate_model(Goal, goal_id)
+    request_body = request.get_json()
+
+    goal.title = request_body["title"]
+
+    db.session.commit()
+
+    return make_response({"goal": goal.to_dict()})
+
+
+# Delete
+@goals_bp.route("/<goal_id>", methods=["DELETE"])
+def delete_goal(goal_id):
+    goal = validate_model(Goal, goal_id)
+
+    db.session.delete(goal)
+    db.session.commit()
+
+    return make_response({"details": f"Goal {goal.id} \"{goal.title}\" successfully deleted"})
 
 # Helper Functions
 def check_goal_data(request):
