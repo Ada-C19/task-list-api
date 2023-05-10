a77b2cf4bb160ae78a59af256f0a791dc759b6cd
Stacey <stacey.l.joseph@gmail.com>
2023-05-09 21:28:14 -0400

    create task route complete

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,12 @@
 from app import db
 
+import datetime
+# from datetime import date
+from sqlalchemy import Column, Integer, DateTime
+
 
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    completed_at = db.Column(db.DateTime, default=None, nullable=True)
--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,27 @@
-from flask import Blueprint
+from app import db
+
+from flask import Blueprint, jsonify, make_response, request
+
+from app.models.task import Task
+
+# creating the task blueprint
+tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+# CREATE TASK
+@tasks_bp.route("", methods=["POST"])
+def create_task():
+    request_body = request.get_json()
+    new_task = Task(title=request_body["title"],
+                    description=request_body["description"], 
+                    completed_at=request_body["completed_at"])
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    # return make_response(f"Task {new_task.title} successfully created", 201)
+    return {"task": {
+        "id": new_task.task_id,
+        "title": new_task.title,
+        "description": new_task.description,
+        "is_complete": True if new_task.completed_at else False  
+    }}, 201
