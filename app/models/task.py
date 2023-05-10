e39e3f25695bf04d8b43450559f974f1a7588c9f
Amber Shay <mashaad21@gmail.com>
2023-05-10 12:04:27 -0600

    create task returns task dictionary

--- app/models/task.py
+++ app/models/task.py
@@ -7,15 +7,15 @@ class Task(db.Model):
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, default=None) # set default value to <null>
-    is_complete = db.Column(db.String, unique=False)    # designate as not completed
+    is_complete = db.Column(db.String, default=False)    # designate as not completed
 
     @classmethod
     def task_from_dict(cls, task_data):
         """Input task as a dictionary. Assumes None/null for completed_at"""
+
         new_task = Task(title=task_data["title"],
                         description=task_data["description"],
                         completed_at=task_data["completed_at"])
-                        # is_complete=task_data["is_complete"]
         return new_task
     
     def task_to_dict(self):
@@ -25,13 +25,13 @@ class Task(db.Model):
         task_as_dict["task_id"] = self.task_id
         task_as_dict["title"] = self.title
         task_as_dict["description"] = self.description
-        task_as_dict["is_complete"] = self.task_complete()
+        task_as_dict["is_complete"] = self.is_complete
 
         return task_as_dict
     
-    def task_complete(self):
-        if self.completed_at == None:
-            self.is_complete = False
-        else:
-            self.is_complete = True
+    # def task_complete(self):
+    #     if self.completed_at == None:
+    #         self.is_complete = False
+    #     else:
+    #         self.is_complete = True
     
