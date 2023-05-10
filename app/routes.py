ba69e1956a3d6bbb69fa83932ace5e6e2660e588
kathleen <kathleen.n.shin@gmail.com>
2023-05-10 11:09:35 -0700

    Created goal CRUD routes

--- app/routes.py
+++ app/routes.py
@@ -125,3 +125,70 @@ def update_task_incomplete(task_id):
     db.session.commit()
 
     return {"task": task.to_dict()}, 200
+
+@goals_bp.route("", methods=["POST"])
+def create_goal():
+    request_body = request.get_json()
+
+    try:
+        new_goal = Goal.from_dict(request_body)
+    except:
+        abort(make_response({"details": "Invalid data"}, 400))
+
+    db.session.add(new_goal)
+    db.session.commit()
+
+    return {"goal": new_goal.to_dict()}, 201
+
+@goals_bp.route("", methods=['GET'])
+def handle_goals():
+    title_query = request.args.get("sort")
+
+    if title_query:
+        if title_query == "asc":
+            goals = Goal.query.order_by(Goal.title.asc())
+        elif title_query == "desc":
+            goals = Goal.query.order_by(Goal.title.desc())
+    else:
+        goals = Goal.query.all()
+    goals_response = [goal.to_dict() for goal in goals]
+
+    return jsonify(goals_response), 200
+
+def get_valid_item_by_id(model, id):
+    try:
+        id = int(id)
+    except:
+        abort(make_response({"message":f"Goal {id} invalid"}, 400))
+
+    item = model.query.get(id)
+
+    return item if item else abort(make_response({"message":f"Goal {id} not found"}, 404))
+
+@goals_bp.route("/<goal_id>", methods=["GET"])
+def handle_goal(goal_id):
+    goal = get_valid_item_by_id(Goal, goal_id)
+    goal.query.get(goal_id)
+
+    return {"goal":{
+        "id": goal.goal_id,
+        "title": goal.title}}, 200
+
+@goals_bp.route("/<goal_id>", methods=["PUT"])
+def update_goal(goal_id):
+    goal = get_valid_item_by_id(Goal, goal_id)
+    request_body = request.get_json()
+
+    goal.title = request_body["title"]
+
+    db.session.commit()
+
+    return {"goal":goal.to_dict()},200
+
+@goals_bp.route("/<goal_id>", methods=["DELETE"])
+def delete_goal(goal_id):
+    goal = get_valid_item_by_id(Goal, goal_id)
+    db.session.delete(goal)
+    db.session.commit()
+
+    return {"details": f"goal {goal_id} \"{goal.title}\" successfully deleted"}
