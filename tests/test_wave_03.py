4b84646c553a4e157359e09af215acf015563f0b
Janice <janicenguyenn@gmail.com>
2023-05-09 20:42:38 -0700

    wrote assert statements for response_body for missing tasks

--- tests/test_wave_03.py
+++ tests/test_wave_03.py
@@ -128,11 +128,11 @@ def test_mark_complete_missing_task(client):
     # Assert
     assert response.status_code == 404
 
-    raise Exception("Complete test with assertion about response body")
+    # raise Exception("Complete test with assertion about response body")
     # *****************************************************************
     # **Complete test with assertion about response body***************
     # *****************************************************************
-
+    assert response_body == {"message":"task 1 not found"}
 
 @pytest.mark.skip(reason="No way to test this feature yet")
 def test_mark_incomplete_missing_task(client):
@@ -143,7 +143,8 @@ def test_mark_incomplete_missing_task(client):
     # Assert
     assert response.status_code == 404
 
-    raise Exception("Complete test with assertion about response body")
+    # raise Exception("Complete test with assertion about response body")
     # *****************************************************************
     # **Complete test with assertion about response body***************
     # *****************************************************************
+    assert response_body == {"message":"task 1 not found"}
