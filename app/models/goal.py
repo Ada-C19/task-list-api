54bd9528a0a04d0b04cbed329c6d2e0291f4aafe
Carolyn Qi <qi.carolyn@gmail.com>
2023-05-09 17:25:36 -0700

    Add class Goal and Post and Get all goals request

--- app/models/goal.py
+++ app/models/goal.py
@@ -4,3 +4,15 @@ from app import db
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
+
+    def to_dict(self):
+        return {
+            "id": self.goal_id, 
+            "title": self.title
+        }
+    
+    @classmethod
+    def from_dict(cls, task_data):
+        return cls(
+            title = task_data["title"]
+        )
