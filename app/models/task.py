c3c06ce31e22c7e002cba8de5986fbab5e8bbb9b
Sarah Kim <kimsuyon2013@gmail.com>
2023-05-09 19:41:41 -0700

    established relationship to goal and created a foreign key for goal id

--- app/models/task.py
+++ app/models/task.py
@@ -2,14 +2,16 @@ from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, default=None)
+    goal_id = db.Column(db.Integer, db.ForeignKey('goal.id'))
+    goal = db.relationship("Goal", back_populates="tasks")
 
     def to_dict(self): 
         return {
-            "id":self.task_id,
+            "id":self.id,
             "title": self.title,
             "description": self.description,
             "is_complete": self.completed_at is not None 
