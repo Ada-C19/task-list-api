5891109cd90573e2f07a0390e9ef9deab1e86e01
Charlie <charliethemushi@gmail.com>
2023-05-10 16:48:03 -0700

    created dict instance and classmethod in goal models

--- app/models/goal.py
+++ app/models/goal.py
@@ -4,3 +4,16 @@ from app import db
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
+
+    def to_dict(self):
+            goal_as_dict = {}
+            goal_as_dict["id"] = self.goal_id 
+            goal_as_dict["title"] = self.title
+        
+            return goal_as_dict
+        
+    @classmethod
+    def from_dict(cls, goal_data):
+        new_goal = Goal(id=goal_data["id"],
+                        title=goal_data["title"])
+        return new_goal
