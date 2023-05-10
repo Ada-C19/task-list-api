0c9a2bc497fccb450712c4b331f3a05169a89d69
Stacey <stacey.l.joseph@gmail.com>
2023-05-10 10:21:10 -0400

    CREATE POST route fixed

--- app/models/task.py
+++ app/models/task.py
@@ -6,7 +6,7 @@ from sqlalchemy import Column, Integer, DateTime
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, default=None, nullable=True)
