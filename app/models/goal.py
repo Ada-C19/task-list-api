5ee4db8e87c55f204250664defad98a528e556cc
Ariel Williams <arielwilliams121013@gmail.com>
2023-05-10 13:22:50 -0700

    In app/models/goal.py and app/models/task.py, created a one-to-many db.relationship between Goal and Task. Created a goal_id as a foreign key in the Task class. Wrote some comments to help remind me what I have done.

--- app/models/goal.py
+++ app/models/goal.py
@@ -1,9 +1,15 @@
 from app import db
 
-
+# "One" side
+# one Goal has many Tasks
+# tasks looks at the class Task and finds the value for attribute/column "goal"
+# tasks attribute is pluralized bc a singular goal HAS MANY tasks
+# db.relationship creates the join table
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
+    tasks = db.relationship("Task", back_populates="goal")
+    
 
     # powerful takeaway: you can call a function on a value in a dict!
     def to_dict(self):
@@ -11,7 +17,6 @@ class Goal(db.Model):
             "id": self.goal_id,
             "title": self.title
         }
-    
     
 
     @classmethod
--- app/models/task.py
+++ app/models/task.py
@@ -1,11 +1,19 @@
 from app import db
 
-
+# "Many" side 
+# Task has one goal
+# Declaring foreign key on the many side 
+# db.relationship creates the join table
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable=True)
+    
+    goal_id = db.Column.Integer, db.ForeignKey("goal.id")
+    goal = db.relationship("Goal", back_populates="tasks")
+    
+
 
     # powerful takeaway: you can call a function on a value in a dict!
     def to_dict(self):
@@ -16,7 +24,6 @@ class Task(db.Model):
             "is_complete": bool(self.completed_at)
         }
     
-
 
     @classmethod
     def from_dict(cls, task_data):
