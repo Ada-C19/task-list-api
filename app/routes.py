f2df6b3bf8039cb64996e054d0d222dc9d927379
nadia <reach.nadia.saria@gmail.com>
2023-05-10 11:38:04 -0700

    implements validate_model

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,19 @@ from flask import Blueprint, jsonify, abort, make_response, request
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 
+def validate_model(cls, model_id):
+    try:
+        model_id = int(model_id)
+    except:
+        abort(make_response(jsonify({"message":f"{cls.__name__} {model_id} invalid"}), 400))
+    
+    model = cls.query.get(model_id)
+
+    if not model:
+        abort(make_response(jsonify({"message":f"{cls.__name__} {model_id} not found"}), 404))
+    
+    return model
+
 @tasks_bp.route("", methods=["GET"])
 def get_all_tasks():
     tasks = Task.query.all()
