975695aab5164faa24fc60595647511afa9b603a
Ariel Williams <arielwilliams121013@gmail.com>
2023-05-10 14:28:41 -0700

    In app/models/goal.py, added lazy=True to db.relationship. In app/models/task.py, added nullable=True in foreign key goal_id value.

--- app/models/goal.py
+++ app/models/goal.py
@@ -8,7 +8,7 @@ from app import db
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
-    tasks = db.relationship("Task", back_populates="goal")
+    tasks = db.relationship("Task", back_populates="goal", lazy=True)
     
 
     # powerful takeaway: you can call a function on a value in a dict!
