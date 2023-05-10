6890e9f5a86c8dda5caf49592a1b584e160425d7
Sarah Kim <kimsuyon2013@gmail.com>
2023-05-09 19:39:03 -0700

    established relationship to task

--- app/models/goal.py
+++ app/models/goal.py
@@ -2,11 +2,12 @@ from app import db
 
 
 class Goal(db.Model):
-    goal_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
+    tasks = db.relationship("Task", back_populates="goal")
 
     def to_dict(self): 
         return {
-            "id":self.goal_id,
+            "id":self.id,
             "title": self.title,
         }
