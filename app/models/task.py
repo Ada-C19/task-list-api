14e96d2a331bb642af5e7f36a449ba2596b49379
Say Ryder <lindz.ryder@gmail.com>
2023-05-09 23:33:20 -0400

    adds task model:

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,26 @@
 from app import db
-
+from flask import make_response 
 
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
+    task_title = db.Column(db.String)
+    task_description=db.Column(db.String)
+    completed_at=db.Column(db.DateTime, nullable=True)
+
+    def task_to_dict(self):
+        return {
+        "task_id": self.task_id,
+        "task_title": self.task_title,
+        "task_description": self.task_description,
+        "completed_at": self.completed_at.strftime("%Y-%m-%d %H:%M:%S")
+        if self.completed_at is not None else None
+        }
+    @classmethod 
+    def create_new_task(cls, request_data):
+        if "task_title" not in request_data or "task_description" not in request_data:
+            raise ValueError("Invalid Request. Missing required fields: name or description")
+        return cls(
+            task_title=request_data["task_title"].title(),
+            task_description=request_data["task_description"],
+            completed_at=request_data.get("completed_at")
+        )
--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,36 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, abort, make_response, request, Response
+from app import db
+from app.models.task import Task
+
+
+tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+@tasks_bp.route("", methods = ["POST"])
+def create_tasks():
+    request_body = request.get_json()
+    try:
+        new_task = Task.create_new_task(request_body)
+        db.session.add(new_task)
+        db.session.commit()
+
+        message = f"Task {new_task.task_title} successfully created"
+        return make_response(jsonify(message), 201)
+
+    except KeyError as e:
+        abort(make_response(f"Invalid request. Missing required value: {e}", 400))
+
+
+
+@tasks_bp.route("", methods = ["GET"])
+def read_all_tasks():
+    tasks_response = []
+    query_params = request.args.to_dict()
+
+    if query_params:
+        query_params = {k.lower(): v.title() for k, v in query_params.items()}
+        tasks = Task.query.filter_by(**query_params).all()
+    else:
+        tasks = Task.query.all()
+
+    tasks_response = [task.task_to_dict() for task in tasks]
+    return jsonify(tasks_response)
