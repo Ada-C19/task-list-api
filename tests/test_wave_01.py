250b75b9721db00bfe0b3b35f27a8f8cacc5afd0
Abby Castillo <abby.machines@gmail.com>
2023-05-09 21:55:52 -0700

    edit test_get_task_not_found to pass

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -60,11 +60,14 @@ def test_get_task_not_found(client):
     # Assert
     assert response.status_code == 404
 
-    raise Exception("Complete test with assertion about response body")
+    # raise Exception("Complete test with assertion about response body")
     # *****************************************************************
     # **Complete test with assertion about response body***************
     # *****************************************************************
-
+    assert "details" in response_body
+    assert response_body == {
+        "details": f"Task 1 not found"
+    }
 
 # @pytest.mark.skip(reason="No way to test this feature yet")
 def test_create_task(client):
