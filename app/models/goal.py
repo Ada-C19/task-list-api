15beffd39c71b5eaee7cec6f6bd0c34611de968f
Mikayla Howden <MDHowden2020@gmail.com>
2023-05-10 08:18:24 -0700

    Established one to many relationship with one goal and many tasks

--- app/models/goal.py
+++ app/models/goal.py
@@ -1,11 +1,12 @@
 from app import db
 from flask import jsonify
 
+# A Goal has many Tasks 
 
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
-    # tasks = db.relationship("Task", back_populates="goal")
+    tasks = db.relationship("Task", back_populates="goal")
 
     def to_dict(self):
         return {
--- app/models/task.py
+++ app/models/task.py
@@ -6,8 +6,10 @@ class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True, autoincrement = True)
     title = db.Column(db.String)
     description = db.Column(db.String)
-    # SQLAlchemy supports nullable columns with specific syntax
-    completed_at = db.Column(db.DateTime, nullable=True)
+    completed_at = db.Column(db.DateTime, nullable=True, default=None)
+    goal_id = db.Column(db.Integer, db.ForeignKey('goal.goal_id'))
+    goal = db.relationship("Goal", back_populates="tasks")
+
 
     # def to_dict(self):
     #     return jsonify (
