affd1ef6b48ba851c32e90bab03d07d8ed053dd5
mlhuynh <greenlinh@gmail.com>
2023-05-10 15:56:47 -0700

    Wrote @classmethod from_dict helper function in app method task.py module.

--- app/models/task.py
+++ app/models/task.py
@@ -9,10 +9,25 @@ class Task(db.Model):
     is_complete = db.Column(db.Boolean)
 
     def to_dict(self):
+        random_bool = True
+        if self.is_complete == True:
+            random_bool = True
+        else:
+            random_bool = False
         return dict(
             id=self.task_id,
             title=self.title,
             description=self.description,
-            completed_at=self.completed_at,
-            is_complete=self.is_complete
+            # In the Wave 1 test, they don't want completed_at in the response
+            # completed_at=self.completed_at,
+            # In the Wave 1 test, it wants is_complete to come back as True/False... in the db, this might be stored as "None" depending on the task
+            is_complete=random_bool
         )
+    
+    @classmethod
+    def from_dict(cls, task_data):
+        new_task = Task(
+            title=task_data["title"], 
+            description=task_data["description"]
+            )
+        return new_task
