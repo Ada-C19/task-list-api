e3f03861608997211dac1e70d1d364a4bee2389d
Stacey <stacey.l.joseph@gmail.com>
2023-05-10 10:46:03 -0400

    READ 1 task GET route complete

--- app/routes.py
+++ app/routes.py
@@ -4,10 +4,11 @@ from flask import Blueprint, jsonify, make_response, request
 
 from app.models.task import Task
 
-# creating the task blueprint
+# DEFINING THE TASK BLUEPRINT
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 
-# CREATE TASK
+# ============================= CRUD ROUTES =============================
+# CREATE TASK- POST request to /tasks
 @tasks_bp.route("", methods=["POST"])
 def create_task():
     request_body = request.get_json()
@@ -25,3 +26,32 @@ def create_task():
         "description": new_task.description,
         "is_complete": True if new_task.completed_at else False  
     }}, 201
+    
+
+# READ ALL TASKS- GET request to /tasks
+@tasks_bp.route("", methods=["GET"])
+def read_all_tasks():
+    tasks = Task.query.all()
+    tasks_response = []
+    for task in tasks:
+        tasks_response.append({
+            "id": task.task_id,
+            "title": task.title,
+            "description": task.description,
+            "is_complete": True if task.completed_at else False 
+        })
+    return jsonify(tasks_response), 200
+
+
+# READ ONE TASK- GET request to /tasks/<task_id>
+@tasks_bp.route("/<task_id>", methods=["GET"])
+def read_one_task(task_id):
+    task = Task.query.get(task_id)
+
+    return { "task": {
+        "id": task.task_id,
+        "title": task.title,
+        "description": task.description,
+        "is_complete": True if task.completed_at else False
+        }
+    }, 200
