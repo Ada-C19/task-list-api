8e0880a444528d43d4294339cde6539d99cbc6bf
Gabby <gabrielleyoung10@gmail.com>
2023-05-10 13:08:48 -0500

    Create and register Goal model

--- app/models/goal.py
+++ app/models/goal.py
@@ -2,4 +2,22 @@ from app import db
 
 
 class Goal(db.Model):
-    goal_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text)
+
+    def to_dict(self):
+        return {
+                "goal": {
+                    "id": self.id,
+                    "title": self.title,
+                }
+            }
+
+    @classmethod
+    def from_dict(cls, goal_dict):
+        try:
+            return cls(
+                title=goal_dict["title"]
+            )
+        except KeyError as error:
+            return f"Missing or invalid key {error}"
