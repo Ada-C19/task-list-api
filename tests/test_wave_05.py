02e88b48a79ca75c61f10b879494ef6d41442c8f
Julie Loh <jlohusky@gmail.com>
2023-05-09 22:16:19 -0700

    completed test_delete_goal_not_found

--- tests/test_wave_05.py
+++ tests/test_wave_05.py
@@ -150,21 +150,25 @@ def test_delete_goal(client, one_goal):
     # *****************************************************************
 
 
-@pytest.mark.skip(reason="test to be completed by student")
+# @pytest.mark.skip(reason="test to be completed by student")
 def test_delete_goal_not_found(client):
-    raise Exception("Complete test")
+    # raise Exception("Complete test")
 
     # Act
     # ---- Complete Act Here ----
+    response = client.delete("/goals/1")
+    response_body = response.get_json()
 
     # Assert
     # ---- Complete Assertions Here ----
     # assertion 1 goes here
+    assert response.status_code == 404
     # assertion 2 goes here
+    assert response_body == {"message":"goal 1 not found"}
     # ---- Complete Assertions Here ----
 
 
-@pytest.mark.skip(reason="No way to test this feature yet")
+# @pytest.mark.skip(reason="No way to test this feature yet")
 def test_create_goal_missing_title(client):
     # Act
     response = client.post("/goals", json={})
