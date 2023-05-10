997b93997d8eb4891bdcb3b79783ce50a29ee946
Elizabeth Zachary <elizabethzachary96@gmail.com>
2023-05-10 11:04:12 -0700

    added from_dict() and to_dict() methods, implemented goals_bp blueprint, and implemented create_goal() for POST method

--- app/models/goal.py
+++ app/models/goal.py
@@ -4,3 +4,13 @@ from app import db
 class Goal(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
+
+    @classmethod
+    def from_dict(cls, data_dict):
+        return cls(title=data_dict["title"])
+    
+    def to_dict(self):
+        return dict(
+            id=self.id,
+            title=self.title
+        )
---
+++ app/routes/goal_routes.py
@@ -0,0 +1,22 @@
+from flask import Blueprint, jsonify, request, make_response, abort
+from app.models.goal import Goal
+from .routes_helpers import validate_model
+from app import db
+
+goals_bp = Blueprint("goals", __name__, url_prefix="/goals")
+
+# create a goal endpoint
+@goals_bp.route("", methods=["POST"])
+def create_goal():
+    request_body = request.get_json()
+
+    try:
+        new_goal = Goal.from_dict(request_body)
+    except KeyError:
+        abort(make_response({"details": "Invalid data"}, 400))
+
+    db.session.add(new_goal)
+    db.session.commit()
+
+    return jsonify({"goal": new_goal.to_dict()}), 201
+
