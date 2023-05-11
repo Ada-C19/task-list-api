011b73ea200f3b3aee57df7119721efe8a97a851
mlhuynh <greenlinh@gmail.com>
2023-05-11 03:08:58 -0700

    Created goals_bp local variable to hold Blueprint instance route in app goal_routes.py module.

--- app/goal_routes.py
+++ app/goal_routes.py
@@ -3,3 +3,5 @@ from app.models.goal import Goal
 from app.models.task import Task
 from flask import Blueprint, jsonify, make_reponse, request
 
+goals_bp = Blueprint("goal_bp", __name__, url_prefix="/goals")
+
