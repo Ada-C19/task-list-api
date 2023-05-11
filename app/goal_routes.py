219e8a1ae2b8fa49316f96a836c0b6f7e39a1889
Megan Gonzalez <meganmgonzalez18@icloud.com>
2023-05-11 17:29:23 -0400

    adds delete goal route

--- app/goal_routes.py
+++ app/goal_routes.py
@@ -76,6 +76,19 @@ def update_goal(goal_id):
         }, 200
     )
 
+@goals_bp.route("/<goal_id>", methods=["DELETE"])
+def delete_goal(goal_id):
+    goal = validate_goal(goal_id)
+
+    db.session.delete(goal)
+    db.session.commit()
+
+    return make_response(
+        {
+            "details": f"Goal {goal.goal_id} \"{goal.title}\" successfully deleted"
+        }, 200
+    )
+
 
 def validate_goal(goal_id):
     goal = Goal.query.get(goal_id)
