97f2275c5abc56cdc17c7361aee7dcb38394117b
Charlie <charliethemushi@gmail.com>
2023-05-10 19:11:58 -0700

    added return structure for goal in task in Goak model

--- app/models/goal.py
+++ app/models/goal.py
@@ -13,9 +13,18 @@ class Goal(db.Model):
         goal_as_dict["title"] = self.title
     
         return goal_as_dict
+    
+    def to_goal_dict(self):
+        task_in_goal_dict = {}
+        task_in_goal_dict["id"] = self.goal_id
+        task_in_goal_dict["task_ids"] = self.task_id
         
+        return task_in_goal_dict
+
     @classmethod
     def from_dict(cls, goal_data):
         new_goal = Goal(
                         title=goal_data["title"])
         return new_goal
+    
+    
