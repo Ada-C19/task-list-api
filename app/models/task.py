86a87f72250af6df44207464123cb76cd4bf5d2c
maggie weir <mlweir98@gmail.com>
2023-05-11 14:48:35 -0400

    created task model

--- app/models/task.py
+++ app/models/task.py
@@ -6,3 +6,21 @@ class Task(db.Model):
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime,nullable=True)
+
+    def to_dict(self):
+        task_as_dict = {}
+        task_as_dict["id"] = self.task_id
+        task_as_dict["title"] = self.title
+        task_as_dict["description"] = self.description
+        task_as_dict["is_complete"] = bool(self.completed_at)
+
+        return task_as_dict
+
+    @classmethod
+    def from_dict(cls, task_data):
+        new_task = Task(
+            title = task_data["title"],
+            description = task_data["description"]
+        )
+
+        return new_task
