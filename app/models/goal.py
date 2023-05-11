34e26da0a9e1acd5cf1f9df9899d0f7dda79e2ef
Janice <janicenguyenn@gmail.com>
2023-05-10 19:31:09 -0700

    implemented to dict and from_dict for goal

--- app/models/goal.py
+++ app/models/goal.py
@@ -4,3 +4,14 @@ from app import db
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.Text)
+
+    def to_dict(self):
+        return{
+            "id": self.goal_id,
+            "title": self.title,
+        }
+
+    @classmethod
+    def from_dict(cls,goal_details):
+        new_goal = Goal(title=goal_details["title"])
+        return new_goal
