6c04dcec1775f06fdd81ca20c0810cb1cf8200e1
Gweneth Johnson <johnsong0119@gmail.com>
2023-05-11 16:38:53 -0700

    IT DELETED MY AJSDKANSDKJF TO DICT FUNCTION

--- app/models/task.py
+++ app/models/task.py
@@ -9,7 +9,18 @@ class Task(db.Model):
     goal_id = db.Column(db.Integer, db.ForeignKey("goal.goal_id"))
     goal = db.relationship("Goal", back_populates="tasks")
 
-    
+    def to_dict(self):
+        return_dict = {}
+        return_dict["id"] = self.task_id
+        return_dict["title"] = self.title
+        return_dict["description"] = self.description
+        if self.completed_at:
+            return_dict["is_complete"] = True
+        if not self.completed_at:
+            return_dict["is_complete"] = False
+        if self.goal_id:
+            return_dict["goal_id"] = self.goal_id
+        return return_dict
 
     @classmethod
     def from_dict(cls, data_dict):
