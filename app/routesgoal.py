b7e957d6a17b8d92e950e4213569cd979725bf5e
Charlie <charliethemushi@gmail.com>
2023-05-10 17:08:58 -0700

    created validation and individual GET endpoint

--- app/routesgoal.py
+++ app/routesgoal.py
@@ -3,6 +3,22 @@ from app.models.goal import Goal
 from flask import Blueprint, jsonify, make_response, request, abort
 from datetime import datetime
 
+
+def validate_goal(goal_id):
+    try:
+        goal_id = int(goal_id)
+    except:
+        abort(make_response({"message":f"goal {goal_id} invalid"}, 400))
+
+    goal = Goal.query.get(goal_id)
+
+    if not goal:
+        abort(make_response({"message":f"goal {goal_id} not found"}, 404))
+
+    return goal
+
+
+
 goal_bp = Blueprint("goals", __name__, url_prefix="/goals")
 @goal_bp.route("", methods=["POST"])
 def create_goals():
@@ -34,5 +50,11 @@ def read_all_gaols():
         goal_response.append(goal.to_dict())
 
     return jsonify(goal_response)
+
+
+@goal_bp.route("/<goal_id>", methods=["GET"])
+def read_one_goal(goal_id):
+    goal = validate_goal(goal_id)
+    return make_response(jsonify({"goal":goal.to_dict()}))
 # @goal_bp.route("", methods=["PUT"])
 # @goal_bp.route("", methods=["DELETE"])
