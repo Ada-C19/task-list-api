104044ccfd076537ba5e335dec1f8194c98c53bb
mlhuynh <greenlinh@gmail.com>
2023-05-10 18:01:39 -0700

    Wrote PATCH endpoint and mark_task_complete function in app routes.py module.

--- app/routes.py
+++ app/routes.py
@@ -71,6 +71,15 @@ def update_task(task_id):
 
     return make_response({"task": task.to_dict()}, 200)
 
+@tasks_bp.route("/<task_id>/mark_complete", methods=["PATCH"])
+def mark_task_complete(task_id):
+    task = validate_model(Task, task_id)
+
+    task.completed_at = datetime.utcnow()
+    db.session.commit()
+
+    return make_response({"task": task.to_dict()}, 200)
+
 @tasks_bp.route("/<task_id>/mark_incomplete", methods=["PATCH"])
 def mark_task_incomplete(task_id):
     task = validate_model(Task, task_id)
