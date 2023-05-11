145a332aae2391b0164a80dbdebcfbf547b3b1d1
Charlie <charliethemushi@gmail.com>
2023-05-10 17:50:15 -0700

    created DELETE endpoint, created realtionship between goal and task, finished assertions for goal endpoints

--- app/routesgoal.py
+++ app/routesgoal.py
@@ -67,4 +67,11 @@ def update_goal(goal_id):
 
     db.session.commit()
     return make_response(jsonify({"goal":goal.to_dict()}),200)
-# @goal_bp.route("", methods=["DELETE"])
+
+@goal_bp.route("<goal_id>", methods=["DELETE"])
+def delete_goal(goal_id):
+    goal = validate_goal(goal_id)
+    db.session.delete(goal)
+    db.session.commit()
+
+    return make_response(jsonify({"details": f"Goal {goal_id} \"{goal.title}\" successfully deleted"}), 200)
