342c5af634e8a917c864e2ca522d5b1b205a8325
Megan Gonzalez <meganmgonzalez18@icloud.com>
2023-05-11 17:03:42 -0400

    adds create goal route

---
+++ app/goal_routes.py
@@ -0,0 +1,32 @@
+from app import db
+from app.models.goal import Goal
+from flask import Blueprint, jsonify, make_response, abort, request
+
+goals_bp = Blueprint("goals'", __name__, url_prefix = "/goals")
+
+@goals_bp.route("", methods=["POST"])
+def create_goal():
+    request_body = request.get_json()
+
+    if not request_body.get("title"):
+        abort(make_response(
+                {
+                    "details": "Invalid data"
+                }, 400
+            ))
+
+    goal = Goal(
+        title = request_body["title"],
+    )
+
+    db.session.add(goal)
+    db.session.commit()
+
+    return make_response(
+        {
+            "goal": {
+                "id": goal.goal_id,
+                "title": goal.title,
+            }
+        }, 201
+    )
