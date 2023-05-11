84d86f11e6aa1f91c14a972062a7da7dcea44095
alyssa <tn.alyssa@gmail.com>
2023-05-11 14:18:51 -0500

    added RENDER_DATABASE_URI

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,4 @@
 from app import db
-from flask import make_response
 
 
 class Task(db.Model):
@@ -7,8 +6,9 @@ class Task(db.Model):
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable=True)
+    goal_id = db.Column(db.ForeignKey('goal.id'),nullable=True)
     goal = db.relationship("Goal", back_populates="tasks")
-    goal_id = db.Column(db.Integer, db.ForeignKey('goal.id'),nullable=True)
+    
 
 
     def to_dict(self):
