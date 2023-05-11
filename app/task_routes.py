92bfd94c4a0521fd23b64003d76e01736a54a8d7
alyssa <tn.alyssa@gmail.com>
2023-05-11 17:27:39 -0500

    refactored mark_task_complete() to move slack external API call to a helper function

--- app/task_routes.py
+++ app/task_routes.py
@@ -49,7 +49,6 @@ def update_task_info(task_id):
     
     task.title = request_body["title"]
     task.description = request_body["description"]
-
     db.session.commit()
 
     return make_response({"task": task.to_dict()})
@@ -61,28 +60,21 @@ def mark_task_complete(task_id):
     task.completed_at = datetime.utcnow()
 
     db.session.commit()
-    
-    token = os.environ.get("SLACK_BOT_TOKEN")
-    external_url = 'https://slack.com/api/chat.postMessage'
-    headers = {"Authorization": f"Bearer {token}"}
-    data = {"channel": "task-notifications", 
-            "text": f"Someone just completed the task \"{task.title}\""}
-    
-    response = requests.post(external_url, headers=headers, json=data)
-    
+    response = send_slack_message(task)
+
+    # response.ok is a boolean value that is True if the HTTP status code is between 
+    # 200 and 299 (indicating a successful request), and False otherwise
     if response.ok:
         return make_response({"task": task.to_dict()})
     else:
         return make_response({"Error message":response.text})
-    
-
 
 
 @tasks_bp.route("<task_id>/mark_incomplete", methods=["PATCH"])
 def mark_task_incomplete(task_id):
     task = validate_model(Task, task_id)
-    task.completed_at = None
 
+    task.completed_at = None
     db.session.commit()
 
     return make_response({"task": task.to_dict()})
@@ -97,3 +89,15 @@ def delete_task(task_id):
     db.session.commit()
 
     return make_response({"details": f"Task {task_id} \"{task.title}\" successfully deleted"})
+
+
+
+# Helper Functions
+def send_slack_message(task):
+    token = os.environ.get("SLACK_BOT_TOKEN")
+    external_url = 'https://slack.com/api/chat.postMessage'
+    headers = {"Authorization": f"Bearer {token}"}
+    data = {"channel": "task-notifications", 
+            "text": f"Someone just completed the task \"{task.title}\""}
+    response = requests.post(external_url, headers=headers, json=data)
+    return response
