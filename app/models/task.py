bb94cd1e63eff178f330e51d16b170589017d659
mlhuynh <greenlinh@gmail.com>
2023-05-11 02:27:12 -0700

    Linked goal and task db by adding goal_id column as foreign key to task table in app model task.py module.

--- app/models/task.py
+++ app/models/task.py
@@ -7,6 +7,7 @@ class Task(db.Model):
     description = db.Column(db.String, nullable=False)
     completed_at = db.Column(db.DateTime, nullable=True)
     is_complete = db.Column(db.Boolean)
+    goal_id = db.Column(db.Integer, db.ForeignKey("goal.goal_id"), nullable=True)
     goal = db.relationship("Goal", back_populates="tasks")
 
     def to_dict(self):
