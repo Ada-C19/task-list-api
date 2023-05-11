40dae606cbeb5fd2a98c90b6581fc00e7ba85d47
TombAnneX <109308405+AKTuminello@users.noreply.github.com>
2023-05-11 05:50:35 -0600

    update task/goal models. tests still green

--- app/models/task.py
+++ app/models/task.py
@@ -4,12 +4,16 @@ class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50), nullable=False)
     description = db.Column(db.String(50), nullable=False)
-    completed_at = db.Column(db.DateTime,nullable=True, default=None)
-    
+    completed_at = db.Column(db.DateTime, nullable=True, default=None)
+
+    # Add the foreign key relationship with Goal
+    goal_id = db.Column(db.Integer, db.ForeignKey('goal.goal_id'), nullable=True)
+
     def to_dict(self):
         return {
             "task_id": self.task_id,
             "title": self.title,
             "description": self.description,
             "completed_at": self.completed_at
-            }
+        }
+
