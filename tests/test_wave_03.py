c996f2750643f302b4da077f6831d5e8665b22ef
mlhuynh <greenlinh@gmail.com>
2023-05-10 18:29:36 -0700

    Completed assertion for test_mark_incomplete_missing_task function in test_wave_03.py module.

--- tests/test_wave_03.py
+++ tests/test_wave_03.py
@@ -143,8 +143,9 @@ def test_mark_incomplete_missing_task(client):
 
     # Assert
     assert response.status_code == 404
+    assert response_body == {f"message": "Task 1 not found"}
 
-    raise Exception("Complete test with assertion about response body")
+    #raise Exception("Complete test with assertion about response body")
     # *****************************************************************
     # **Complete test with assertion about response body***************
     # *****************************************************************
