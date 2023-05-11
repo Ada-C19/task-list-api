d1544936dd63762b6e8215dfb5875f701076a521
Katherine Guarnizo <guarnizo.katherine.5@gmail.com>
2023-05-11 10:22:18 -0400

    fix typo in Task model when bulding one to many relationship and added nullable=True to goal_id

--- app/models/task.py
+++ app/models/task.py
@@ -6,9 +6,9 @@ class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     description = db.Column(db.String, nullable=False)
-    completed_at = db.Column(db.DateTime)
-    goal_id = db.Column(db.Integer, db.ForeignKey('goal.goal_id'))
-    goal = db.relationship("Goal", back_populates="taks")
+    completed_at = db.Column(db.DateTime, nullable=True)
+    goal_id = db.Column(db.Integer, db.ForeignKey('goal.goal_id'), nullable=True)
+    goal = db.relationship("Goal", back_populates="tasks")
 
     def to_dict(self):
         if not self.completed_at:
