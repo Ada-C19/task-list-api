11faa6752df2978000e7c6a0e6507320d99011e6
Yvett Jimenez <yvett.nez@gmail.com>
2023-05-11 00:06:55 -0500

    validate task updated to validate model

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import db
 from app.models.task import Task
+from app.models.goal import Goal
 from datetime import datetime
 from flask import Blueprint, jsonify, make_response, request, abort
 from dotenv import load_dotenv
@@ -10,6 +11,7 @@ load_dotenv()
 API_TOKEN = os.environ.get("API_TOKEN")
 
 task_bp = Blueprint("task", __name__, url_prefix="/tasks")
+goal_bp = Blueprint("goal", __name__, url_prefix="/goals")
 
 # wave 1 routes
 @task_bp.route("", methods=["POST"])
@@ -44,7 +46,7 @@ def read_all_tasks():
 
 @task_bp.route("/<task_id>", methods=["GET"])
 def get_one_task(task_id):
-    task = validate_task(task_id)
+    task = validate_model(Task, task_id)
     message  = {
             "task": task.to_dict()
         }
@@ -52,7 +54,7 @@ def get_one_task(task_id):
 
 @task_bp.route("/<task_id>", methods=["PUT"])
 def update_task(task_id):
-    task = validate_task(task_id)
+    task = validate_model(Task, task_id)
     request_body = request.get_json()
 
     task.title = request_body["title"]
@@ -67,22 +69,34 @@ def update_task(task_id):
     return make_response(message, 200)
     
 # validate helper function
-def validate_task(task_id):
-    try:
-        task_id = int(task_id)
-    except:
-        abort(make_response({"message":f"task {task_id} invalid"}, 400))
+# def validate_task(task_id):
+#     try:
+#         task_id = int(task_id)
+#     except:
+#         abort(make_response({"message":f"task {task_id} invalid"}, 400))
 
-    tasks = Task.query.all()
-    for task in tasks:
-        if task.task_id == task_id:
-            return task
+#     tasks = Task.query.all()
+#     for task in tasks:
+#         if task.task_id == task_id:
+#             return task
     
-    abort(make_response({"message":f"task {task_id} not found"}, 404))
+#     abort(make_response({"message":f"task {task_id} not found"}, 404))
+def validate_model(cls, model_id):
+    try:
+        model_id = int(model_id)
+    except:
+        abort(make_response({"message":f"{cls.__name__.lower()} {model_id} invalid"}, 400))
+
+    model = cls.query.get(model_id)
+    if not model:
+        abort(make_response({"message":f"{cls.__name__.lower()} {model_id} not found"}, 404))
+
+    return model
+    
 
 @task_bp.route("/<task_id>", methods=["DELETE"])
 def delete_task(task_id):
-    task = validate_task(task_id)
+    task = validate_model(Task, task_id)
     
     db.session.delete(task)
     db.session.commit()
@@ -96,7 +110,7 @@ def delete_task(task_id):
 # wave 3
 @task_bp.route("/<task_id>/mark_complete", methods=["PATCH"])
 def mark_task_complete(task_id):
-    task = validate_task(task_id)
+    task = validate_model(Task, task_id)
 
     task.completed_at = datetime.now()
 
@@ -110,7 +124,7 @@ def mark_task_complete(task_id):
 
 @task_bp.route("/<task_id>/mark_incomplete", methods=["PATCH"])
 def mark_task_incomplete(task_id):
-    task = validate_task(task_id)
+    task = validate_model(Task, task_id)
 
     task.completed_at = None
 
@@ -132,3 +146,21 @@ def post_to_slack(task_title):
         }
     response = requests.post(url, headers=headers, data=data)
     return response
+
+# goals routes - wave 5
+# goal_bp.route("", methods=["POST"])
+# def create_goal():
+#     request_body = request.get_json()
+#     try:
+#         new_task = Task(title = request_body["title"],
+#                         description = request_body["description"])
+        
+#         db.session.add(new_task)
+#         db.session.commit()
+
+#         message  = {
+#             "task": new_task.to_dict()
+#         }
+#         return make_response(message, 201)
+#     except KeyError as e:
+#         abort(make_response({"details": "Invalid data"}, 400))
