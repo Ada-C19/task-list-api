62c371e4c6620aa97c64e6232590b611b8dbc240
Janice <janicenguyenn@gmail.com>
2023-05-11 09:01:12 -0700

    fixed test

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -67,7 +67,6 @@ def test_get_task_not_found(client):
     assert response_body == {"message":"Task 1 not found"}
 
 # @pytest.mark.skip(reason="No way to test this feature yet")
-def test_create_task(client):
     # Act
     response = client.post("/tasks", json={
         "title": "A Brand New Task",
