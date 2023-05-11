1ffd7fa5aaa2cc520808b107c344803dd7e6646d
mlhuynh <greenlinh@gmail.com>
2023-05-11 02:52:01 -0700

    Renamed routes.py module in app folder to task_routes.py.

---
+++ app/task_routes.py
@@ -0,0 +1,114 @@
+from app import db
+from app.models.task import Task
+from flask import Blueprint, jsonify, make_response, request, abort
+from sqlalchemy import asc, desc
+from datetime import datetime
+import os, requests
+
+
+tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
+
+def validate_model(task_id):
+    try:
+        task_id = int(task_id)
+    except:
+        abort(make_response({"message": f"Task {task_id} is invalid"}, 400))
+
+    task = Task.query.get(task_id)
+
+    if not task:
+        abort(make_response({"message": f"Task {task_id} not found"}, 404))
+    
+    return task
+
+def slack_bot_message(message):
+    slack_api_key = os.environ.get("SLACK_BOT_TOKEN")
+    slack_url = "https://slack.com/api/chat.postMessage"
+    header = {"Authorization": slack_api_key}
+
+    slack_query_params = {
+        "channel": "task-notifications",
+        "text": message
+    }
+    print(slack_api_key)
+    requests.post(url=slack_url, data=slack_query_params, headers=header)
+
+# -----------------------------------
+#               ROUTES
+# -----------------------------------
+
+@tasks_bp.route("", methods=["POST"])
+def create_task():
+    request_body = request.get_json()
+
+    if "title" not in request_body or "description" not in request_body:
+        return make_response({"details": "Invalid data"}, 400)
+
+    new_task = Task.from_dict(request_body)
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return make_response({"task": new_task.to_dict()}, 201)
+
+@tasks_bp.route("", methods=["GET"])
+def read_all_tasks():
+    sort_query = request.args.get("sort")
+    if sort_query:
+        if sort_query == "asc":
+            tasks = Task.query.order_by(Task.title.asc()).all()
+        elif sort_query == "desc":
+            tasks = Task.query.order_by(Task.title.desc()).all()
+    else:
+        tasks = Task.query.all()
+    
+    all_tasks = [task.to_dict() for task in tasks]
+
+    return jsonify(all_tasks), 200
+
+@tasks_bp.route("/<task_id>", methods=["GET"])
+def read_one_task(task_id):
+    task = validate_model(task_id)
+
+    return make_response({"task": task.to_dict()}, 200)
+
+@tasks_bp.route("/<task_id>", methods=["PUT"])
+def update_task(task_id):
+    task = validate_model(task_id)
+    request_body = request.get_json()
+
+    task.title = request_body["title"]
+    task.description = request_body["description"]
+
+    db.session.commit()
+
+    return make_response({"task": task.to_dict()}, 200)
+
+@tasks_bp.route("/<task_id>/mark_complete", methods=["PATCH"])
+def mark_task_complete(task_id):
+    task = validate_model(task_id)
+
+    task.completed_at = datetime.utcnow()
+    db.session.commit()
+
+    slack_bot_message(f"Someone just completed the task {task.title}")
+
+    return make_response({"task": task.to_dict()}, 200)
+
+@tasks_bp.route("/<task_id>/mark_incomplete", methods=["PATCH"])
+def mark_task_incomplete(task_id):
+    task = validate_model(task_id)
+
+    task.completed_at = None
+    db.session.commit()
+
+    return make_response({"task": task.to_dict()}, 200)
+
+@tasks_bp.route("/<task_id>", methods=["DELETE"])
+def delete_task(task_id):
+    task = validate_model(task_id)
+
+    db.session.delete(task)
+    db.session.commit()
+
+    return make_response({"details": f"Task {task.task_id} \"{task.title}\" successfully deleted"}), 200
