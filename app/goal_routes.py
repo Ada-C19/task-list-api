1c75a9ba76642432a252728cc57b791aea1130d7
Janice <janicenguyenn@gmail.com>
2023-05-11 08:32:53 -0700

    refactored to use helper function

--- app/goal_routes.py
+++ app/goal_routes.py
@@ -1,20 +1,9 @@
 from app import db
 from app.models.goal import Goal
 from flask import Blueprint, jsonify, request, make_response, abort
-
+from app.helper_function import get_valid_item_by_id
 
 goals_bp = Blueprint("goals", __name__, url_prefix="/goals")
-
-def validate_task(cls,goal_id):
-    try:
-        goal_id = int(goal_id)
-    except:
-        abort(make_response({"message":f"{cls.__name__} {goal_id} invalid"}, 400))
-    
-    goal = Goal.query.get(goal_id)
-    if not goal:
-        abort(make_response({"message":f"{cls.__name__} {goal_id} not found"}, 404))
-    return goal
 
 # POST request to create new goal
 @goals_bp.route("", methods=['POST'])
@@ -50,12 +39,12 @@ def get_all_goals():
 
 @goals_bp.route("/<goal_id>", methods=["GET"])
 def get_by_id(goal_id):
-    goal = validate_task(Goal, goal_id)
+    goal = get_valid_item_by_id(Goal, goal_id)
     return {"goal": goal.to_dict()}, 200
 
 @goals_bp.route("/<goal_id>", methods=["PUT"])
-def update_task(goal_id):
-    goal_to_update = validate_task(Goal, goal_id)
+def update_goal(goal_id):
+    goal_to_update = get_valid_item_by_id(Goal, goal_id)
 
     request_body = request.get_json()
   
@@ -67,8 +56,8 @@ def update_task(goal_id):
     return {"goal": goal_to_update.to_dict()}, 200
 
 @goals_bp.route("/<goal_id>", methods=["DELETE"])
-def delete_task(goal_id):
-    goal = validate_task(Goal,goal_id)
+def delete_goal(goal_id):
+    goal = get_valid_item_by_id(Goal,goal_id)
 
     db.session.delete(goal)
     db.session.commit()
@@ -76,3 +65,4 @@ def delete_task(goal_id):
     return{
         "details": f"Goal {goal_id} \"{goal.title}\" successfully deleted"
     }, 200
+
