f7acc595cc87749ca52cb841ea851c15ebb79aee
Jay King <jayriverking@gmail.com>
2023-05-11 11:45:21 -0400

    change test database to render db to pass Learn tests

--- app/__init__.py
+++ app/__init__.py
@@ -20,7 +20,7 @@ def create_app(test_config=None):
     else:
         app.config["TESTING"] = True
         app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-            "SQLALCHEMY_TEST_DATABASE_URI")
+            "RENDER_DATABASE_URI")
 
     # Import models here for Alembic setup
     from app.models.task import Task
