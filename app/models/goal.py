deca24547a0a4b20f823c2216e44eeda17dbcb9e
Charlie <charliethemushi@gmail.com>
2023-05-10 17:06:01 -0700

    created POST and GET endpoints for goal'

--- app/models/goal.py
+++ app/models/goal.py
@@ -6,14 +6,14 @@ class Goal(db.Model):
     title = db.Column(db.String)
 
     def to_dict(self):
-            goal_as_dict = {}
-            goal_as_dict["id"] = self.goal_id 
-            goal_as_dict["title"] = self.title
-        
-            return goal_as_dict
+        goal_as_dict = {}
+        goal_as_dict["id"] = self.goal_id 
+        goal_as_dict["title"] = self.title
+    
+        return goal_as_dict
         
     @classmethod
     def from_dict(cls, goal_data):
-        new_goal = Goal(id=goal_data["id"],
+        new_goal = Goal(
                         title=goal_data["title"])
         return new_goal
--- app/routesgoal.py
+++ app/routesgoal.py
@@ -1,10 +1,38 @@
 from app import db
-from app.models.task import Task
+from app.models.goal import Goal
 from flask import Blueprint, jsonify, make_response, request, abort
 from datetime import datetime
 
 goal_bp = Blueprint("goals", __name__, url_prefix="/goals")
 @goal_bp.route("", methods=["POST"])
+def create_goals():
+    
+    request_body = request.get_json()
+    try:
+        new_goal = Goal.from_dict(request_body)
+    except:
+        abort(make_response({"details": "Invalid data"},400))
+        
+    db.session.add(new_goal)
+    db.session.commit()
+    
+    response_body = {"goal": new_goal.to_dict()}
+    
+    return make_response(jsonify(response_body), 201)
+
 @goal_bp.route("", methods=["GET"])
-@goal_bp.route("", methods=["PUT"])
-@goal_bp.route("", methods=["DELETE"])
+def read_all_gaols():
+    title = request.args.get("title")
+    if title:
+        goals = Goal.query.filter_by(title=title)
+    else:
+        goals = Goal.query.all()
+
+
+    goal_response = []
+    for goal in goals:
+        goal_response.append(goal.to_dict())
+
+    return jsonify(goal_response)
+# @goal_bp.route("", methods=["PUT"])
+# @goal_bp.route("", methods=["DELETE"])
