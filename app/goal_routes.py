ec1e8cfdea347a5ea5ac155df7438964aaf8e515
mlhuynh <greenlinh@gmail.com>
2023-05-11 03:22:44 -0700

    Wrote POST endpoint and create_goal function in app goal_routes.py module.

--- app/goal_routes.py
+++ app/goal_routes.py
@@ -5,3 +5,16 @@ from flask import Blueprint, jsonify, make_reponse, request
 
 goals_bp = Blueprint("goal_bp", __name__, url_prefix="/goals")
 
+@goals_bp.route("", methods=["POST"])
+def create_goal():
+    request_body = request.get_json()
+
+    if "title" not in request_body:
+        return make_reponse({"details": "Invalid data"}, 400)
+    
+    new_goal = Goal.from_dict(request_body)
+
+    db.session.add(new_goal)
+    db.session.commit()
+
+    return make_reponse({"goal": new_goal.todict()}, 201)
