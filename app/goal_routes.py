0bffbdd6a8f4c7995dc611bdd144f447cc76e5a2
Abby Castillo <abby.machines@gmail.com>
2023-05-12 03:08:41 -0700

    implement DELETE /goals/<goal_id> endpoint (delete_goal)

--- app/goal_routes.py
+++ app/goal_routes.py
@@ -45,3 +45,17 @@ def get_goals():
         goals_response.append(goal.to_dict())
     
     return jsonify(goals_response)
+
+# Get one saved goal
+
+# Update goal
+
+# Delete goal
+@goals_bp.route("/<goal_id>", methods=["DELETE"])
+def delete_goal(goal_id):
+    goal = validate_goal(goal_id)
+    
+    db.session.delete(goal)
+    db.session.commit()
+    
+    return make_response({"details": f'Goal {goal.goal_id} "{goal.title}" successfully deleted'})
