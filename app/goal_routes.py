48073a22498cd0d6ae05809d830aa2648c982b4d
Abby Castillo <abby.machines@gmail.com>
2023-05-12 02:38:27 -0700

    set up goals_bp and validate_goal helper function, along with necessary dependencies

---
+++ app/goal_routes.py
@@ -0,0 +1,16 @@
+from app import db
+from app.models.goal import Goal
+from flask import Blueprint, abort, make_response
+
+goals_bp = Blueprint("goals_bp", __name__, url_prefix="/goals")
+
+def validate_goal(goal_id):
+    try:
+        goal_id = int(goal_id)
+    except:
+        abort(make_response({"message": f"goal {goal_id} is invalid"}, 400))
+    
+    goal = Goal.query.get(goal_id)
+    
+    if not goal:
+        abort(make_response({"details": f"Goal {goal_id} not found"}, 404))
