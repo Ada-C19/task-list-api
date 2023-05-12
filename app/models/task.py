6130e9d624bd36b014b6e509a031d54a5ccdccb5
Megan Gonzalez <meganmgonzalez18@icloud.com>
2023-05-12 14:35:53 -0400

    adds two class methods

--- app/models/task.py
+++ app/models/task.py
@@ -8,3 +8,30 @@ class Task(db.Model):
     completed_at = db.Column(db.DateTime, nullable=True)
     goal_id = db.Column(db.Integer, db.ForeignKey("goal.goal_id"), nullable=True)
     goal = db.relationship("Goal", back_populates ="tasks")
+
+    def to_dict(self):
+        if self.goal_id:
+            return {
+                        "id": self.task_id,
+                        "title": self.title,
+                        "goal_id": self.goal_id,
+                        "description": self.description,
+                        "is_complete": bool(self.completed_at)
+                    }
+
+        else:
+            return {
+                        "id": self.task_id,
+                        "title": self.title,
+                        "description": self.description,
+                        "is_complete": bool(self.completed_at)
+                    }
+
+    @classmethod
+    def from_dict(cls, task_data):
+        new_task = Task(
+            title = task_data["title"],
+            description = task_data["description"],
+        )
+        return new_task
+
