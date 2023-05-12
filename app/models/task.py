8e7ac8741be536665f7a10e3389ea68fc3d0df96
Amber Shay <mashaad21@gmail.com>
2023-05-11 21:36:51 -0600

    connect goal and task models as relationships

--- app/models/task.py
+++ app/models/task.py
@@ -7,6 +7,8 @@ class Task(db.Model):
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable=True)
+    goal_id = db.Column(db.Integer, db.ForeignKey("goal.goal_id"))
+    goal = db.relationship("Goal", back_populates="tasks")
 
     @classmethod
     def task_from_dict(cls, task_data):
