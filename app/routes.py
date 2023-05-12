c6151de1f1380aa5af239704822c6a0dc4da88b3
La Tasha Pollard <latashajpollard@gmail.com>
2023-05-11 19:00:30 -0500

    completes wave 2 with all test passing

--- app/routes.py
+++ app/routes.py
@@ -38,7 +38,14 @@ def create_task():
 
 @task_bp.route("", methods=["GET"])
 def get_task():
-    tasks = Task.query.all()
+    sort_query = request.args.get("sort")
+    if sort_query == "asc":
+        tasks = Task.query.order_by(Task.title).all()
+    elif sort_query == "desc":
+        tasks = Task.query.order_by(Task.title.desc()).all()
+
+    else:
+        tasks = Task.query.all()
     results = [task.to_dict() for task in tasks]
 
     return jsonify(results)
