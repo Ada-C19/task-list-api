d808524e5f433904519e13173ec2e65f0998b0b0
Megan Gonzalez <meganmgonzalez18@icloud.com>
2023-05-12 14:35:17 -0400

    adds two class methods

--- app/models/goal.py
+++ app/models/goal.py
@@ -5,3 +5,17 @@ class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     tasks = db.relationship("Task", back_populates="goal", lazy = True)
+
+    def to_dict(self):
+
+        return {
+            "id": self.goal_id,
+            "title": self.title,
+                    }
+
+    @classmethod
+    def from_dict(cls, goal_data):
+        new_goal = Goal(
+            title = goal_data["title"],
+        )
+        return new_goal
