29b0b0fc8668ceaf307958e5b18dd87ab7d560ac
maggie weir <mlweir98@gmail.com>
2023-05-13 12:17:53 -0400

    added to_dict method to class goal

--- app/models/goal.py
+++ app/models/goal.py
@@ -6,3 +6,10 @@ class Goal(db.Model):
     title = db.Column(db.String)
     tasks = db.relationship("Task", back_populates="goal",lazy=True)
     
+    def to_dict(self):
+        goal_as_dict = {}
+        goal_as_dict["id"] = self.goal_id
+        goal_as_dict["title"] = self.title
+        
+
+        return goal_as_dict
