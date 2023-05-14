b45b8aed24fc9dc2e1319e3b182cd89bee432673
Abby <argoodm@gmail.com>
2023-05-14 10:22:01 -0400

    added post route and also recreated database with correct completed at constriant

--- app/models/task.py
+++ app/models/task.py
@@ -2,4 +2,19 @@ from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String) #when refactoring change to varchar
+    description = db.Column(db.String) # when refacoring change to varvhar
+    completed_at = db.Column(db.DateTime, default=None, nullable=True) 
+
+
+# wave 1
+#Our task list API should be able to work with an entity called Task.
+# Tasks are entities that describe a task a user wants to complete. They contain a:
+# title to name the task
+# description to hold details about the task
+# an optional datetime that the task is completed on
+# Our goal for this wave is to be able to create, read, update, and delete different tasks. 
+# We will create RESTful routes for this different operations.
+
+
--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,53 @@
-from flask import Blueprint
+from app import db
+from app.models.task import Task
+from flask import Blueprint, jsonify, make_response, request
+
+
+task_list_bp = Blueprint("task_list", __name__, url_prefix="/tasks")
+
+
+# create
+@task_list_bp.route("", methods=["POST"])
+
+def post_task():
+    request_body = request.get_json()
+
+    if "title" not in request_body or "description" not in request_body:
+        return make_response("Invalid request", 400)
+    new_task = Task(
+        title = request_body["title"],
+        description = request_body["description"],
+        completed_at = request_body["completed_at"]
+    )
+
+    db.session.add(new_task)
+    db.session.commit() 
+    
+    return make_response(f"New task: {new_task.title},  created", 201)
+
+# if not completed_at:
+#         return make_response({
+#             "task": {"id": new_task.id,
+#             "title": new_task.title,
+#             "description": new.task.description,
+#             "is_complete": false}
+#   }, 201
+#         )
+
+
+
+
+
+
+
+
+
+# #read
+# @task_list_bp.route("/tasks", methods=["GET"])
+
+
+# #update
+
+# # delete
+
+
