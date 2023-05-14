565e13bcc66153f79041c6124c2ee13223ab0b3e
Paige <ph.paigee@gmail.com>
2023-05-13 22:49:05 -0400

    working on wave 01 routes

--- app/models/task.py
+++ app/models/task.py
@@ -2,16 +2,20 @@ from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
-    completed_at = db.Column(db.DateTime)
+    completed_at = db.Column(db.DateTime, nullable=False)
 
     def to_dict(self):
-        task_as_dict = {}
-        task_as_dict["id"] = self.task_id
-        task_as_dict["title"] = self.title
-        task_as_dict["description"] = self.description
-        task_as_dict["completed_at"] = False
-
+        task_as_dict = {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "is_complete": bool(self.completed_at)
+        }
         return task_as_dict
+
+    # @classmethod
+    # def from_dict(cls, task_data):
+    #     return cls(title=task_data["title"], description=task_data["description"])
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, abort, make_response, request
-from app.models.task import Task
+from models.task import Task
 from app import db
 
 tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
@@ -21,13 +21,16 @@ def validate_model(cls, model_id):
 
 @tasks_bp.route("", methods=["POST"])
 def create_task():
-    response_body = request.get_json()
-    new_task = Task(title="Research APIs",
-                    description="Look for youtube videos and examples",
-                    is_complete=False)
+    request_body = request.get_json()
+
+    new_task = Task(title=request_body["title"],
+                    description=request_body["description"],
+                    completed_at=request_body["is_complete"])
     
     db.session.add(new_task)
     db.session.commit()
+
+    return make_response(jsonify(f"task {new_task.title} successfully created"), 201)
 
 
 @tasks_bp.route("", methods=["GET"])
@@ -39,27 +42,28 @@ def read_all_tasks():
         tasks_response.append(task.to_dict())
     return jsonify(tasks_response)
 
-@tasks_bp.route("/<task_id>", methods=["GET"])
-def read_one_task(task_id):
-    task = validate_model(Task, task_id)
-    return task.to_dict()
+# @tasks_bp.route("/<task_id>", methods=["GET"])
+# def read_one_task(task_id):
+#     task = validate_model(Task, task_id)
+#     return task.to_dict()
 
-@tasks_bp.route("/<task_id>", methods=["PUT"])
-def update_task(task_id):
-    task = validate_model(Task, task_id)
-    request_body = request.get_json()
+# @tasks_bp.route("/<task_id>", methods=["PUT"])
+# def update_task(task_id):
+#     task = validate_model(Task, task_id)
+#     request_body = request.get_json()
 
-    task.name = request_body["name"]
-    task.description = request_body["description"]
+#     task.name = request_body["name"]
+#     task.description = request_body["description"]
+#     task.completed_at = request_body["is_complete"]
 
-    db.session.commit()
-    return make_response(f"Task #{task.id} successfully updated", 200)
+#     db.session.commit()
+#     return make_response(f"Task #{task.id} successfully updated", 200)
 
-@tasks_bp.route("/<task_id>", methods=["DELETE"])
-def delete_task(task_id):
-    task = validate_model(Task, task_id)
+# @tasks_bp.route("/<task_id>", methods=["DELETE"])
+# def delete_task(task_id):
+#     task = validate_model(Task, task_id)
 
-    db.session.delete(task)
-    db.session.commit()
+#     db.session.delete(task)
+#     db.session.commit()
 
-    return make_response(f"Task #{task.id} successfully deleted!", 200)
+#     return make_response(f"Task #{task.id} successfully deleted!", 200)
