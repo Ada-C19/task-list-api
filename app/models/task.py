71e9fc9c87d422cbc945ff24ae7487019dd8e85b
Say Ryder <lindz.ryder@gmail.com>
2023-05-14 14:44:25 -0400

    modified task model to match db table

--- app/models/task.py
+++ app/models/task.py
@@ -3,7 +3,7 @@ from app import db
 
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String)
+    title = db.Column(db.String(length=255))
     description=db.Column(db.String)
     completed_at=db.Column(db.DateTime, nullable=True)
     is_complete= db.Column(db.Boolean, default=False)
