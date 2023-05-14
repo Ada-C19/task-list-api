1f913146f8933c93166fe7fba64cfb039303c1db
Paige <ph.paigee@gmail.com>
2023-05-14 18:59:29 -0400

    new routes folder to hold task and goal routes

--- app/models/goal.py
+++ app/models/goal.py
@@ -3,3 +3,11 @@ from app import db
 
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True)
+    title = db.Columbn(db.String)
+
+    def to_dict(self):
+        goal_dict ={
+            "id": self.goal_id,
+            "title": self.title
+        }
+        return goal_dict
