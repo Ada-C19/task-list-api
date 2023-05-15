bd82091cfa6f79c6bffe2c0a4f6fecfd33ba7df6
Erina Perez <erinaperez@gmail.com>
2023-05-15 00:10:26 -0700

    Updates to deploy on Render

--- app/__init__.py
+++ app/__init__.py
@@ -14,13 +14,14 @@ def create_app(test_config=None):
     app = Flask(__name__)
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+
     if test_config is None:
         app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-            "SQLALCHEMY_DATABASE_URI")
+            "RENDER_DATABASE_URI")
     else:
         app.config["TESTING"] = True
         app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-            "SQLALCHEMY_TEST_DATABASE_URI")
+            "RENDER_DATABASE_URI")
 
     from app.models.task import Task
     from app.models.goal import Goal
