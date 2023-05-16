eb33fc28567024dac4cb68f6f151712622685ed6
Hannah <hannahrwatkins@gmail.com>
2023-05-15 20:51:41 -0400

    Created model for wave 1 again

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,22 @@
-from flask import Blueprint
+from flask import Blueprint, make_response
+from flask.json import jsonify
+# from app import db
+from app.models.task import Task
+
+tasks_bp = Blueprint("read_all_tasks", __name__, url_prefix=("/tasks"))
+
+# create route (get) to read all tasks
+@tasks_bp.route("", methods=["GET"])
+def read_all_tasks():
+    tasks_response = []
+    tasks = Task.query.all() 
+    
+    for task in tasks: 
+        tasks_response.append({
+            "id": task.task_id,
+            "title": task.title,
+            "description": task.description,
+            "is_complete": task.completed_at != None
+        })
+        
+    return jsonify(tasks_response)
