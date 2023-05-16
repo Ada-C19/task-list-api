494a30ad96a5d99bec6ad646ccafa7570cedb73f
an2084 <angelanawang47@gmail.com>
2023-05-16 15:28:12 -0400

    Post method working

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,16 @@
 from app import db
 
-
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    completed_at = db.Column(db.DateTime, nullable=True)
+
+def to_dict(self):
+    task_as_dict = {}
+    task_as_dict["id"] = self.id
+    task_as_dict["title"] = self.title
+    task_as_dict["description"] = self.description
+    task_as_dict["is_complete"] = self.is_complete
+
+    return task_as_dict
--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,45 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, abort, make_response, request
+from app import db
+from app.models.goal import Goal
+from app.models.task import Task
+
+goal_bp = Blueprint("goal", __name__, url_prefix="/goal")
+task_bp = Blueprint("task", __name__, url_prefix="/task")
+
+@task_bp.route("", methods=['POST'])
+def create_task():
+    request_body = request.get_json()
+    
+    task = Task(title=request_body["title"],
+                    description=request_body["description"],
+                    completed_at=request_body["completed_at"])
+
+    
+    
+    db.session.add(task)
+    db.session.commit()
+    
+    return jsonify({
+        "task": {
+            "id": task.id,
+            "title": task.title,
+            "description": task.description,
+            "is_complete": True if task.completed_at else False
+                }
+                    }), 201
+
+@task_bp.route("", methods = ["GET"])
+def read_all_tasks():
+    title_query = request.args.get("title")
+
+    if title_query:
+        tasks = Task.query.filter_by(title=title_query)
+    else:
+        tasks = Task.query.all()
+        
+    tasks_response = []
+    
+    for task in tasks:
+        tasks_response.append(task.to_dict())
+    
+    return jsonify(tasks_response)
