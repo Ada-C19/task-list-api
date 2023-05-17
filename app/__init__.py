693a39f18f9647f2d39433cb3d968119c1ef9f1d
Aisha <amdahir1@gmail.com>
2023-05-17 07:23:58 -0500

    changed test config

--- app/__init__.py
+++ app/__init__.py
@@ -14,16 +14,17 @@ def create_app(test_config=None):
     app = Flask(__name__)
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-        "RENDER_DATABASE_URI")
+    
 
-    # if test_config is None:
-    #     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-    #         "SQLALCHEMY_DATABASE_URI")
-    # else:
-        # app.config["TESTING"] = True
+    if test_config is None:
         # app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-        #     "SQLALCHEMY_TEST_DATABASE_URI")
+        #     "SQLALCHEMY_DATABASE_URI")
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+        "RENDER_DATABASE_URI")
+    else:
+        app.config["TESTING"] = True
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+            "SQLALCHEMY_TEST_DATABASE_URI")
 
     # Import models here for Alembic setup
     from app.models.task import Task
