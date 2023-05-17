f46514e8d48ad2197217ea5827ea59196091b160
an2084 <angelanawang47@gmail.com>
2023-05-17 17:32:25 -0400

    Almost completed wave 06

--- app/routes.py
+++ app/routes.py
@@ -123,9 +123,74 @@ def mark_task_incomplete(task_id):
 
     return jsonify({"task":task.to_dict()}), 200
 
+#################################GOAL ROUTES #############################################
+
+@goals_bp.route("", methods=['POST'])
+
+def create_goal():
+
+    request_body = request.get_json()
+
+    if "title" not in request_body:
+        abort(make_response({"details": "Invalid data"}, 400))
+
+    new_goal = Goal(title=request_body["title"])
+
+    db.session.add(new_goal)
+    db.session.commit()
+    
+    return {
+        "goal": new_goal.to_dict()
+    }, 201
+
+    
+
+
+@goals_bp.route("", methods = ["GET"])
+
+def read_all_goals():
+    goals = Goal.query.all()
+        
+    goals_response = []
+    
+    for goal in goals:
+        goals_response.append(goal.to_dict())
+    
+    return jsonify(goals_response), 200
 
 
 
+@goals_bp.route("/<goal_id>", methods = ["GET"])
+def read_one_goal(goal_id):
+    
+    goal = validate_model(Goal, goal_id)
 
+    return jsonify({"goal":goal.to_dict()}), 200
+
+
+
+@goals_bp.route("/<goal_id>", methods=["PUT"])
+def update_goal(goal_id):
+
+    task = validate_model(Goal, goal_id)
+    
+    request_body = request.get_json()
+    
+    goal.title = request_body["title"]
+
+    db.session.add(goal)
+    db.session.commit()
+
+    return jsonify({"goal":goal.to_dict()}), 200
+
+
+@goals_bp.route("/<goal_id>", methods=["DELETE"])
+def delete_goal(goal_id):
+    goal = validate_model(Goal, goal_id)
+
+    db.session.delete(goal)
+    db.session.commit()
+
+    return make_response({"details": f"Goal {goal_id} \"{goal.title}\" successfully deleted"})
 
 
