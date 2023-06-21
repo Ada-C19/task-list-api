e42e58f0557c39ee99e5700d48c3d187422afc14
Anna Lima <annavanwesep@gmail.com>
2023-06-21 14:16:27 -0700

    added cors connection

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,16 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os
 from dotenv import load_dotenv
+from flask_cors import CORS
 
 db = SQLAlchemy()
 migrate = Migrate()
 load_dotenv()
 
 def create_app(test_config=None, foo=None):
+    app = Flask(__name__)
+    CORS(app)
+    app.config['CORS_HEADERS'] = 'Content-Type'
     app = Flask(__name__)
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
