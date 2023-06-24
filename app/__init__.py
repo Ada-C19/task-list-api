51d78d27dcf79d255af89aec6379091d2bf63140
lulu <lulusun1210@gmail.com>
2023-06-23 20:46:12 -0500

    fix cors

--- app/__init__.py
+++ app/__init__.py
@@ -14,6 +14,7 @@ load_dotenv()
 def create_app(test_config=None):
     app = Flask(__name__)
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config['CORS_HEADERS'] = 'Content-Type'
 
     if test_config is None:
         # app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
