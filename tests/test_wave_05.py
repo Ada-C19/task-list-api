251e7a2bdf3c7a18352d8b844e91186a54478b8b
abbymachines <abby.machines@gmail.com>
2023-07-10 17:13:43 -0700

    remove pass from test_get_goal_not_found

--- tests/test_wave_05.py
+++ tests/test_wave_05.py
@@ -48,15 +48,12 @@ def test_get_goal(client, one_goal):
 
 # @pytest.mark.skip(reason="test to be completed by student")
 def test_get_goal_not_found(client):
-    pass
+    # pass
     # Act
     response = client.get("/goals/1")
     response_body = response.get_json()
-
-    # raise Exception("Complete test")
-    # Assert
-    # ---- Complete Test ----
-    # assertion 1 goes here
+    
+    #Assert
     assert response.status_code == 404
     # assertion 2 goes here
     assert response_body == {"details": "Goal 1 not found"}
