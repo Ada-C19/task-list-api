9ca8e1c22323f842bc7bf83397e0347b0d74cb80
abbymachines <abby.machines@gmail.com>
2023-07-10 17:11:23 -0700

    remove extraneous comments from test wave 01

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -59,11 +59,6 @@ def test_get_task_not_found(client):
 
     # Assert
     assert response.status_code == 404
-
-    # raise Exception("Complete test with assertion about response body")
-    # *****************************************************************
-    # **Complete test with assertion about response body***************
-    # *****************************************************************
     assert "details" in response_body
     assert response_body == {
         "details": f"Task 1 not found"
@@ -133,11 +128,6 @@ def test_update_task_not_found(client):
 
     # Assert
     assert response.status_code == 404
-
-    # raise Exception("Complete test with assertion about response body")
-    # *****************************************************************
-    # **Complete test with assertion about response body***************
-    # *****************************************************************
     assert "details" in response_body
     assert response_body == {
         "details": f"Task 1 not found"
@@ -167,11 +157,6 @@ def test_delete_task_not_found(client):
 
     # Assert
     assert response.status_code == 404
-
-    # raise Exception("Complete test with assertion about response body")
-    # *****************************************************************
-    # **Complete test with assertion about response body***************
-    # *****************************************************************
     assert "details" in response_body
     assert response_body == {
         "details": f"Task 1 not found"
